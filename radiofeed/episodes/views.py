ae0695cf738dac36430323fde3667df1cd8962a0
Dan Jacob <danjac2018@gmail.com>
2020-12-07 01:52:58 +0200

    Trigram op

--- radiofeed/episodes/views.py
+++ radiofeed/episodes/views.py
@@ -25,18 +25,18 @@ def episode_detail(request, episode_id, slug=None):
 
 @require_POST
 def play_episode(request, episode_id):
+    """Add episode to session"""
     episode = get_object_or_404(
         Episode.objects.select_related("podcast"), pk=episode_id
     )
-    # if user logged in, update history in DB
     request.session["player"] = {"episode": episode.id, "current_time": 0}
     return TemplateResponse(request, "episodes/_player.html", {"episode": episode})
 
 
 @require_POST
 def stop_episode(request, episode_id):
+    """Remove episode from session"""
     episode = get_object_or_404(Episode, pk=episode_id)
-    # if user logged in, update history in DB
     if (
         "player" in request.session
         and request.session["player"]["episode"] == episode.id
@@ -47,13 +47,13 @@ def stop_episode(request, episode_id):
 
 @require_POST
 def episode_progress(request, episode_id):
+    """Update current play time of episode"""
     episode = get_object_or_404(Episode, pk=episode_id)
     if (
         "player" in request.session
         and request.session["player"]["episode"] == episode.id
     ):
         player = request.session["player"]
-        # if user logged in, update history in DB
         try:
             current_time = int(json.loads(request.body)["current_time"])
         except (json.JSONDecodeError, KeyError):
