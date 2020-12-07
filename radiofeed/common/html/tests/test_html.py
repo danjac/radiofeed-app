59124ecbfc7c04072b37a229d84fe57eb223d19d
Dan Jacob <danjac2018@gmail.com>
2020-12-07 14:21:30 +0200

    Episode and podcast tests

---
+++ radiofeed/common/html/tests/test_html.py
@@ -0,0 +1,20 @@
+# Local
+from .. import clean_html_content, stripentities
+
+
+class TestCleanHtmlContent:
+    def test_clean_html_content_if_safe(self):
+        text = "<p>testing with paras</p>"
+        assert clean_html_content(text) == text
+
+    def test_clean_html_content_if_unsafe(self):
+        text = "<script>alert('xss ahoy!')</script>"
+        assert clean_html_content(text) == "alert('xss ahoy!')"
+
+    def test_named_stripentities(self):
+        text = "this &amp; that"
+        assert stripentities(text) == "this & that"
+
+    def test_numeric_stripentities(self):
+        text = "gov&#8217;t"
+        assert stripentities(text) == "gov’t"
