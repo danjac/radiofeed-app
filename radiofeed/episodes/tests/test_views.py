974a4067920b88a9461b0b810a600fa42e78414a
Dan Jacob <danjac2018@gmail.com>
2020-12-07 13:54:04 +0200

    Episode and podcast tests

--- radiofeed/episodes/tests/test_views.py
+++ radiofeed/episodes/tests/test_views.py
@@ -1,3 +1,9 @@
+# Standard Library
+import json
+
+# Django
+from django.urls import reverse
+
 # Third Party Libraries
 import pytest
 
@@ -11,13 +17,46 @@ pytestmark = pytest.mark.django_db
 class TestEpisodeList:
     def test_get(self, rf):
         EpisodeFactory.create_batch(3)
-        response = views.episode_list(rf.get("/"))
-        assert response.status_code == 200
-        assert len(response.context_data["episodes"]) == 3
+        resp = views.episode_list(rf.get(reverse("episodes:episode_list")))
+        assert resp.status_code == 200
+        assert len(resp.context_data["episodes"]) == 3
 
 
 class TestEpisodeDetail:
     def test_get(self, rf, episode):
-        response = views.episode_detail(rf.get("/"), episode.id, episode.slug)
-        assert response.status_code == 200
-        assert response.context_data["episode"] == episode
+        resp = views.episode_detail(
+            rf.get(episode.get_absolute_url()), episode.id, episode.slug
+        )
+        assert resp.status_code == 200
+        assert resp.context_data["episode"] == episode
+
+
+class TestStartPlayer:
+    def test_post(self, rf, episode):
+        req = rf.post(reverse("episodes:start_player", args=[episode.id]))
+        req.session = {}
+        resp = views.start_player(req, episode.id)
+        assert resp.status_code == 200
+        assert req.session["player"] == {"episode": episode.id, "current_time": 0}
+
+
+class TestStopPlayer:
+    def test_post(self, rf, episode):
+        req = rf.post(reverse("episodes:stop_player"))
+        req.session = {"player": {"episode": episode.id, "current_time": 1000}}
+        resp = views.stop_player(req)
+        assert resp.status_code == 204
+        assert req.session == {}
+
+
+class TestUpdatePlayerTime:
+    def test_post(self, rf, episode):
+        req = rf.post(
+            reverse("episodes:update_player_time"),
+            data=json.dumps({"current_time": 1030}),
+            content_type="application/json",
+        )
+        req.session = {"player": {"episode": episode.id, "current_time": 1000}}
+        resp = views.update_player_time(req)
+        assert resp.status_code == 204
+        assert req.session == {"player": {"episode": episode.id, "current_time": 1030}}
