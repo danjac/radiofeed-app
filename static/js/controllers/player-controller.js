b7e23784f3bfdca69de3422c2ec71aee61dd0edc
Dan Jacob <danjac2018@gmail.com>
2020-12-08 21:12:18 +0200

    Code tidy

--- static/js/controllers/player-controller.js
+++ static/js/controllers/player-controller.js
@@ -88,91 +88,11 @@ export default class extends Controller {
   timeUpdate() {
     const { currentTime } = this.audioTarget;
     this.currentTimeValue = currentTime;
-
-    const pcComplete = this.getPercentComplete();
-
-    this.progressTarget.style.width = pcComplete + '%';
-    this.indicatorTarget.style.left = this.getCurrentPostion(pcComplete) + 'px';
-
-    this.sendTimeUpdate();
   }
 
   progress() {
     const { buffered } = this.audioTarget;
     this.bufferTarget.style.width = this.getPercentBuffered(buffered) + '%';
-  }
-
-  getBuffered(buffered) {
-    const rv = [];
-    for (let i = 0; i < buffered.length; ++i) {
-      const start = buffered.start(i);
-      const end = buffered.end(i);
-      rv.push([start, end]);
-    }
-    return rv;
-  }
-
-  getPercentComplete() {
-    if (!this.currentTimeValue || !this.durationValue) {
-      return 0;
-    }
-
-    return (this.currentTimeValue / this.durationValue) * 100;
-  }
-
-  getPercentBuffered(buffered) {
-    buffered = this.getBuffered(buffered);
-    if (buffered.length === 0) {
-      return 0;
-    }
-    const maxBuffered = buffered.reduce(
-      (acc, timeRange) => (timeRange[1] > acc ? timeRange[1] : acc),
-      0
-    );
-    return (maxBuffered / this.durationValue) * 100 - this.getPercentComplete();
-  }
-
-  getProgressWidth(pcComplete) {
-    const clientWidth = this.progressBarTarget.clientWidth;
-    let completeWidth = 0;
-    if (clientWidth === 0) {
-      return 0;
-    } else {
-      // min 1rem to accomodate indicator
-      const minWidth = (16 / clientWidth) * 100;
-      return pcComplete > minWidth ? pcComplete : minWidth;
-    }
-  }
-
-  getCurrentPostion(pcComplete) {
-    let currentPosition = this.progressBarTarget.getBoundingClientRect().left - 16;
-    const width = this.getProgressWidth(pcComplete);
-    if (width) {
-      currentPosition += this.progressBarTarget.clientWidth * (width / 100);
-    }
-    return currentPosition;
-  }
-
-  sendTimeUpdate() {
-    // update every 15s or so
-    const diff = Math.ceil(Math.abs(this.currentTimeValue - this.lastUpdated || 0));
-    if (diff % 15 === 0) {
-      axios.post(this.progressUrlValue, { current_time: this.currentTimeValue });
-      this.lastUpdated = this.currentTimeValue;
-    }
-  }
-
-  formatTime(value) {
-    if (!value || value < 0) return '00:00:00';
-    const duration = Math.floor(parseInt(value));
-
-    const hours = Math.floor(duration / 3600);
-    const minutes = Math.floor((duration % 3600) / 60);
-    const seconds = Math.floor(duration % 60);
-
-    return [hours, minutes, seconds]
-      .map((t) => t.toString().padStart(2, '0'))
-      .join(':');
   }
 
   // observers
@@ -208,9 +128,88 @@ export default class extends Controller {
   }
 
   currentTimeValueChanged() {
+    if (this.hasProgressTarget && this.hasIndicatorTarget) {
+      const pcComplete = this.getPercentComplete();
+
+      this.progressTarget.style.width = pcComplete + '%';
+      this.indicatorTarget.style.left = this.getCurrentPostion(pcComplete) + 'px';
+    }
+
     if (this.hasCounterTarget) {
       this.counterTarget.textContent =
         '-' + this.formatTime(this.durationValue - this.currentTimeValue);
     }
+
+    this.sendTimeUpdate();
+  }
+
+  getPercentBuffered(buffered) {
+    const arr = [];
+    for (let i = 0; i < buffered.length; ++i) {
+      const start = buffered.start(i);
+      const end = buffered.end(i);
+      arr.push([start, end]);
+    }
+
+    if (arr.length === 0) {
+      return 0;
+    }
+
+    const maxBuffered = arr.reduce(
+      (acc, timeRange) => (timeRange[1] > acc ? timeRange[1] : acc),
+      0
+    );
+    return (maxBuffered / this.durationValue) * 100 - this.getPercentComplete();
+  }
+
+  getPercentComplete() {
+    if (!this.currentTimeValue || !this.durationValue) {
+      return 0;
+    }
+
+    return (this.currentTimeValue / this.durationValue) * 100;
+  }
+
+  getWidthComplete(pcComplete) {
+    const clientWidth = this.progressBarTarget.clientWidth;
+    let completeWidth = 0;
+    if (clientWidth === 0) {
+      return 0;
+    } else {
+      // min 1rem to accomodate indicator
+      const minWidth = (16 / clientWidth) * 100;
+      return pcComplete > minWidth ? pcComplete : minWidth;
+    }
+  }
+
+  getCurrentPostion(pcComplete) {
+    let currentPosition = this.progressBarTarget.getBoundingClientRect().left - 16;
+    const width = this.getWidthComplete(pcComplete);
+    if (width) {
+      currentPosition += this.progressBarTarget.clientWidth * (width / 100);
+    }
+    return currentPosition;
+  }
+
+  sendTimeUpdate() {
+    // update every 15s or so
+    const diff = Math.ceil(Math.abs(this.currentTimeValue - this.lastUpdated || 0));
+    if (diff % 15 === 0) {
+      axios.post(this.progressUrlValue, { current_time: this.currentTimeValue });
+      this.lastUpdated = this.currentTimeValue;
+    }
+  }
+
+  formatTime(value) {
+    if (!value || value < 0) return '00:00:00';
+    const duration = Math.floor(parseInt(value));
+
+    const hours = Math.floor(duration / 3600);
+    const minutes = Math.floor((duration % 3600) / 60);
+    const seconds = Math.floor(duration % 60);
+
+    return [hours, minutes, seconds]
+      .map((t) => t.toString().padStart(2, '0'))
+      .join(':');
   }
 }
