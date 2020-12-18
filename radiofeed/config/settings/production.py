2293899f41e0c693f457339b2fba4d91e39d1177
Dan Jacob <danjac2018@gmail.com>
2020-12-18 02:21:36 +0200

    Generic prod settings

--- radiofeed/config/settings/production.py
+++ radiofeed/config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *  # noqa
 from .mixins.aws import *  # noqa
 from .mixins.mailgun import *  # noqa
 from .mixins.secure import *  # noqa
+
 # from .mixins.sentry import *  # noqa
 
-# Required for Heroku SSL
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
