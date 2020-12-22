277a42648ae04ebf70195268c59faced89d7a83d
Dan Jacob <danjac2018@gmail.com>
2020-12-22 20:23:19 +0200

    Player fixes

--- static/js/controllers/episode-controller.js
+++ static/js/controllers/episode-controller.js
@@ -13,6 +13,7 @@ export default class extends Controller {
     currentTime: Number,
     playUrl: String,
     playing: Boolean,
+    completed: Boolean,
   };
 
   connect() {
@@ -45,27 +46,31 @@ export default class extends Controller {
     const { currentTime, episode, completed } = event.detail;
     if (episode === this.episodeValue) {
       this.playingValue = false;
-      if (completed) {
-        this.currentTimeValue = 0;
-        this.currentTimeTarget.classList.add(this.completedClass);
-        this.currentTimeTarget.textContent = this.durationValue;
-      } else {
-        this.currentTimeValue = currentTime;
-      }
+      this.currentTimeValue = currentTime;
+      this.completed = completed;
     }
   }
 
   update({ detail: { episode, time_remaining, completed } }) {
     if (episode && episode === this.episodeValue) {
-      if (completed) {
-        this.currentTimeValue = 0;
-        this.currentTimeTarget.textContent = this.durationValue;
-        this.currentTimeTarget.classList.add(this.completedClass);
-      } else if (time_remaining) {
+      if (time_remaining) {
         this.currentTimeTarget.textContent = '~' + time_remaining;
       } else {
         this.currentTimeTarget.textContent = this.durationValue;
       }
+      this.completedValue = completed;
+    }
+  }
+
+  completedValueChanged() {
+    if (this.completedValue) {
+      this.currentTimeValue = 0;
+      if (this.hasCurrentTimeTarget) {
+        this.currentTimeTarget.textContent = this.durationValue;
+        this.currentTimeTarget.classList.add(this.completedClass);
+      }
+    } else if (this.hasCurrentTimeTarget) {
+      this.currentTimeTarget.classList.remove(this.completedClass);
     }
   }
 
