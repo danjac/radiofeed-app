90bf1356d20d9e6009c3bf6450c5b7bc7eed77e8
Dan Jacob <danjac2018@gmail.com>
2020-12-22 19:20:17 +0200

    Toggle class

--- static/js/controllers/toggle-controller.js
+++ static/js/controllers/toggle-controller.js
@@ -2,13 +2,15 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   static targets = ['item'];
+  static classes = ['toggle'];
 
   toggle(event) {
     event.preventDefault();
     event.stopPropagation();
 
+    const toggleClass = this.hasToggleClass ? this.toggleClass : 'hidden';
     this.itemTargets.forEach((item) => {
-      item.classList.toggle('hidden');
+      item.classList.toggle(toggleClass);
     });
   }
 }
