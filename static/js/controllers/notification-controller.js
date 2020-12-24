49c812648cefcc644db491527be002ad00f1ab8c
Dan Jacob <danjac2018@gmail.com>
2020-12-25 01:13:29 +0200

    Simplify notification controller

--- static/js/controllers/notification-controller.js
+++ static/js/controllers/notification-controller.js
@@ -1,29 +1,17 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  // permanently or temporarily remove notifications e.g.
-  // flash messages or cookie banners
-  //
-
   static values = {
     timeout: Number,
-    storageKey: String,
   };
 
   connect() {
-    if (this.hasStorageKeyValue && window.localStorage.getItem(this.storageKeyValue)) {
-      // automatically remove if storage key present
-      this.remove();
-    } else if (this.hasTimeoutValue && this.timeoutValue > 0) {
-      // automatically toggle elements on page load after timeout
+    if (this.hasTimeoutValue && this.timeoutValue > 0) {
       setTimeout(() => this.remove(), this.timeoutValue);
     }
   }
 
   remove() {
-    if (this.hasStorageKeyValue) {
-      window.localStorage.setItem(this.storageKeyValue, true);
-    }
     this.element.remove();
   }
 }
