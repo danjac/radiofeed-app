656fb1676ad9d69c53d0854532df68e675c9b063
Dan Jacob <danjac2018@gmail.com>
2020-12-25 11:33:28 +0200

    More tests

--- radiofeed/episodes/tests/test_utils.py
+++ radiofeed/episodes/tests/test_utils.py
@@ -10,6 +10,9 @@ class TestFormatDuration:
     def test_format_duration_if_less_than_one_minute(self):
         assert format_duration(30) == "<1min"
 
+    def test_format_duration_if_less_than_ten_minutes(self):
+        assert format_duration(540) == "9min"
+
     def test_format_duration_if_less_than_one_hour(self):
         assert format_duration(2400) == "40min"
 
