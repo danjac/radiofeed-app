e6a1f640a769bc420a449d0526f5ae6862aa37da
Dan Jacob <danjac2018@gmail.com>
2020-12-26 17:59:26 +0200

    Turbo fixes

--- radiofeed/common/turbo/__init__.py
+++ radiofeed/common/turbo/__init__.py
@@ -2,11 +2,11 @@
 from django.template.loader import render_to_string
 
 
-def render_turbo_stream(target, action, content=""):
+def render_turbo_stream(action, target, content=""):
     return f'<turbo-stream target="{target}" action="{action}"><template>{content}</template></turbo-stream>'
 
 
 def render_turbo_stream_template_to_string(template, context, action, target, **kwargs):
     return render_turbo_stream(
-        target, action, render_to_string(template, context, **kwargs)
+        action, target, render_to_string(template, context, **kwargs)
     )
