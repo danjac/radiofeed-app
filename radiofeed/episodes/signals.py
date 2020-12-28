470a6ffed71ee181cb7aef06cca1e03996e0bf7b
Dan Jacob <danjac2018@gmail.com>
2020-12-28 20:34:19 +0200

    Disable signals

--- radiofeed/episodes/signals.py
+++ radiofeed/episodes/signals.py
@@ -1,6 +1,6 @@
 # Django
-from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.dispatch import receiver
+# from django.contrib.auth.signals import user_logged_in, user_logged_out
+# from django.dispatch import receiver
 
 
 def close_player(sender, user, request, **kwargs):
@@ -8,5 +8,5 @@ def close_player(sender, user, request, **kwargs):
     request.session.pop("player", None)
 
 
-receiver(user_logged_in)(close_player)
-receiver(user_logged_out)(close_player)
+# receiver(user_logged_in)(close_player)
+# receiver(user_logged_out)(close_player)
