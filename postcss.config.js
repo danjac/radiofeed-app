dcfb8c45612b75ecf068cbd5481e474d01f27f09
Dan Jacob <danjac2018@gmail.com>
2020-12-29 13:07:46 +0200

    Add eslint disable to webpack.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-// Copyright (c) 2020 by Dan Jacob
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 /* eslint-disable */
 module.exports = {
   plugins: [
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const path = require('path');
 const isProduction = process.env.NODE_ENV === 'production';
 
