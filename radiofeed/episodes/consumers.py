21de06374ebc1487c69531c08d3ace8f2690ad91
Dan Jacob <danjac2018@gmail.com>
2021-01-05 19:08:53 +0200

    Refactor consumers

--- radiofeed/episodes/consumers.py
+++ radiofeed/episodes/consumers.py
@@ -16,34 +16,6 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
     async def disconnect(self, code):
         await self.channel_layer.group_discard("player", self.channel_name)
 
-    @database_sync_to_async
-    def get_episode(self, event):
-        try:
-            return Episode.objects.get(pk=event["episode"])
-        except Episode.DoesNotExist:
-            return None
-
-    def event_matches_request_id(self, event):
-        return self.request_id == event["request_id"]
-
-    async def send_turbo_stream(self, episode, target, template_name, context=None):
-        await self.send(
-            TurboStream(target)
-            .replace.template(template_name, {"episode": episode, **(context or {})},)
-            .render()
-        )
-
-    async def send_episode_play_buttons(self, event, is_playing):
-        if self.event_matches_request_id(event) and (
-            episode := await self.get_episode(event)
-        ):
-            await self.send_turbo_stream(
-                episode,
-                f"episode-play-buttons-{episode.id}",
-                "episodes/_play_buttons_toggle.html",
-                {"is_playing": is_playing},
-            )
-
     async def player_start(self, event):
         await self.send_episode_play_buttons(event, is_playing=True)
 
@@ -55,8 +27,32 @@ class PlayerConsumer(AsyncJsonWebsocketConsumer):
             episode := await self.get_episode(event)
         ):
             await self.send_turbo_stream(
-                episode,
                 f"episode-current-time-{episode.id}",
                 "episodes/_current_time.html",
-                event["info"],
+                {"episode": episode, **event["info"]},
+            )
+
+    @database_sync_to_async
+    def get_episode(self, event):
+        try:
+            return Episode.objects.get(pk=event["episode"])
+        except Episode.DoesNotExist:
+            return None
+
+    def event_matches_request_id(self, event):
+        return self.request_id == event["request_id"]
+
+    async def send_turbo_stream(self, target, template_name, context=None):
+        await self.send(
+            TurboStream(target).replace.template(template_name, context or {}).render()
+        )
+
+    async def send_episode_play_buttons(self, event, is_playing):
+        if self.event_matches_request_id(event) and (
+            episode := await self.get_episode(event)
+        ):
+            await self.send_turbo_stream(
+                f"episode-play-buttons-{episode.id}",
+                "episodes/_play_buttons_toggle.html",
+                {"episode": episode, "is_playing": is_playing},
             )
