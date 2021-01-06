3d8753fb72893ad81789e5b5a54d560bd020d607
Dan Jacob <danjac2018@gmail.com>
2021-01-06 19:39:26 +0200

    Set max 1mb content length on images

--- radiofeed/podcasts/rss_parser/images.py
+++ radiofeed/podcasts/rss_parser/images.py
@@ -26,11 +26,23 @@ def fetch_image_from_url(image_url):
     try:
         if not image_url:
             raise ValueError("image_url is empty")
+
+        # check head for image size
+
+        resp = requests.head(image_url, headers=get_headers())
+        resp.raise_for_status()
+
+        # Max 1MB
+        if (content_length := int(resp.headers["Content-Length"]) / 1048576.0) > 1.0:
+            raise ValueError(f"{image_url} too large: {content_length}")
+
+        content_type = resp.headers["Content-Type"].split(";")[0]
+
+        filename = get_image_filename(image_url, content_type)
+
         resp = requests.get(image_url, headers=get_headers())
         resp.raise_for_status()
 
-        content_type = resp.headers["Content-Type"].split(";")[0]
-        filename = get_image_filename(image_url, content_type)
         return ImageFile(io.BytesIO(resp.content), name=filename)
     except (requests.RequestException, KeyError, ValueError) as e:
         raise InvalidImageURL from e
