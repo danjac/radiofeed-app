dab8ddcf07710178e0ced6d2db49f82246d199da
Dan Jacob <danjac2018@gmail.com>
2021-01-23 02:00:24 +0200

    Image optimization

--- radiofeed/podcasts/rss_parser/images.py
+++ radiofeed/podcasts/rss_parser/images.py
@@ -31,8 +31,8 @@ def fetch_image_from_url(image_url):
         resp = requests.head(image_url, headers=get_headers())
         resp.raise_for_status()
 
-        # Max 5MB
-        if (content_length := int(resp.headers["Content-Length"]) / 1048576.0) > 5.0:
+        # Max 6MB
+        if (content_length := int(resp.headers["Content-Length"]) / 1048576.0) > 6.0:
             raise ValueError(f"{image_url} too large: {content_length}")
 
         content_type = resp.headers["Content-Type"].split(";")[0]
