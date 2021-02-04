0e7c999f6b8cd7a85fe8ac55b0ffc3184450351d
Dan Jacob <danjac2018@gmail.com>
2021-02-04 16:43:13 +0200

    Re-enable sentry

--- radiofeed/config/settings/production.py
+++ radiofeed/config/settings/production.py
@@ -3,7 +3,6 @@ from .base import *  # noqa
 from .mixins.aws import *  # noqa
 from .mixins.mailgun import *  # noqa
 from .mixins.secure import *  # noqa
-
-# from .mixins.sentry import *  # noqa
+from .mixins.sentry import *  # noqa
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
