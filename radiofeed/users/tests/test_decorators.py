1b3abd2c177774d517742e4bdcff1ff0ad6d89b0
Dan Jacob <danjac2018@gmail.com>
2021-02-06 18:17:47 +0200

    More tests

---
+++ radiofeed/users/tests/test_decorators.py
@@ -0,0 +1,27 @@
+import http
+
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+
+import pytest
+
+from ..decorators import ajax_login_required
+
+
+@ajax_login_required
+def ajax_view(request):
+    return HttpResponse("OK")
+
+
+class TestAjaxLoginRequired:
+    def test_is_authenticated(self, rf, user_model):
+        req = rf.get("/")
+        req.user = user_model()
+        resp = ajax_view(req)
+        assert resp.status_code == http.HTTPStatus.OK
+
+    def test_is_anonymous(self, rf, anonymous_user):
+        req = rf.get("/")
+        req.user = anonymous_user
+        with pytest.raises(PermissionDenied):
+            ajax_view(req)
