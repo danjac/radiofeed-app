43a86ffe7808007bf5842ff93e0f5ea1b9cc240b
Dan Jacob <danjac2018@gmail.com>
2021-02-10 18:20:34 +0200

    Return result

--- radiofeed/shortcuts.py
+++ radiofeed/shortcuts.py
@@ -1,27 +1,19 @@
-import dataclasses
-from typing import Type
+import contextlib
+from typing import Generator, Tuple, Type
 
 from django.forms import Form
 from django.http import HttpRequest
 
 
-@dataclasses.dataclass
-class FormResult:
-    form: Form
-    is_valid: bool = False
-
-    def __bool__(self) -> bool:
-        return self.is_valid
-
-
+@contextlib.contextmanager
 def handle_form(
     request: HttpRequest, form_class: Type[Form], **form_kwargs
-) -> FormResult:
+) -> Generator[Tuple[Form, bool], None, None]:
     # in case form has "request" as arg
     if "_request" in form_kwargs:
         form_kwargs["request"] = form_kwargs.pop("_request")
     if request.method == "POST":
         form = form_class(data=request.POST, files=request.FILES, **form_kwargs)
-        return FormResult(form, form.is_valid())
+        yield form, form.is_valid()
     else:
-        return FormResult(form_class(**form_kwargs))
+        yield form_class(**form_kwargs), False
