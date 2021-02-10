8d5da6a22cefdf16667748a2ad7f3319ea862b68
Dan Jacob <danjac2018@gmail.com>
2021-02-10 17:17:14 +0200

    Use bigintegerfield

--- radiofeed/episodes/models.py
+++ radiofeed/episodes/models.py
@@ -53,6 +53,8 @@ EpisodeManager: models.Manager = models.Manager.from_queryset(EpisodeQuerySet)
 
 class Episode(models.Model):
 
+    id = models.BigAutoField(primary_key=True, editable=False)
+
     podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
 
     guid = models.TextField()
