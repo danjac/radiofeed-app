905dc8b1982a831f83515804c44df2207436b8c1
Dan Jacob <danjac2018@gmail.com>
2021-02-21 14:51:39 +0200

    Button component

--- radiofeed/template/tests/test_components.py
+++ radiofeed/template/tests/test_components.py
@@ -1,14 +1,24 @@
 from django import forms
 
-from ..components import FormComponent
+from ..components import ButtonComponent, FormComponent
 
 
 class MyForm(forms.Form):
     text = forms.CharField()
 
 
-class TestFormComponents:
+class TestFormComponent:
     def test_context(self):
         form = MyForm()
         ctx = FormComponent("form").context(form)
         assert ctx["form"] == form
+
+
+class TestButtonComponent:
+    def test_context_is_button(self):
+        ctx = ButtonComponent("button").context("test")
+        assert ctx["tag"] == "button"
+
+    def test_context_is_link(self):
+        ctx = ButtonComponent("button").context("test", href="/")
+        assert ctx["tag"] == "a"
