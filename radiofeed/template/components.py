578eafc253b5a8620123beb947d18ec51525e8ce
Dan Jacob <danjac2018@gmail.com>
2021-02-21 15:34:33 +0200

    Search form component

--- radiofeed/template/components.py
+++ radiofeed/template/components.py
@@ -8,27 +8,6 @@ from django_components import component
 from .defaulttags import htmlattrs
 
 
-class ShareButtonsComponent(component.Component):
-    def context(self, url: str, subject: str) -> Dict:
-        url = parse.quote(self.outer_context["request"].build_absolute_uri(url))
-        subject = parse.quote(subject)
-
-        return {
-            "share_urls": {
-                "email": f"mailto:?subject={subject}&body={url}",
-                "facebook": f"https://www.facebook.com/sharer/sharer.php?u={url}",
-                "twitter": f"https://twitter.com/share?url={url}&text={subject}",
-                "linkedin": f"https://www.linkedin.com/sharing/share-offsite/?url={url}",
-            }
-        }
-
-    def template(self, context: Dict) -> str:
-        return "components/buttons/_share_buttons.html"
-
-
-component.registry.register(name="share_buttons", component=ShareButtonsComponent)
-
-
 class FormComponent(component.Component):
     def context(self, form: Form, action_url: str = "", **attrs) -> Dict:
         return {"form": form, "action_url": action_url, "attrs": htmlattrs(attrs)}
@@ -41,7 +20,27 @@ component.registry.register(name="form", component=FormComponent)
 
 
 class SearchFormComponent(component.Component):
-    ...
+    def context(
+        self,
+        search_url: str = "",
+        search_param: str = "q",
+        placeholder: str = "",
+        css_class: str = "",
+        **attrs,
+    ):
+        return {
+            "search_url": search_url,
+            "search_param": search_param,
+            "placeholder": placeholder,
+            "css_class": css_class,
+            "attrs": htmlattrs(attrs),
+        }
+
+    def template(self, context: Dict) -> str:
+        return "components/forms/_search_form.html"
+
+
+component.registry.register(name="search_form", component=SearchFormComponent)
 
 
 class ButtonComponent(component.Component):
@@ -84,3 +83,24 @@ class IconComponent(component.Component):
 
 
 component.registry.register(name="icon", component=IconComponent)
+
+
+class ShareButtonsComponent(component.Component):
+    def context(self, url: str, subject: str) -> Dict:
+        url = parse.quote(self.outer_context["request"].build_absolute_uri(url))
+        subject = parse.quote(subject)
+
+        return {
+            "share_urls": {
+                "email": f"mailto:?subject={subject}&body={url}",
+                "facebook": f"https://www.facebook.com/sharer/sharer.php?u={url}",
+                "twitter": f"https://twitter.com/share?url={url}&text={subject}",
+                "linkedin": f"https://www.linkedin.com/sharing/share-offsite/?url={url}",
+            }
+        }
+
+    def template(self, context: Dict) -> str:
+        return "components/buttons/_share_buttons.html"
+
+
+component.registry.register(name="share_buttons", component=ShareButtonsComponent)
