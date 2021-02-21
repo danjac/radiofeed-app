afbd05976f79ab537c331245964939453886cc71
Dan Jacob <danjac2018@gmail.com>
2021-02-21 02:35:10 +0200

    Test components

---
+++ radiofeed/episodes/components.py
@@ -0,0 +1,37 @@
+from typing import Dict
+
+from django.urls import reverse
+
+from django_components import component
+
+from radiofeed.template.defaulttags import htmlattrs
+
+from .models import Episode
+
+
+class EpisodeComponent(component.Component):
+    def context(
+        self,
+        episode: Episode,
+        dom_id: str = "",
+        podcast_url: str = "",
+        actions_url: str = "",
+        **attrs,
+    ) -> Dict:
+        return {
+            "episode": episode,
+            "podcast": episode.podcast,
+            "dom_id": dom_id or episode.get_dom_id(),
+            "duration": episode.get_duration_in_seconds(),
+            "actions_url": actions_url
+            or reverse("episodes:actions", args=[episode.id]),
+            "episode_url": episode.get_absolute_url(),
+            "podcast_url": podcast_url or episode.podcast.get_absolute_url(),
+            "attrs": htmlattrs(attrs),
+        }
+
+    def template(self, context: Dict) -> str:
+        return "episodes/_episode.html"
+
+
+component.registry.register(name="episode", component=EpisodeComponent)
---
+++ radiofeed/template/components.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from django.forms import Form
+
+from django_components import component
+
+from .defaulttags import htmlattrs
+
+
+class FormComponent(component.Component):
+    def context(self, form: Form, action_url: str = "", **attrs) -> Dict:
+        return {"form": form, "action_url": action_url, "attrs": htmlattrs(attrs)}
+
+    def template(self, context: Dict) -> str:
+        return "forms/_form_component.html"
+
+
+component.registry.register(name="form", component=FormComponent)
