893f706d52092c04a5a608175a34a38691f9a8d9
Dan Jacob <danjac2018@gmail.com>
2021-02-21 14:39:33 +0200

    Button component

--- radiofeed/template/components.py
+++ radiofeed/template/components.py
@@ -19,7 +19,29 @@ component.registry.register(name="form", component=FormComponent)
 
 
 class ButtonComponent(component.Component):
-    ...
+    def context(
+        self,
+        text: str,
+        icon: str = "",
+        type: str = "default",
+        css_class: str = "",
+        **attrs,
+    ) -> Dict:
+
+        return {
+            "text": text,
+            "icon": icon,
+            "type": type,
+            "css_class": css_class,
+            "tag": "a" if "href" in attrs else "button",
+            "attrs": htmlattrs(attrs),
+        }
+
+    def template(self, context: Dict) -> str:
+        return "forms/_button.html"
+
+
+component.registry.register(name="button", component=ButtonComponent)
 
 
 class IconComponent(component.Component):
