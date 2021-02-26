8c1f108fbd799d3dc0c5dc46fbf6347cfc402e34
Dan Jacob <danjac2018@gmail.com>
2021-02-26 19:26:35 +0200

    Use make_context

--- radiofeed/shortcuts.py
+++ radiofeed/shortcuts.py
@@ -4,14 +4,14 @@ from typing import Type
 
 from django.forms import Form
 from django.http import HttpRequest
-from django.template.context import RequestContext
+from django.template.context import make_context
 from django_components.component import registry
 
 
 def render_component(request: HttpRequest, component_name: str, *args, **kwargs) -> str:
     """Render a component as string. Use with turbo streams/frames
     to render the component in a response."""
-    context = RequestContext(request, {"request": request})
+    context = make_context({"request": request}, request)
 
     component = registry.get(component_name)(component_name)
     component.outer_context = context.flatten()
