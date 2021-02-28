75f403ae27e22f73a88344414e313fd8e2334db3
Dan Jacob <danjac2018@gmail.com>
2021-02-28 17:05:52 +0200

    Better typeahead

--- static/src/js/controllers/autocomplete-controller.js
+++ static/src/js/controllers/autocomplete-controller.js
@@ -4,63 +4,19 @@ export default class extends Controller {
   static targets = ['results', 'result', 'form', 'input'];
   static values = { search: String };
 
-  connect() {
-    this.index = 0;
-  }
-
-  typeahead(event) {
-    console.log('typeahead...', event.code);
-    if (event.code === 'escape') {
-      return;
-    }
+  typeahead() {
     this.searchValue = this.inputTarget.value.trim();
   }
 
-  navigate(event) {
-    console.log('navigate...', event.code);
-    if (this.resultTargets.length === 0) {
-      return;
-    }
-    let position, target;
-
-    switch (event.code) {
-      case 'Escape':
-        this.close();
-        break;
-      case 'ArrowDown':
-        position = 1;
-        break;
-      case 'ArrowUp':
-        position = -1;
-        break;
-      default:
-        position = 0;
-    }
-
-    if (position) {
-      target = this.resultTargets[this.index];
-    }
-
-    if (target) {
-      this.index += position;
-      target.focus();
-    } else {
-      this.index = 0;
-      this.inputTarget.focus();
+  close(event) {
+    if (event.code === 'Escape' && this.resultTargets.length > 0) {
+      this.resultsTarget.textContent = '';
+      this.resultsTarget.setAttribute('src', '');
     }
   }
-
   searchValueChanged() {
     if (this.searchValue.length > 3) {
       this.formTarget.requestSubmit();
     }
-  }
-
-  close() {
-    this.inputTarget.value = '';
-    this.searchValue = '';
-    console.log('resultstarget', this.resultstarget);
-    this.resultsTarget.textContent = '';
-    this.resultsTarget.setAttribute('src', '');
   }
 }
