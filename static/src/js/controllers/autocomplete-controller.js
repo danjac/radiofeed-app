b278b4ff0461574450b616ddc023faa0ee756cd6
Dan Jacob <danjac2018@gmail.com>
2021-02-28 23:08:39 +0200

    More search fixes

--- static/src/js/controllers/autocomplete-controller.js
+++ static/src/js/controllers/autocomplete-controller.js
@@ -1,9 +1,14 @@
 import { Controller } from 'stimulus';
+import debounce from 'lodash.debounce';
 
 export default class extends Controller {
   static targets = ['results', 'result', 'form', 'input'];
   static values = { search: String };
   static classes = ['selected'];
+
+  initialize() {
+    this.typeahead = debounce(this.typeahead, 500).bind(this);
+  }
 
   connect() {
     this.index = 0;
