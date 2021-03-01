b1d565df5bd4ab9cdc8a073869f4e72594e42c20
Dan Jacob <danjac2018@gmail.com>
2021-03-01 13:17:31 +0200

    Nav tweaks

--- static/src/js/controllers/toggle-controller.js
+++ static/src/js/controllers/toggle-controller.js
@@ -3,11 +3,15 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
   static targets = ['item'];
   static classes = ['toggle'];
+  static values = { scroll: Boolean };
 
   toggle() {
     const toggleClass = this.hasToggleClass ? this.toggleClass : 'hidden';
     this.itemTargets.forEach((item) => {
       item.classList.toggle(toggleClass);
     });
+    if (this.scrollValue) {
+      document.body.scrollTop = 0;
+    }
   }
 }
