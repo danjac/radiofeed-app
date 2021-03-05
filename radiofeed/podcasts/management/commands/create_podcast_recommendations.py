cd2a4e46cfddac997fccbccd5370a9a1222fea41
Dan Jacob <danjac2018@gmail.com>
2021-03-05 14:34:03 +0200

    Refactor recommender

--- radiofeed/podcasts/management/commands/create_podcast_recommendations.py
+++ radiofeed/podcasts/management/commands/create_podcast_recommendations.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from radiofeed.podcasts.recommender import PodcastRecommender
+from radiofeed.podcasts.recommender import recommend
 
 
 class Command(BaseCommand):
     help = "Creates new podcast recommendations."
 
     def handle(self, *args, **kwargs) -> None:
-        PodcastRecommender.recommend()
+        recommend()
