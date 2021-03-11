9a6faa3cd16bb9646a4936ff2d51f0982510cf92
Dan Jacob <danjac2018@gmail.com>
2021-03-12 00:24:39 +0200

    Remove redundant form

--- radiofeed/podcasts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Podcast
-
-
-class PodcastForm(forms.ModelForm):
-    class Meta:
-        model = Podcast
-        fields = ("title", "rss", "itunes")
