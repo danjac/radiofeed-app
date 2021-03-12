2972aa36b7baa7be8f12afa576822e0e6d1261b9
Dan Jacob <danjac2018@gmail.com>
2021-03-12 13:32:05 +0200

    Adding index

--- radiofeed/podcasts/models.py
+++ radiofeed/podcasts/models.py
@@ -155,6 +155,7 @@ class Podcast(models.Model):
 
     class Meta:
         indexes = [
+            models.Index(fields=["-created", "-pub_date"]),
             models.Index(fields=["-pub_date"]),
             models.Index(fields=["pub_date"]),
             GinIndex(fields=["search_vector"]),
