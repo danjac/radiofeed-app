dc697c68ead7acf7f45a01ab43ec188daca159ae
Dan Jacob <danjac2018@gmail.com>
2021-03-13 16:57:07 +0200

    CategoryAdmin fix

--- radiofeed/podcasts/admin.py
+++ radiofeed/podcasts/admin.py
@@ -9,7 +9,7 @@ from .models import Category, Podcast
 
 
 @admin.register(Category)
-class CategoryAdmin(AdminImageMixin, admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     ordering = ("name",)
     list_display = ("name", "parent", "itunes_genre_id")
     search_fields = ("name",)
