7d29d3e82048abe6718b383266eadc8f4f96171a
Dan Jacob <danjac2018@gmail.com>
2021-03-13 15:51:57 +0200

    Admin fixes

--- radiofeed/episodes/admin.py
+++ radiofeed/episodes/admin.py
@@ -1,4 +1,8 @@
+from typing import Tuple
+
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from .models import Episode
 
@@ -8,4 +12,11 @@ class EpisodeAdmin(admin.ModelAdmin):
     ordering = ("-id",)
     list_display = ("__str__", "podcast", "pub_date")
     raw_id_fields = ("podcast",)
-    search_fields = ("title", "podcast__title")
+    search_fields = ("search_document",)
+
+    def get_search_results(
+        self, request: HttpRequest, queryset: QuerySet, search_term: str
+    ) -> Tuple[QuerySet, bool]:
+        if not search_term:
+            return super().get_search_results(request, queryset, search_term)
+        return queryset.search(search_term).order_by("-rank", "-pub_date"), False
--- radiofeed/podcasts/admin.py
+++ radiofeed/podcasts/admin.py
@@ -1,4 +1,8 @@
+from typing import Iterable, Tuple
+
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 from sorl.thumbnail.admin import AdminImageMixin
 
 from .models import Category, Podcast
@@ -15,13 +19,15 @@ class PubDateFilter(admin.SimpleListFilter):
     title = "Pub date"
     parameter_name = "pub_date"
 
-    def lookups(self, request, model_admin):
+    def lookups(
+        self, request: HttpRequest, model_admin: admin.ModelAdmin
+    ) -> Iterable[Tuple[str, str]]:
         return (
             ("yes", "With pub date"),
             ("no", "With no pub date"),
         )
 
-    def queryset(self, request, queryset):
+    def queryset(self, request: HttpRequest, queryset) -> QuerySet:
         value = self.value()
         if value == "yes":
             return queryset.filter(pub_date__isnull=False)
@@ -34,10 +40,12 @@ class PromotedFilter(admin.SimpleListFilter):
     title = "Promoted"
     parameter_name = "promoted"
 
-    def lookups(self, request, model_admin):
+    def lookups(
+        self, request: HttpRequest, model_admin: admin.ModelAdmin
+    ) -> Iterable[Tuple[str, str]]:
         return (("yes", "Promoted"),)
 
-    def queryset(self, request, queryset):
+    def queryset(self, request: HttpRequest, queryset: QuerySet) -> QuerySet:
         value = self.value()
         if value == "yes":
             return queryset.filter(promoted=True)
@@ -50,5 +58,12 @@ class PodcastAdmin(AdminImageMixin, admin.ModelAdmin):
 
     ordering = ("-pub_date",)
     list_display = ("__str__", "pub_date")
-    search_fields = ("title", "authors")
+    search_fields = ("search_document",)
     raw_id_fields = ("recipients",)
+
+    def get_search_results(
+        self, request: HttpRequest, queryset: QuerySet, search_term: str
+    ) -> Tuple[QuerySet, bool]:
+        if not search_term:
+            return super().get_search_results(request, queryset, search_term)
+        return queryset.search(search_term).order_by("-rank", "-pub_date"), False
