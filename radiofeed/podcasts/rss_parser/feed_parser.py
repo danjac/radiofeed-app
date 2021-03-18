f741b7e4fe853ae350c4068b479c13f79e65d698
Dan Jacob <danjac2018@gmail.com>
2021-03-18 16:18:53 +0200

    More refactoring

--- radiofeed/podcasts/rss_parser/feed_parser.py
+++ radiofeed/podcasts/rss_parser/feed_parser.py
@@ -1,16 +1,14 @@
 from typing import Generator, List, Optional
 
 import lxml
-import requests
 
-from lxml.etree import ElementBase, XMLSyntaxError
+from lxml.etree import ElementBase
 from pydantic import ValidationError
 
 from radiofeed.episodes.models import Episode
 
 from ..models import Podcast
-from .exceptions import InvalidFeedError, RssParserError
-from .headers import get_headers
+from .exceptions import InvalidFeedError
 from .models import Audio, Feed, Item
 
 NAMESPACES = {
@@ -23,40 +21,6 @@ NAMESPACES = {
     "podcast": "https://podcastindex.org/namespace/1.0",
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
 }
-
-
-def sync_rss_feed(podcast: Podcast, force_update: bool) -> List[Episode]:
-    """Fetches RSS and generates Feed. Checks etag header if we need to do an update.
-    If any errors occur (e.g. RSS unavailable or invalid RSS) the error is saved in database
-    and RssParserError raised.
-    """
-
-    try:
-        head_response = requests.head(podcast.rss, headers=get_headers(), timeout=5)
-        head_response.raise_for_status()
-        if (
-            (etag := head_response.headers.get("ETag", ""))
-            and etag == podcast.etag
-            and not force_update
-        ):
-            return []
-
-        response = requests.get(
-            podcast.rss, headers=get_headers(), stream=True, timeout=5
-        )
-        response.raise_for_status()
-        return parse_feed(response.content, podcast, etag, force_update)
-    except (
-        InvalidFeedError,
-        ValidationError,
-        XMLSyntaxError,
-        requests.RequestException,
-    ) as e:
-        podcast.sync_error = str(e)
-        podcast.num_retries += 1
-        podcast.save()
-
-        raise RssParserError(podcast.sync_error) from e
 
 
 def parse_feed(
