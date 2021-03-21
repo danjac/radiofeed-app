83775cc2a846027bd0cf44a4b04d5d3c3ee33396
Dan Jacob <danjac2018@gmail.com>
2021-03-21 17:01:37 +0200

    Removing annotation checks

--- radiofeed/typing.py
+++
@@ -1,9 +0,0 @@
-from typing import Callable, Union
-
-from django.conf import settings
-from django.contrib.auth.models import AnonymousUser
-from django.http import HttpRequest, HttpResponse
-
-AnyUser = Union[AnonymousUser, settings.AUTH_USER_MODEL]
-
-HttpCallable = Callable[[HttpRequest], HttpResponse]
