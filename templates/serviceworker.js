bdcda47e9d8158dded733900f1934b62a4a8e899
Dan Jacob <danjac2018@gmail.com>
2021-03-23 23:21:54 +0200

    SW fixes

--- templates/serviceworker.js
+++ templates/serviceworker.js
@@ -19,8 +19,9 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open(cacheName).then(function(cache) {
       return cache.match(event.request).then(function (response) {
-        return response || fetch(new Request(event.request.url, {mode: 'no-cors'})).then(function(response) {
-          cache.put(event.request, response.clone());
+        const request = new Request(event.request.url, {mode: 'no-cors'})
+        return response || fetch(request).then(function(response) {
+          cache.put(event.request, response);
           return response;
         });
       });
