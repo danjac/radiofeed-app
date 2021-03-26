978049e2a7823137f93234ab3a6a6a7ec5bc9f81
Dan Jacob <danjac2018@gmail.com>
2021-03-26 14:44:48 +0200

    More admin optimizations

--- radiofeed/episodes/admin.py
+++ radiofeed/episodes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.db import connection
+from django.db import connections
+from django.template.defaultfilters import truncatechars
 
 from .models import Episode
 
@@ -9,12 +10,12 @@ class CountProxy:
 
     def __init__(self, query):
         self.query = query
+        self.db = self.query.db
 
     def __call__(self):
         query = self.query._query
         if not (query.group_by or query.where or query.distinct):
-            # cursor = connections[self.db].cursor()
-            cursor = connection.cursor()
+            cursor = connections[self.db].cursor()
             cursor.execute(
                 "SELECT reltuples FROM pg_class WHERE relname = %s",
                 [self.query.model._meta.db_table],
@@ -30,14 +31,24 @@ class CountProxy:
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
-    ordering = ("-id",)
-    list_display = ("__str__", "podcast", "pub_date")
+    ordering = ("-pub_date",)
+    list_display = ("episode_title", "podcast_title", "pub_date")
     raw_id_fields = ("podcast",)
     search_fields = ("search_document",)
-    # show_full_result_count = False
+    show_full_result_count = False
+
+    def episode_title(self, obj):
+        return truncatechars(obj.title, 30)
+
+    episode_title.short_description = "Title"
+
+    def podcast_title(self, obj):
+        return truncatechars(obj.title, 30)
+
+    podcast_title.short_description = "Podcast"
 
     def get_queryset(self, request):
-        qs = super().get_queryset(request)
+        qs = super().get_queryset(request).select_related("podcast")
         qs.count = CountProxy(qs)
         return qs
 
