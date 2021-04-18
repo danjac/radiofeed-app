91bdd8c30ca8b36491100b30970e7cd43c344fd2
Dan Jacob <danjac2018@gmail.com>
2021-04-18 15:48:58 +0300

    Add clear sessions task

--- audiotrails/config/celery_app.py
+++ audiotrails/config/celery_app.py
@@ -1,7 +1,5 @@
-# Standard Library
 import os
 
-# Third Party Libraries
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "audiotrails.config.settings.local")
@@ -14,3 +12,10 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print(f"Request: {self.request}")
+
+
+@app.task(bind=True)
+def clear_sessions(self):
+    from django.core.management import call_command  # noqa
+
+    call_command("clearsessions")
