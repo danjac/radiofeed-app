698aa2374f41316912666f8c73cfb7cb9a138b26
Dan Jacob <danjac2018@gmail.com>
2021-04-23 20:24:38 +0300

    Build script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o nounset
 
+# build and deploy image to Docker Hub
+
 podman build -t docker.io/danjac2018/audiotrails:latest .
 podman login docker.io
 podman push docker.io/danjac2018/audiotrails:latest
