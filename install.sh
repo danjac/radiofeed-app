f742c477be63b4ba7070caa3abe5284bc5a84433
Dan Jacob <danjac2018@gmail.com>
2021-04-23 23:10:34 +0300

    Tidy up install

--- install.sh
+++ install.sh
@@ -9,9 +9,13 @@ podman build -t $IMAGE .
 
 podman pod create --name audiopod -p 8000:8000 -p 8025:8025 -p 1025 -p 5432 -p 6379
 
+# postgres
 mkdir -p ${PWD}/db
 
-podman run --name postgresql --pod audiopod -e POSTGRES_PASSWORD=postgres -d -v "${PWD}/db:/var/lib/postgresql/data:z" postgres:11.8
+podman run --name postgresql \
+    --pod audiopod \
+    -e POSTGRES_PASSWORD=postgres \
+    -d -v "${PWD}/db:/var/lib/postgresql/data:z" postgres:11.8
 
 # redis
 podman run --name redis --pod audiopod -d redis
