af05db5b338252c13cbc9af4bdda0a5b6050831b
Dan Jacob <danjac2018@gmail.com>
2021-04-24 01:31:47 +0300

    Break up episodes views into pkg

--- audiotrails/episodes/urls.py
+++ audiotrails/episodes/urls.py
@@ -1,66 +1,75 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    episode_detail,
+    favorites,
+    history,
+    index,
+    player,
+    preview,
+    queue,
+    search_episodes,
+)
 
 app_name = "episodes"
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("search/episodes/", views.search_episodes, name="search_episodes"),
+    path("", index, name="index"),
+    path("search/episodes/", search_episodes, name="search_episodes"),
     path(
         "episodes/preview/<int:episode_id>/",
-        views.preview,
+        preview,
         name="episode_preview",
     ),
     path(
         "episodes/<int:episode_id>/<slug:slug>/",
-        views.episode_detail,
+        episode_detail,
         name="episode_detail",
     ),
     path(
         "player/<int:episode_id>/~start/",
-        views.start_player,
+        player.start_player,
         name="start_player",
     ),
     path(
         "player/~close/",
-        views.close_player,
+        player.close_player,
         name="close_player",
     ),
     path(
         "player/~next/",
-        views.play_next_episode,
+        player.play_next_episode,
         name="play_next_episode",
     ),
     path(
         "player/~timeupdate/",
-        views.player_update_current_time,
+        player.player_update_current_time,
         name="player_update_current_time",
     ),
-    path("history/", views.history, name="history"),
+    path("history/", history.index, name="history"),
     path(
         "history/<int:episode_id>/~remove/",
-        views.remove_audio_log,
+        history.remove_audio_log,
         name="remove_audio_log",
     ),
-    path("favorites/", views.favorites, name="favorites"),
+    path("favorites/", favorites.index, name="favorites"),
     path(
         "favorites/<int:episode_id>/~add/",
-        views.add_favorite,
+        favorites.add_favorite,
         name="add_favorite",
     ),
     path(
         "favorites/<int:episode_id>/~remove/",
-        views.remove_favorite,
+        favorites.remove_favorite,
         name="remove_favorite",
     ),
-    path("queue/", views.queue, name="queue"),
-    path("queue/~move/", views.move_queue_items, name="move_queue_items"),
-    path("queue/<int:episode_id>/~add/", views.add_to_queue, name="add_to_queue"),
+    path("queue/", queue.index, name="queue"),
+    path("queue/~move/", queue.move_queue_items, name="move_queue_items"),
+    path("queue/<int:episode_id>/~add/", queue.add_to_queue, name="add_to_queue"),
     path(
         "queue/<int:episode_id>/~remove/",
-        views.remove_from_queue,
+        queue.remove_from_queue,
         name="remove_from_queue",
     ),
 ]
