d3b5a547ac8906a6f119e76d99b4b27485911840
Dan Jacob <danjac2018@gmail.com>
2021-04-23 17:14:53 +0300

    Change image names

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o nounset
 
+buildah bud -f django.dockerfile -t audiotrails.dev/django .
+buildah bud -f assets.dockerfile -t audiotrails.dev/assets .
+
 podman pod create --name audiopod -p 8000:8000 -p 5432 -p 6379 -p 8025
 
 mkdir -p ${PWD}/db
@@ -20,30 +23,30 @@ podman run --name webapp \
     --pod audiopod \
     --env-file=.env \
     -v "${PWD}:/app/:z" \
-    -d danjac.dev/django /start-django
+    -d audiotrails.dev/django /start-django
 
 # celeryworker
 podman run --name celeryworker \
     --pod audiopod \
     --env-file=.env \
     -v "${PWD}:/app/:z" \
-    -d danjac.dev/django /start-celeryworker
+    -d audiotrails.dev/django /start-celeryworker
 
 # celerybeat
 podman run --name celerybeat \
     --pod audiopod \
     --env-file=.env \
     -v "${PWD}:/app/:z" \
-    -d danjac.dev/django /start-celerybeat
+    -d audiotrails.dev/django /start-celerybeat
 
 # watch js
 podman run --name watchjs \
     --pod audiopod \
     -v "${PWD}:/app/:z" \
-    -d danjac.dev/assets /start-watchjs
+    -d audiotrails.dev/assets /start-watchjs
 
 # watch css
 podman run --name watchcss \
     --pod audiopod \
     -v "${PWD}:/app/:z" \
-    -d danjac.dev/assets /start-watchcss
+    -d audiotrails.dev/assets /start-watchcss
