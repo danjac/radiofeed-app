5138cc74299e4b6cfcbb56f0e96d7df48d1b689b
Dan Jacob <danjac2018@gmail.com>
2021-04-24 14:16:45 +0300

    Use  variable

--- install.sh
+++ install.sh
@@ -33,37 +33,39 @@ podman run --name redis --pod $POD -d redis
 echo "starting mailhog: runs test mail server on http://localhost:8025"
 podman run --name mailhog --pod $POD -d mailhog/mailhog:v1.0.0
 
+VOLUME="${PWD}:/app/:z"
+
 echo "starting webapp: runs Django development server on http://localhost:8000"
 podman run --name webapp \
     --pod $POD \
     --env-file=.env \
-    -v "${PWD}:/app/:z" \
+    -v $VOLUME \
     -d $IMAGE /start-django
 
 echo "starting celeryworker: runs celery process"
 podman run --name celeryworker \
     --pod $POD \
     --env-file=.env \
-    -v "${PWD}:/app/:z" \
+    -v $VOLUME \
     -d $IMAGE /start-celeryworker
 
 echo "starting watchjs: runs esbuild process"
 podman run --name watchjs \
     --pod $POD \
     --env-file=.env \
-    -v "${PWD}:/app/:z" \
+    -v $VOLUME \
     -d $IMAGE /start-watchjs
 
 echo "starting watchcss: runs Tailwind JIT process"
 podman run --name watchcss \
     --pod $POD \
     --env-file=.env \
-    -v "${PWD}:/app/:z" \
+    -v $VOLUME \
     -d $IMAGE /start-watchcss
 
 echo "creating celerybeat: to start run 'podman start celerybeat'"
 podman create --name celerybeat \
     --pod $POD \
     --env-file=.env \
-    -v "${PWD}:/app/:z" \
+    -v $VOLUME \
     $IMAGE /start-celerybeat
