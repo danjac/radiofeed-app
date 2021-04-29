59de245a9fe91224a31c661de34cd37226aeb843
Dan Jacob <danjac2018@gmail.com>
2021-04-30 01:59:36 +0300

    Revert "Add comment"
    This reverts commit 12c0125c74b644b0871f5cd86dbc5aa99a58ec4a.

--- audiotrails/podcasts/rss_parser/models.py
+++ audiotrails/podcasts/rss_parser/models.py
@@ -142,7 +142,6 @@ class Feed(BaseModel):
         podcast.save()
 
         if new_cover_image and podcast.cover_image:
-            # pre-cache thumbnail if available
             podcast.get_cover_image_thumbnail()
         podcast.categories.set(categories)
 
