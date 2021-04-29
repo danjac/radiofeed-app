54620c0b45d0c13fc87969b49220eda076d31e6a
Dan Jacob <danjac2018@gmail.com>
2021-04-29 20:18:38 +0300

    Add check-updates

--- static/src/js/controllers/modal-controller.js
+++ static/src/js/controllers/modal-controller.js
@@ -1,17 +1,22 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['close', 'modal'];
+  static targets = ['close', 'content'];
 
   close() {
     // empty content of frame and reset src
-    this.modalTarget.textContent = '';
-    this.modalTarget.removeAttribute('src');
+    this.element.textContent = '';
+    this.element.removeAttribute('src');
   }
 
   closeOnEsc(event) {
     if (event.code === 'Escape') {
       this.close();
     }
+  }
+
+  clickOutside(event) {
+    event.preventDefault();
+    this.close();
   }
 }
