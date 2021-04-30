f73e7ea0151301f7986b1dc9fe16fe6f351b5396
Dan Jacob <danjac2018@gmail.com>
2021-04-30 18:43:20 +0300

    User tests

--- audiotrails/users/tests/test_models.py
+++ audiotrails/users/tests/test_models.py
@@ -1,62 +1,60 @@
-# Third Party Libraries
-import pytest
-
 from allauth.account.models import EmailAddress
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 
-# Local
 from ..factories import UserFactory
 
-pytestmark = pytest.mark.django_db
+User = get_user_model()
 
 
-class TestUserManager:
-    def test_create_user(self, user_model):
+class UserManagerTests(TestCase):
+    def test_create_user(self):
 
-        user = user_model.objects.create_user(
+        user = User.objects.create_user(
             username="tester", email="tester@gmail.com", password="t3ZtP4s31"
         )
-        assert user.check_password("t3ZtP4s31")
+        self.assertTrue(user.check_password("t3ZtP4s31"))
 
-    def test_create_superuser(self, user_model):
+    def test_create_superuser(self):
 
-        user = user_model.objects.create_superuser(
+        user = User.objects.create_superuser(
             username="tester", email="tester@gmail.com", password="t3ZtP4s31"
         )
-        assert user.is_superuser
-        assert user.is_staff
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
 
-    def test_for_email_matching_email_field(self, user_model):
+    def test_for_email_matching_email_field(self):
 
         user = UserFactory(email="test@gmail.com")
-        assert user_model.objects.for_email("test@gmail.com").first() == user
+        self.assertEqual(User.objects.for_email("test@gmail.com").first(), user)
 
-    def test_for_email_matching_email_address_instance(self, user_model):
+    def test_for_email_matching_email_address_instance(self):
 
         user = UserFactory()
         EmailAddress.objects.create(user=user, email="test@gmail.com")
-        assert user_model.objects.for_email("test@gmail.com").first() == user
+        self.assertEqual(User.objects.for_email("test@gmail.com").first(), user)
 
-    def test_matches_usernames(self, user_model):
+    def test_matches_usernames(self):
         user_1 = UserFactory(username="first")
         user_2 = UserFactory(username="second")
         user_3 = UserFactory(username="third")
 
         names = ["second", "FIRST", "SEconD"]  # duplicate
 
-        users = user_model.objects.matches_usernames(names)
-        assert len(users) == 2
-        assert user_1 in users
-        assert user_2 in users
-        assert user_3 not in users
+        users = User.objects.matches_usernames(names)
+        self.assertEqual(len(users), 2)
+        self.assertTrue(user_1 in users)
+        self.assertTrue(user_2 in users)
+        self.assertFalse(user_3 in users)
 
         # check empty set returns no results
-        assert user_model.objects.matches_usernames([]).count() == 0
+        self.assertEqual(User.objects.matches_usernames([]).count(), 0)
 
 
-class TestUserModel:
-    def test_get_email_addresses(self, user):
-
+class UserModelTests(TestCase):
+    def test_get_email_addresses(self):
+        user = UserFactory()
         user.emailaddress_set.create(email="test1@gmail.com")
         emails = user.get_email_addresses()
-        assert user.email in emails
-        assert "test1@gmail.com" in emails
+        self.assertTrue(user.email in emails)
+        self.assertTrue("test1@gmail.com" in emails)
