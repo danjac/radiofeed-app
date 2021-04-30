1772c1417dc47e62d0d663aab250f7d294eadc2f
Dan Jacob <danjac2018@gmail.com>
2021-04-30 15:55:56 +0300

    Podcast tests

--- audiotrails/podcasts/tests/test_tasks.py
+++ audiotrails/podcasts/tests/test_tasks.py
@@ -1,7 +1,8 @@
 import datetime
 
-import pytest
+from unittest.mock import patch
 
+from django.test import TestCase
 from django.utils import timezone
 
 from audiotrails.users.factories import UserFactory
@@ -10,66 +11,68 @@ from .. import tasks
 from ..factories import PodcastFactory
 from ..rss_parser import RssParserError
 
-pytestmark = pytest.mark.django_db
 
-
-@pytest.fixture
-def mock_send_email(mocker):
-    return mocker.patch(
+class SendRecommendationEmailsTests(TestCase):
+    @patch(
         "audiotrails.podcasts.tasks.send_recommendations_email",
         autospec=True,
     )
-
-
-@pytest.fixture
-def mock_sync_podcast_feed(mocker):
-    return mocker.patch(
-        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
-        autospec=True,
-    )
-
-
-@pytest.fixture
-def mock_parse_rss(mocker):
-    return mocker.patch(
-        "audiotrails.podcasts.tasks.parse_rss",
-        autospec=True,
-    )
-
-
-class TestSendRecommendationEmails:
     def test_send_if_user_inactive(self, mock_send_email):
         UserFactory(is_active=False, send_recommendations_email=True)
         tasks.send_recommendation_emails()
         mock_send_email.assert_not_called()
 
+    @patch(
+        "audiotrails.podcasts.tasks.send_recommendations_email",
+        autospec=True,
+    )
     def test_send_if_user_emails_off(self, mock_send_email):
         UserFactory(is_active=True, send_recommendations_email=False)
         tasks.send_recommendation_emails()
         mock_send_email.assert_not_called()
 
+    @patch(
+        "audiotrails.podcasts.tasks.send_recommendations_email",
+        autospec=True,
+    )
     def test_send_if_user_emails_on(self, mock_send_email):
         UserFactory(is_active=True, send_recommendations_email=True)
         tasks.send_recommendation_emails()
         mock_send_email.assert_called()
 
 
-class TestSyncPodcastFeeds:
+class SyncPodcastFeedsTests(TestCase):
+    @patch(
+        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
+        autospec=True,
+    )
     def test_podcast_has_too_many_retries(self, mock_sync_podcast_feed):
         PodcastFactory(num_retries=3)
         tasks.sync_podcast_feeds()
         mock_sync_podcast_feed.assert_not_called()
 
+    @patch(
+        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
+        autospec=True,
+    )
     def test_podcast_just_updated(self, mock_sync_podcast_feed):
         PodcastFactory(last_updated=timezone.now())
         tasks.sync_podcast_feeds()
         mock_sync_podcast_feed.assert_not_called()
 
+    @patch(
+        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
+        autospec=True,
+    )
     def test_podcast_never_updated(self, mock_sync_podcast_feed):
         PodcastFactory(last_updated=None)
         tasks.sync_podcast_feeds()
         mock_sync_podcast_feed.assert_called()
 
+    @patch(
+        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
+        autospec=True,
+    )
     def test_podcast_updated_more_than_12_hours_ago(self, mock_sync_podcast_feed):
         PodcastFactory(last_updated=timezone.now() - datetime.timedelta(hours=24))
         tasks.sync_podcast_feeds()
@@ -77,14 +80,26 @@ class TestSyncPodcastFeeds:
 
 
 class TestSyncPodcastFeed:
+    @patch(
+        "audiotrails.podcasts.tasks.parse_rss",
+        autospec=True,
+    )
     def test_no_podcast_found(self, mock_parse_rss):
         tasks.sync_podcast_feed(12345)
         mock_parse_rss.assert_not_called()
 
+    @patch(
+        "audiotrails.podcasts.tasks.parse_rss",
+        autospec=True,
+    )
     def test_parser_exception(self, mock_parse_rss, podcast):
         mock_parse_rss.side_effect = RssParserError("Boom")
         tasks.sync_podcast_feed(podcast.rss)
 
+    @patch(
+        "audiotrails.podcasts.tasks.parse_rss",
+        autospec=True,
+    )
     def test_ok(self, mock_parse_rss, podcast):
         tasks.sync_podcast_feed(podcast.rss)
         mock_parse_rss.assert_called()
