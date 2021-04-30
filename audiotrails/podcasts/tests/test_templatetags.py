f6641c41449810a7f68269909b6037539c05259d
Dan Jacob <danjac2018@gmail.com>
2021-04-30 15:50:35 +0300

    Podcast tests

--- audiotrails/podcasts/tests/test_templatetags.py
+++ audiotrails/podcasts/tests/test_templatetags.py
@@ -1,28 +1,29 @@
-import pytest
+from django.test import TestCase
 
 from ..factories import PodcastFactory
 from ..models import _cover_image_placeholder
 from ..templatetags.podcasts import cover_image
 
-pytestmark = pytest.mark.django_db
 
+class CoverImageTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.podcast = PodcastFactory()
 
-class TestCoverImage:
-    def test_lazy_cover_image_provided(self, podcast):
-        dct = cover_image(podcast, cover_image=_cover_image_placeholder, lazy=True)
-        assert dct["cover_image"] is not None
-        assert dct["podcast"] == podcast
-        assert dct["lazy"] is False
+    def test_lazy_cover_image_provided(self):
+        dct = cover_image(self.podcast, cover_image=_cover_image_placeholder, lazy=True)
+        self.assertTrue(dct["cover_image"])
+        self.assertFalse(dct["lazy"])
+        self.assertEqual(dct["podcast"], self.podcast)
 
-    def test_lazy_cover_image_not_provided(self, podcast):
-        dct = cover_image(podcast, cover_image=None, lazy=True)
-        assert dct["cover_image"] is None
-        assert dct["podcast"] == podcast
-        assert dct["lazy"] is True
+    def test_lazy_cover_image_not_provided(self):
+        dct = cover_image(self.podcast, cover_image=None, lazy=True)
+        self.assertFalse(dct["cover_image"])
+        self.assertTrue(dct["lazy"])
+        self.assertEqual(dct["podcast"], self.podcast)
 
     def test_not_lazy(self):
-        podcast = PodcastFactory(cover_image="test.jpg")
-        dct = cover_image(podcast, cover_image=None, lazy=False)
-        assert dct["cover_image"] is not None
-        assert dct["podcast"] == podcast
-        assert dct["lazy"] is False
+        dct = cover_image(self.podcast, cover_image=None, lazy=False)
+        self.assertTrue(dct["cover_image"])
+        self.assertFalse(dct["lazy"])
+        self.assertEqual(dct["podcast"], self.podcast)
