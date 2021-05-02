af33b2f7721a4b0594b8f00e19bbebff217a2feb
Dan Jacob <danjac2018@gmail.com>
2021-05-03 02:36:01 +0300

    Other player events

---
+++ static/src/js/utils.js
@@ -0,0 +1,32 @@
+export const dispatch = (el, event, detail = {}) => {
+  console.log('dispatch', event, detail);
+  el.dispatchEvent(
+    new CustomEvent(event, {
+      detail,
+      bubbles: true,
+    })
+  );
+};
+
+export const formatDuration = (value) => {
+  if (isNaN(value) || value < 0) return '00:00:00';
+  const duration = Math.floor(value);
+
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = Math.floor(duration % 60);
+
+  return [hours, minutes, seconds].map((t) => t.toString().padStart(2, '0')).join(':');
+};
+
+export const percent = (nominator, denominator) => {
+  if (!denominator || !nominator) {
+    return 0;
+  }
+
+  if (denominator > nominator) {
+    return 100;
+  }
+
+  return (denominator / nominator) * 100;
+};
