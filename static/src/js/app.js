a183e72d0a2cc4d83160c1482530b029010bf428
Dan Jacob <danjac2018@gmail.com>
2021-05-02 11:36:09 +0300

    extension

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,2 +1,17 @@
 import 'htmx.org';
 import 'alpinejs';
+
+const maybeIntersect = (elt) => {
+  console.log('interect goes here');
+};
+
+const { htmx } = window;
+
+htmx.defineExtension('intersect', {
+  onEvent(name, event) {
+    if (name === 'htmx:afterProcessNode') {
+      const { elt } = event.detail;
+      const observer = new IntersectionObserver((entries) => {});
+    }
+  },
+});
