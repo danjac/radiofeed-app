3aadff871bb3c52f4f685cab8e209c957bb8690a
Dan Jacob <danjac2018@gmail.com>
2021-05-02 20:55:38 +0300

    Refactoring player

--- static/src/js/player.js
+++ static/src/js/player.js
@@ -1,3 +1,26 @@
+const formatDuration = (value) => {
+  if (isNaN(value) || value < 0) return '00:00:00';
+  const duration = Math.floor(value);
+
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = Math.floor(duration % 60);
+
+  return [hours, minutes, seconds].map((t) => t.toString().padStart(2, '0')).join(':');
+};
+
+const calcPercentComplete = (duration, currentTime) => {
+  if (!currentTime || !duration) {
+    return 0;
+  }
+
+  if (currentTime > duration) {
+    return 100;
+  }
+
+  return (currentTime / duration) * 100;
+};
+
 export default function (htmx, options) {
   const { csrfToken, urls } = options;
 
@@ -18,26 +41,46 @@ export default function (htmx, options) {
     podcast: null,
     currentTime: 0,
     duration: 0,
+    audio: null,
     isPlaying: false,
     isPaused: false,
     isWaiting: false,
+    counter: '-00:00:00',
   };
 
   const playerInfoTag = document.getElementById('player-info');
 
   const instance = {
     ...defaults,
-    initialize() {},
+    initialize() {
+      if (this.episode) {
+        console.log('we have episode', this.episode);
+        this.startAudio();
+      }
+      this.$watch('episode', (value) => {
+        console.log('episode???', value);
+        this.stopAudio();
+        if (value) {
+          this.startAudio();
+        }
+      });
+      this.$watch('duration', (value) => {
+        this.updateProgressBar(value, this.currentTime);
+      });
+      this.$watch('currentTime', (value) => {
+        this.updateProgressBar(this.duration, value);
+      });
+    },
     openPlayer(event) {
-      const { episode, podcast, currentTime } = event.detail;
-      this.episode = episode;
-      this.podcast = podcast;
-      this.currentTime = currentTime;
+      if (event) {
+        const { episode, podcast, currentTime } = event.detail;
+        this.episode = episode;
+        this.podcast = podcast;
+        this.currentTime = currentTime;
+      }
       this.$nextTick(() => {
-        this.$refs.audio.play();
-        // hook up any htmx events
+        // re-hook up any htmx events
         htmx.process(this.$el);
-        // set up watchers
       });
     },
     closePlayer() {
@@ -48,11 +91,11 @@ export default function (htmx, options) {
     },
     // audio events
     loaded() {
-      this.duration = this.$refs.audio.duration;
+      this.duration = this.audio.duration;
     },
     timeUpdate() {
-      if (this.$refs.audio) {
-        this.currentTime = this.$refs.audio.currentTime;
+      if (this.audio) {
+        this.currentTime = this.audio.currentTime;
         /*
           const body = new FormData();
           body.append('current_time', this.currentTime);
@@ -64,23 +107,85 @@ export default function (htmx, options) {
     },
     resumed() {
       console.log('resumed');
-      //this.isPlaying = true;
-      //this.isPaused = false;
-      //this.isWaiting = false;
+      this.isPlaying = true;
+      this.isPaused = false;
+      this.isWaiting = false;
     },
     paused() {
       console.log('paused');
-      //this.isPlaying = false;
-      //this.isPaused = true;
-      //this.isWaiting = false;
+      this.isPlaying = false;
+      this.isPaused = true;
+      this.isWaiting = false;
     },
     waiting() {
       console.log('waiting');
-      //this.isWaiting = true;
+      this.isWaiting = true;
     },
     active() {
       console.log('active');
-      //this.isWaiting = false;
+      this.isWaiting = false;
+    },
+    ended() {
+      console.log('ended');
+      this.$dispatch('close-player');
+    },
+    startAudio() {
+      this.audio = new Audio(this.episode.mediaUrl);
+      this.audio.currentTime = this.currentTime;
+      const events = this.getAudioEvents();
+      Object.keys(events).forEach((name) => {
+        this.audio.addEventListener(name, events[name].bind(this));
+      });
+      this.audio.play().catch(() => this.audio.pause());
+    },
+    stopAudio() {
+      if (this.audio) {
+        this.audio.pause();
+        const events = this.getAudioEvents();
+        Object.keys(events).forEach((name) => {
+          this.audio.removeEventListener(name, events[name].bind(this));
+        });
+        this.audio = null;
+      }
+    },
+    getAudioEvents() {
+      return {
+        play: this.resumed,
+        paused: this.paused,
+        error: this.waiting,
+        suspend: this.waiting,
+        stalled: this.waiting,
+        canplaythrough: this.active,
+        canplay: this.active,
+        playing: this.active,
+        loadedmetadata: this.loaded,
+        ended: this.ended,
+        timeupdate: this.timeUpdate,
+      };
+    },
+    updateProgressBar(duration, currentTime) {
+      this.counter = '-' + formatDuration(duration - currentTime);
+      const pcComplete = calcPercentComplete(duration, currentTime);
+      this.$refs.indicator.style.left =
+        this.calcCurrentIndicatorPosition(pcComplete) + 'px';
+    },
+    calcCurrentIndicatorPosition(pcComplete) {
+      const clientWidth = this.$refs.progressBar.clientWidth;
+      let currentPosition, width;
+
+      currentPosition = this.$refs.progressBar.getBoundingClientRect().left - 24;
+
+      if (clientWidth === 0) {
+        width = 0;
+      } else {
+        // min 1rem to accomodate indicator
+        const minWidth = (16 / clientWidth) * 100;
+        width = pcComplete > minWidth ? pcComplete : minWidth;
+      }
+      if (width) {
+        currentPosition += clientWidth * (width / 100);
+      }
+      return currentPosition;
     },
   };
 
@@ -90,5 +195,6 @@ export default function (htmx, options) {
       ...JSON.parse(playerInfoTag.textContent),
     };
   }
+
   return instance;
 }
