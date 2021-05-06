54a166f214d8f8ca3f36c7b2db8659f44e23d636
Dan Jacob <danjac2018@gmail.com>
2021-05-06 13:38:27 +0300

    Enabling whitenoise

--- audiotrails/config/settings/mixins/aws.py
+++ audiotrails/config/settings/mixins/aws.py
@@ -1,15 +1,18 @@
 from ..base import env
 
 DEFAULT_FILE_STORAGE = "audiotrails.config.storages.MediaStorage"
-STATICFILES_STORAGE = "audiotrails.config.storages.StaticStorage"
 
-AWS_MEDIA_LOCATION = "media"
-AWS_STATIC_LOCATION = "static"
+AWS_MEDIA_LOCATION = env("AWS_MEDIA_LOCATION", default="media")
+AWS_STATIC_LOCATION = env("AWS_STATIC_LOCATION", default="static")
+
+AWS_CLOUDFRONT_MEDIA_DOMAIN = AWS_S3_CUSTOM_DOMAIN = env(
+    "AWS_CLOUDFRONT_MEDIA_DOMAIN", default=None
+)
+AWS_CLOUDFRONT_STATIC_DOMAIN = env("AWS_CLOUDFRONT_STATIC_DOMAIN", default=None)
 
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default=None)
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default=None)
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default=None)
-AWS_S3_CUSTOM_DOMAIN = env("AWS_S3_CUSTOM_DOMAIN", default=None)
 AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="eu-north-1")
 
 AWS_QUERYSTRING_AUTH = False
