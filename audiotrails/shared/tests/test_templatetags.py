a0b962983dd919b8cd1b3318b94faa722a55520f
Dan Jacob <danjac2018@gmail.com>
2021-05-08 23:42:08 +0300

    Test fixes

--- audiotrails/shared/tests/test_templatetags.py
+++ audiotrails/shared/tests/test_templatetags.py
@@ -159,8 +159,10 @@ class HxLinkTests(SimpleTestCase):
     def test_with_model(self):
         c = Category(id=1000, name="Test Category")
         url = c.get_absolute_url()
-        self.assertEqual(hx_link(c), f'href="{url}" hx-get="{url}"')
+        self.assertEqual(hx_link(c), f'href="{url}" hx-get="{url}" hx-push-url="true"')
 
     def test_with_simple_url(self):
         url = "/discover/"
-        self.assertEqual(hx_link(url), f'href="{url}" hx-get="{url}"')
+        self.assertEqual(
+            hx_link(url), f'href="{url}" hx-get="{url}" hx-push-url="true"'
+        )
