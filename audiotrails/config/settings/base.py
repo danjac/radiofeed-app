5f1bba7128040f0b2a28107b767103708e60dc30
Dan Jacob <danjac2018@gmail.com>
2021-05-09 03:07:37 +0300

    Removing whitenoise

--- audiotrails/config/settings/base.py
+++ audiotrails/config/settings/base.py
@@ -86,7 +86,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django_permissions_policy.PermissionsPolicyMiddleware",
     "django.contrib.sites.middleware.CurrentSiteMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
