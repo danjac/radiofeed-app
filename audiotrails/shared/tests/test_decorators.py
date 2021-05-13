89d8c4af66e4638272a9853f949bdc692e8bab1f
Dan Jacob <danjac2018@gmail.com>
2021-05-13 13:04:02 +0300

    JSON parser decorator

--- audiotrails/shared/tests/test_decorators.py
+++ audiotrails/shared/tests/test_decorators.py
@@ -1,4 +1,5 @@
 import http
+import json
 
 from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import PermissionDenied
@@ -7,11 +8,16 @@ from django.test import RequestFactory, TestCase
 
 from audiotrails.users.factories import UserFactory
 
-from ..decorators import ajax_login_required
+from ..decorators import accepts_json, ajax_login_required
 
 
 @ajax_login_required
-def my_view(request):
+def my_ajax_view(request):
+    return HttpResponse()
+
+
+@accepts_json
+def my_json_view(request):
     return HttpResponse()
 
 
@@ -22,9 +28,33 @@ class AjaxLoginRequiredTests(TestCase):
     def test_anonymous(self):
         req = self.rf.get("/")
         req.user = AnonymousUser()
-        self.assertRaises(PermissionDenied, my_view, req)
+        self.assertRaises(PermissionDenied, my_ajax_view, req)
 
     def test_authenticated(self):
         req = self.rf.get("/")
         req.user = UserFactory()
-        self.assertEqual(my_view(req).status_code, http.HTTPStatus.OK)
+        self.assertEqual(my_ajax_view(req).status_code, http.HTTPStatus.OK)
+
+
+class AcceptJsonTests(TestCase):
+    def setUp(self):
+        self.rf = RequestFactory()
+
+    def test_invalid_content_type(self):
+        req = self.rf.post(
+            "/", content_type="text/html", data=json.dumps({"testing": "ok"})
+        )
+        self.assertEqual(my_json_view(req).status_code, http.HTTPStatus.BAD_REQUEST)
+        self.assertFalse(hasattr(req, "json"))
+
+    def test_invalid_json(self):
+        req = self.rf.post("/", content_type="application/json", data="testing")
+        self.assertEqual(my_json_view(req).status_code, http.HTTPStatus.BAD_REQUEST)
+        self.assertFalse(hasattr(req, "json"))
+
+    def test_valid_json(self):
+        req = self.rf.post(
+            "/", content_type="application/json", data=json.dumps({"testing": "ok"})
+        )
+        self.assertEqual(my_json_view(req).status_code, http.HTTPStatus.OK)
+        self.assertEqual(req.json, {"testing": "ok"})
