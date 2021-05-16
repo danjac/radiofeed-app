b1d8f29172d3ab9f43edff95bd34125d51d51952
Dan Jacob <danjac2018@gmail.com>
2021-05-17 00:30:03 +0300

    Type hints

--- audiotrails/users/models.py
+++ audiotrails/users/models.py
@@ -1,38 +1,31 @@
+from __future__ import annotations
+
+from typing import Iterable, Optional
+
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
 class UserQuerySet(models.QuerySet):
-    def for_email(self, email):
+    def for_email(self, email: str) -> models.QuerySet:
         """Returns users matching this email address, including both
         primary and secondary email addresses
-
-        Args:
-            email (str): email address
-
-        Returns:
-            QuerySet
         """
         return self.filter(
             models.Q(emailaddress__email__iexact=email) | models.Q(email__iexact=email)
         )
 
-    def matches_usernames(self, names):
-        """Returns users matching the (case insensitive) username.
-
-        Args:
-            names (list): list of usernames
-
-        Returns:
-            QuerySet
-        """
+    def matches_usernames(self, names: Iterable[str]) -> models.QuerySet:
+        """Returns users matching the (case insensitive) username."""
         if not names:
             return self.none()
         return self.filter(username__iregex=r"^(%s)+" % "|".join(names))
 
 
 class UserManager(BaseUserManager.from_queryset(UserQuerySet)):  # type: ignore
-    def create_user(self, username, email, password=None, **kwargs):
+    def create_user(
+        self, username: str, email: str, password: Optional[str] = None, **kwargs
+    ) -> User:
         user = self.model(
             username=username, email=self.normalize_email(email), **kwargs
         )
@@ -40,7 +33,9 @@ class UserManager(BaseUserManager.from_queryset(UserQuerySet)):  # type: ignore
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password, **kwargs):
+    def create_superuser(
+        self, username: str, email: str, password: str, **kwargs
+    ) -> User:
         return self.create_user(
             username,
             email,
@@ -52,11 +47,11 @@ class UserManager(BaseUserManager.from_queryset(UserQuerySet)):  # type: ignore
 
 
 class User(AbstractUser):
-    send_recommendations_email = models.BooleanField(default=True)
+    send_recommendations_email: bool = models.BooleanField(default=True)
 
     objects = UserManager()
 
-    def get_email_addresses(self):
+    def get_email_addresses(self) -> set[str]:
         """Get set of emails belonging to user.
 
         Returns:
