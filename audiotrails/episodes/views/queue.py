05d59cb55b6e501665f166beb1f582176b5c9dea
Dan Jacob <danjac2018@gmail.com>
2021-05-17 00:27:27 +0300

    Type hints

--- audiotrails/episodes/views/queue.py
+++ audiotrails/episodes/views/queue.py
@@ -3,18 +3,18 @@ import http
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.db.models import Max
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.template.response import TemplateResponse
 from django.views.decorators.http import require_POST
 
 from audiotrails.shared.decorators import accepts_json, ajax_login_required
 
-from ..models import QueueItem
+from ..models import Episode, QueueItem
 from . import get_episode_or_404
 
 
 @login_required
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return TemplateResponse(
         request,
         "episodes/queue.html",
@@ -28,7 +28,7 @@ def index(request):
 
 @require_POST
 @ajax_login_required
-def add_to_queue(request, episode_id):
+def add_to_queue(request: HttpRequest, episode_id: int) -> HttpResponse:
 
     episode = get_episode_or_404(request, episode_id, with_podcast=True)
 
@@ -48,7 +48,7 @@ def add_to_queue(request, episode_id):
 
 @require_POST
 @ajax_login_required
-def remove_from_queue(request, episode_id):
+def remove_from_queue(request: HttpRequest, episode_id: int) -> HttpResponse:
     episode = get_episode_or_404(request, episode_id)
     QueueItem.objects.filter(episode=episode, user=request.user).delete()
     return render_queue_toggle(request, episode, False)
@@ -57,7 +57,7 @@ def remove_from_queue(request, episode_id):
 @require_POST
 @accepts_json
 @ajax_login_required
-def move_queue_items(request):
+def move_queue_items(request: HttpRequest) -> HttpResponse:
 
     qs = QueueItem.objects.filter(user=request.user)
     items = qs.in_bulk()
@@ -75,7 +75,9 @@ def move_queue_items(request):
     return HttpResponse(status=http.HTTPStatus.NO_CONTENT)
 
 
-def render_queue_toggle(request, episode, is_queued):
+def render_queue_toggle(
+    request: HttpRequest, episode: Episode, is_queued: bool
+) -> HttpResponse:
     response = HttpResponse(status=http.HTTPStatus.NO_CONTENT)
     response["HX-Trigger"] = "reload-queue"
     return response
