0a2fcd51b7d1f1eb326409c76ae918d585643dce
Dan Jacob <danjac2018@gmail.com>
2021-05-17 22:12:07 +0300

    Type hints

--- audiotrails/podcasts/management/commands/crawl_itunes.py
+++ audiotrails/podcasts/management/commands/crawl_itunes.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 
 from audiotrails.podcasts import itunes
 
@@ -6,7 +6,7 @@ from audiotrails.podcasts import itunes
 class Command(BaseCommand):
     help = "Fetches top iTunes results from API for each category and adds new podcasts to database"
 
-    def add_arguments(self, parser):
+    def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
             "--limit",
             type=int,
@@ -14,8 +14,8 @@ class Command(BaseCommand):
             help="Max results from iTunes API",
         )
 
-    def handle(self, *args, **options):
-        new_podcasts = itunes.crawl_itunes(options["limit"])
+    def handle(self, **options: str) -> None:
+        new_podcasts = itunes.crawl_itunes(int(options["limit"]))
 
         if new_podcasts:
             self.stdout.write(self.style.SUCCESS(f"{new_podcasts} new podcast(s)"))
