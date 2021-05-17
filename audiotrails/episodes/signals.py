dba26e1b33983a4cbde45858f8ef59a8651692b9
Dan Jacob <danjac2018@gmail.com>
2021-05-17 16:35:34 +0300

    Play button changes

--- audiotrails/episodes/signals.py
+++ audiotrails/episodes/signals.py
@@ -1,8 +1,13 @@
+from typing import Any
+
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
+from django.http import HttpRequest
+
+from audiotrails.shared.types import AnyUser
 
 
-def close_player(sender, user, request, **kwargs):
+def close_player(sender: Any, user: AnyUser, request: HttpRequest, **kwargs) -> None:
     # removes current player from session
     request.session.pop("player", None)
 
--- audiotrails/podcasts/sitemaps.py
+++ audiotrails/podcasts/sitemaps.py
@@ -1,29 +1,30 @@
 import datetime
 
 from django.contrib.sitemaps import Sitemap
+from django.db import QuerySet
 from django.utils import timezone
 
 from .models import Category, Podcast
 
 
 class CategorySitemap(Sitemap):
-    changefreq = "never"
-    priority = 0.3
+    changefreq: str = "never"
+    priority: float = 0.3
 
-    def items(self):
+    def items(self) -> QuerySet:
         return Category.objects.order_by("name")
 
 
 class PodcastSitemap(Sitemap):
-    changefreq = "hourly"
-    priority = 0.5
-    limit = 100
+    changefreq: str = "hourly"
+    priority: float = 0.5
+    limit: int = 100
 
-    def items(self):
+    def items(self) -> QuerySet:
         return Podcast.objects.filter(
             pub_date__isnull=False,
             pub_date__gt=timezone.now() - datetime.timedelta(hours=24),
         ).order_by("-pub_date")
 
-    def lastmod(self, item):
+    def lastmod(self, item: Podcast) -> datetime.datetime:
         return item.pub_date
