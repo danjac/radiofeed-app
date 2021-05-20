fa4504b1df08834c6ec9b418594352752bd3be5b
Dan Jacob <danjac2018@gmail.com>
2021-05-20 23:07:46 +0300

    Remove redundant tag

--- audiotrails/episodes/templatetags/player.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag(takes_context=True)
-def is_playing(context, episode):
-    return context["request"].player.is_playing(episode)
