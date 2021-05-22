144b0516f22142838b60297a60bdd89449908e91
Dan Jacob <danjac2018@gmail.com>
2021-05-22 15:55:18 +0300

    Remove django-split-settings

---
+++ audiotrails/config/settings/mixins/permissions.py
@@ -0,0 +1,17 @@
+# https://pypi.org/project/django-permissions-policy/
+
+PERMISSIONS_POLICY: dict[str, list] = {
+    "accelerometer": [],
+    "ambient-light-sensor": [],
+    "camera": [],
+    "document-domain": [],
+    "encrypted-media": [],
+    "fullscreen": [],
+    "geolocation": [],
+    "gyroscope": [],
+    "interest-cohort": [],
+    "magnetometer": [],
+    "microphone": [],
+    "payment": [],
+    "usb": [],
+}
--- audiotrails/config/settings/production.py
+++ audiotrails/config/settings/production.py
@@ -3,28 +3,9 @@ from .base import BASE_DIR
 from .mixins.aws import *  # noqa
 from .mixins.aws import AWS_STATIC_CLOUDFRONT_DOMAIN, AWS_STATIC_LOCATION
 from .mixins.mailgun import *  # noqa
+from .mixins.permissions import *  # noqa
 from .mixins.secure import *  # noqa
 from .mixins.sentry import *  # noqa
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-# https://pypi.org/project/django-permissions-policy/
-
-PERMISSIONS_POLICY: dict[str, list] = {
-    "accelerometer": [],
-    "ambient-light-sensor": [],
-    "camera": [],
-    "document-domain": [],
-    "encrypted-media": [],
-    "fullscreen": [],
-    "geolocation": [],
-    "gyroscope": [],
-    "interest-cohort": [],
-    "magnetometer": [],
-    "microphone": [],
-    "payment": [],
-    "usb": [],
-}
 
 STATIC_URL = "https://" + AWS_STATIC_CLOUDFRONT_DOMAIN + "/" + AWS_STATIC_LOCATION + "/"
 
