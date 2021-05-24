3e260a80480b45d1e0d365a317e6f08a61a9e420
Dan Jacob <danjac2018@gmail.com>
2021-05-24 19:16:31 +0300

    Remove morphdom

---
+++ assets/js/lazyload.js
@@ -0,0 +1,25 @@
+export default function lazyLoadImages(elt) {
+  const lazyImages = [].slice.call(elt.querySelectorAll('img.lazy'));
+
+  if ('IntersectionObserver' in window) {
+    const lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          const { target } = entry;
+          target.src = target.dataset.src;
+          target.classList.remove('lazy');
+          lazyImageObserver.unobserve(target);
+        }
+      });
+    });
+
+    lazyImages.forEach(function (img) {
+      lazyImageObserver.observe(img);
+    });
+  } else {
+    lazyImages.forEach(function (img) {
+      img.src = img.dataset.src;
+      img.classList.remove('lazy');
+    });
+  }
+}
