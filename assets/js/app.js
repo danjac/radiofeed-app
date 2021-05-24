6ad55beb4c6e92b4977dce8586ea67353717712b
Dan Jacob <danjac2018@gmail.com>
2021-05-24 19:16:21 +0300

    Remove morphdom

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,9 @@
 import 'alpinejs';
 import 'htmx.org';
 
-import morphdom from 'morphdom';
-
 import dragDrop from './dragdrop';
+import lazyLoadImages from './lazyload';
 import Player from './player';
-
-export function lazyLoadImages(elt) {
-  const lazyImages = [].slice.call(elt.querySelectorAll('img.lazy'));
-
-  if ('IntersectionObserver' in window) {
-    const lazyImageObserver = new IntersectionObserver(function (entries, observer) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          const { target } = entry;
-          target.src = target.dataset.src;
-          target.classList.remove('lazy');
-          lazyImageObserver.unobserve(target);
-        }
-      });
-    });
-
-    lazyImages.forEach(function (img) {
-      lazyImageObserver.observe(img);
-    });
-  } else {
-    lazyImages.forEach(function (img) {
-      img.src = img.dataset.src;
-      img.classList.remove('lazy');
-    });
-  }
-}
 
 (function () {
   // events
@@ -43,24 +16,6 @@ export function lazyLoadImages(elt) {
 
   document.addEventListener('htmx:load', (event) => lazyLoadImages(event.detail.elt));
   document.addEventListener('DOMContentLoaded', () => lazyLoadImages(document));
-
-  window.htmx.defineExtension('morphdom-swap', {
-    isInlineSwap: function (swapStyle) {
-      return swapStyle === 'morphdom';
-    },
-
-    handleSwap: function (swapStyle, target, fragment) {
-      if (swapStyle === 'morphdom') {
-        if (fragment.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
-          morphdom(target, fragment.firstElementChild);
-          return [target];
-        } else {
-          morphdom(target, fragment.outerHTML);
-          return [target];
-        }
-      }
-    },
-  });
 
   // globals
   //
