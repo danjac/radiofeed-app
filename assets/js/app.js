683b62ec7197eea8d09416a2e37553c40f0aeda7
Dan Jacob <danjac2018@gmail.com>
2021-05-25 19:07:03 +0300

    Npm updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,30 +4,7 @@ import 'htmx.org';
 import dragDrop from './dragdrop';
 import Player from './player';
 
-function lazyLoadImages(elt) {
-  elt.querySelectorAll('img.lazy').forEach((img) => {
-    new IntersectionObserver((entries, observer) => {
-      if (entries[0].isIntersecting) {
-        window.htmx.trigger(img, 'lazyload');
-        observer.disconnect();
-      }
-    }).observe(img);
-  });
-}
-
 (function () {
-  document.addEventListener('DOMContentLoaded', () => lazyLoadImages(document));
-  document.addEventListener('htmx:load', (event) => lazyLoadImages(event.detail.elt));
-
-  document.addEventListener('htmx:afterSwap', (event) => {
-    // workaround for https://github.com/bigskysoftware/htmx/issues/456
-    if (event.target.id === 'content') {
-      window.scrollTo(0, event.target.offsetTop - 160);
-    }
-  });
-
-  // globals
-  //
   window.dragDrop = dragDrop;
   window.Player = Player;
 })();
