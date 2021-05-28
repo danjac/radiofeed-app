4bc3a1ae2171fdf37388d3ba85820f984e8aa3b6
Dan Jacob <danjac2018@gmail.com>
2021-05-28 12:47:16 +0300

    Split out feed_parser module

--- audiotrails/podcasts/feed_parser.py
+++ audiotrails/podcasts/feed_parser.py
@@ -24,6 +24,94 @@ class RssParserError(ValueError):
     ...
 
 
+@dataclasses.dataclass
+class Audio:
+
+    type: str
+    url: str
+    length: int | None = None
+    rel: str = ""
+
+    def __post_init__(self) -> None:
+        _validate_url(self.url)
+        _validate_audio_type(self.type)
+        _validate_audio_type_length(self.type)
+
+
+@dataclasses.dataclass
+class Item:
+    audio: Audio | None
+
+    title: str
+    guid: str
+    duration: str
+
+    raw_pub_date: str
+
+    explicit: bool = False
+    description: str = ""
+
+    link: str = ""
+    keywords: str = ""
+
+    pub_date: datetime | None = None
+
+    def __post_init__(self) -> None:
+        if (pub_date := parse_date(self.raw_pub_date)) is None:
+            raise ValidationError("missing or invalid date")
+
+        if self.audio is None:
+            raise ValidationError("missing audio")
+
+        _validate_duration_length(self.duration)
+
+        self.pub_date = pub_date
+        self.link = _clean_url(self.link)
+
+
+@dataclasses.dataclass
+class Feed:
+    title: str
+    description: str
+    creators: set[str]
+    cover_url: str | None
+    categories: list[str]
+    items: list[Item]
+
+    explicit: bool = False
+
+    language: str = "en"
+    link: str = ""
+
+    def __post_init__(self) -> None:
+        if len(self.items) == 0:
+            raise ValidationError("Must be at least one item")
+
+        self.link = _clean_url(self.link)
+
+        self.language = (
+            self.language.replace("-", "").strip()[:2] if self.language else "en"
+        ).lower()
+
+    def get_creators(self) -> str:
+        return ", ".join(
+            {
+                c
+                for c in {
+                    c.lower(): c for c in [c.strip() for c in self.creators]
+                }.values()
+                if c
+            }
+        )
+
+    def get_pub_date(self) -> datetime | None:
+        now = timezone.now()
+        try:
+            return max(item.pub_date for item in self.items if item.pub_date < now)
+        except ValueError:
+            return None
+
+
 def parse_feed(raw: bytes) -> Feed:
 
     try:
@@ -200,94 +288,6 @@ class ItemParser(RssParser):
         if keywords := self.parse_text("itunes:keywords"):
             rv.append(keywords)
         return " ".join(rv)
-
-
-@dataclasses.dataclass
-class Audio:
-
-    type: str
-    url: str
-    length: int | None = None
-    rel: str = ""
-
-    def __post_init__(self) -> None:
-        _validate_url(self.url)
-        _validate_audio_type(self.type)
-        _validate_audio_type_length(self.type)
-
-
-@dataclasses.dataclass
-class Item:
-    audio: Audio | None
-
-    title: str
-    guid: str
-    duration: str
-
-    raw_pub_date: str
-
-    explicit: bool = False
-    description: str = ""
-
-    link: str = ""
-    keywords: str = ""
-
-    pub_date: datetime | None = None
-
-    def __post_init__(self) -> None:
-        if (pub_date := parse_date(self.raw_pub_date)) is None:
-            raise ValidationError("missing or invalid date")
-
-        if self.audio is None:
-            raise ValidationError("missing audio")
-
-        _validate_duration_length(self.duration)
-
-        self.pub_date = pub_date
-        self.link = _clean_url(self.link)
-
-
-@dataclasses.dataclass
-class Feed:
-    title: str
-    description: str
-    creators: set[str]
-    cover_url: str | None
-    categories: list[str]
-    items: list[Item]
-
-    explicit: bool = False
-
-    language: str = "en"
-    link: str = ""
-
-    def __post_init__(self) -> None:
-        if len(self.items) == 0:
-            raise ValidationError("Must be at least one item")
-
-        self.link = _clean_url(self.link)
-
-        self.language = (
-            self.language.replace("-", "").strip()[:2] if self.language else "en"
-        ).lower()
-
-    def get_creators(self) -> str:
-        return ", ".join(
-            {
-                c
-                for c in {
-                    c.lower(): c for c in [c.strip() for c in self.creators]
-                }.values()
-                if c
-            }
-        )
-
-    def get_pub_date(self) -> datetime | None:
-        now = timezone.now()
-        try:
-            return max(item.pub_date for item in self.items if item.pub_date < now)
-        except ValueError:
-            return None
 
 
 def _clean_url(url: str | None) -> str:
