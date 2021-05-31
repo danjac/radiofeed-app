20503c2accb5d9ae20bb7ef165ea8eba6a4c3ab3
Dan Jacob <danjac2018@gmail.com>
2021-05-31 23:25:09 +0300

    Test fixes

--- audiotrails/podcasts/feed_parser.py
+++ audiotrails/podcasts/feed_parser.py
@@ -48,6 +48,8 @@ def parse_feed(podcast: Podcast) -> list[Episode]:
 
 
 def sync_podcast_status(podcast: Podcast, result: box.Box) -> bool:
+    """Checks result HTTP status: returns True if
+    any required changes made to podcast."""
 
     if result.status == http.HTTPStatus.GONE:
         # HTTP gone: podcast is dead
