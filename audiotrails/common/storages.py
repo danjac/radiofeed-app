141e5c0b1865a0218b517cf83b87b20836fe3cbe
Dan Jacob <danjac2018@gmail.com>
2021-06-16 23:16:53 +0300

    Remove old storages

--- audiotrails/common/storages.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class MediaStorage(S3Boto3Storage):
-    location: str = settings.AWS_MEDIA_LOCATION
-    file_overwrite: bool = False
