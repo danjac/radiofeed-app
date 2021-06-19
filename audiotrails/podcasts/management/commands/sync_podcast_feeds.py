89bae8fc61cf6d9e4d2dbb88a423580ed9ea2376
Dan Jacob <danjac2018@gmail.com>
2021-06-19 17:31:24 +0300

    Simplify commands

--- audiotrails/podcasts/management/commands/sync_podcast_feeds.py
+++ audiotrails/podcasts/management/commands/sync_podcast_feeds.py
@@ -1,56 +1,10 @@
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import BaseCommand
 
-from audiotrails.podcasts.feed_parser import parse_feed
-from audiotrails.podcasts.models import Podcast
-from audiotrails.podcasts.tasks import sync_podcast_feed, sync_podcast_feeds
+from audiotrails.podcasts.tasks import sync_podcast_feeds
 
 
 class Command(BaseCommand):
     help = "Updates all podcasts from their RSS feeds."
 
-    def add_arguments(self, parser: CommandParser) -> None:
-
-        parser.add_argument(
-            "--run-job",
-            action="store_true",
-            help="Just runs the sync_podcast_feeds celery task with no arguments",
-        )
-
-        parser.add_argument(
-            "--no-pub-date",
-            action="store_true",
-            help="Updates only podcasts without a pub date",
-        )
-
-        parser.add_argument(
-            "--use-celery",
-            action="store_true",
-            help="Sync each podcast using celery task",
-        )
-
     def handle(self, *args, **options) -> None:
-
-        if options["run_job"]:
-            sync_podcast_feeds.delay()
-            return
-
-        podcasts = Podcast.objects.all()
-
-        if options["no_pub_date"]:
-            podcasts = podcasts.filter(pub_date__isnull=True)
-
-        use_celery = options["use_celery"]
-
-        for podcast in podcasts:
-            self.sync_podcast(podcast, use_celery)
-
-    def sync_podcast(self, podcast: Podcast, use_celery: bool) -> None:
-        if use_celery:
-            self.stdout.write(f"Create sync task for {podcast}")
-            sync_podcast_feed.delay(rss=podcast.rss)
-        else:
-            try:
-                self.stdout.write(f"Syncing podcast {podcast}")
-                parse_feed(podcast)
-            except Exception as e:
-                self.stdout.write(self.style.ERROR(str(e)))
+        sync_podcast_feeds.delay()
