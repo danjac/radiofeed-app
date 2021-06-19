d588bebe25adf5d79a7034cff3847f2783bdf1d9
Dan Jacob <danjac2018@gmail.com>
2021-06-19 15:05:42 +0300

    Middleware tests

--- audiotrails/common/middleware.py
+++ audiotrails/common/middleware.py
@@ -11,9 +11,6 @@ class BaseMiddleware:
     def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]):
         self.get_response = get_response
 
-    def __call__(self, request: HttpRequest) -> HttpResponse:
-        return self.get_response(request)
-
 
 class Search:
     search_param: str = "q"
