1fda683f16e9825ed322413cb68686db10005e6b
Dan Jacob <danjac2018@gmail.com>
2021-06-19 11:42:58 +0300

    More date parser tests

--- audiotrails/podcasts/tests/test_date_parser.py
+++ audiotrails/podcasts/tests/test_date_parser.py
@@ -8,6 +8,17 @@ from audiotrails.podcasts.date_parser import parse_date
 
 
 class ParseDateTests(SimpleTestCase):
+    def test_parse_date_if_empty_str(self) -> None:
+        self.assertEqual(parse_date(""), None)
+
+    def test_parse_date_if_none(self) -> None:
+        self.assertEqual(parse_date(None), None)
+
+    def test_parse_date_if_not_tz_aware(self) -> None:
+        dt = datetime.datetime(2020, 6, 19, 16, 58, 3)
+        new_dt = parse_date(dt)
+        self.assertEqual(new_dt.tzinfo, pytz.UTC)
+
     def test_parse_date_if_date(self) -> None:
         dt = datetime.datetime(2020, 6, 19, 16, 58, 3, tzinfo=pytz.UTC)
         self.assertEqual(parse_date(dt), dt)
