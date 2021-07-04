70dfd8e977260d48614e4484be171e235d0750af
Dan Jacob <danjac2018@gmail.com>
2021-07-04 15:15:42 +0300

    Task tests

--- audiotrails/podcasts/tests/test_tasks.py
+++ audiotrails/podcasts/tests/test_tasks.py
@@ -1,8 +1,7 @@
 import datetime
 
-from unittest.mock import Mock, patch
+import pytest
 
-from django.test import TestCase
 from django.utils import timezone
 
 from audiotrails.podcasts import tasks
@@ -10,85 +9,80 @@ from audiotrails.podcasts.factories import PodcastFactory
 from audiotrails.users.factories import UserFactory
 
 
-class SendRecommendationEmailsTests(TestCase):
-    @patch(
-        "audiotrails.podcasts.tasks.send_recommendations_email",
-        autospec=True,
-    )
-    def test_send_if_user_inactive(self, mock_send_email: Mock) -> None:
+class TestSendRecommendationEmails:
+    @pytest.fixture
+    def mock_send_email(self, mocker):
+        return mocker.patch(
+            "audiotrails.podcasts.tasks.send_recommendations_email",
+            autospec=True,
+        )
+
+    def test_send_if_user_inactive(self, db, mock_send_email):
         UserFactory(is_active=False, send_recommendations_email=True)
         tasks.send_recommendation_emails()
         mock_send_email.assert_not_called()
 
-    @patch(
-        "audiotrails.podcasts.tasks.send_recommendations_email",
-        autospec=True,
-    )
-    def test_send_if_user_emails_off(self, mock_send_email: Mock) -> None:
+    def test_send_if_user_emails_off(self, db, mock_send_email):
         UserFactory(is_active=True, send_recommendations_email=False)
         tasks.send_recommendation_emails()
         mock_send_email.assert_not_called()
 
-    @patch(
-        "audiotrails.podcasts.tasks.send_recommendations_email",
-        autospec=True,
-    )
-    def test_send_if_user_emails_on(self, mock_send_email: Mock) -> None:
+    def test_send_if_user_emails_on(self, db, mock_send_email):
         UserFactory(is_active=True, send_recommendations_email=True)
         tasks.send_recommendation_emails()
         mock_send_email.assert_called()
 
 
-class CreatePodcastRecommendationsTests(TestCase):
-    @patch("audiotrails.podcasts.tasks.recommend", autospec=True)
-    def test_create_podcast_recommendations(self, mock_recommend) -> None:
+class TestCreatePodcastRecommendationsTests:
+    def test_create_podcast_recommendations(self, db, mocker):
+        mock_recommend = mocker.patch(
+            "audiotrails.podcasts.tasks.recommend", autospec=True
+        )
         tasks.create_podcast_recommendations()
         mock_recommend.assert_called()
 
 
-class CrawlItunesTests(TestCase):
-    @patch("audiotrails.podcasts.tasks.itunes.crawl_itunes", autospec=True)
-    def test_crawl_itunes(self, mock_crawl_itunes) -> None:
+class TestCrawlItunes:
+    def test_crawl_itunes(self, db, mocker):
+        mock_crawl_itunes = mocker.patch(
+            "audiotrails.podcasts.tasks.itunes.crawl_itunes", autospec=True
+        )
+
         tasks.crawl_itunes()
         mock_crawl_itunes.assert_called()
 
 
-class SyncPodcastFeedsTests(TestCase):
-    @patch(
-        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
-        autospec=True,
-    )
-    def test_podcast_just_updated(self, mock_sync_podcast_feed: Mock) -> None:
+class TestSyncPodcastFeedsTests:
+    @pytest.fixture
+    def mock_sync_podcast_feed(self, mocker):
+        return mocker.patch(
+            "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
+            autospec=True,
+        )
+
+    def test_podcast_just_updated(self, db, mock_sync_podcast_feed):
         PodcastFactory(pub_date=timezone.now())
         tasks.sync_podcast_feeds()
         mock_sync_podcast_feed.assert_not_called()
 
-    @patch(
-        "audiotrails.podcasts.tasks.sync_podcast_feed.delay",
-        autospec=True,
-    )
-    def test_podcast_updated_more_than_12_hours_ago(
-        self, mock_sync_podcast_feed: Mock
-    ) -> None:
+    def test_podcast_updated_more_than_12_hours_ago(self, db, mock_sync_podcast_feed):
         PodcastFactory(pub_date=timezone.now() - datetime.timedelta(hours=24))
         tasks.sync_podcast_feeds()
         mock_sync_podcast_feed.assert_called()
 
 
-class TestSyncPodcastFeed(TestCase):
-    @patch(
-        "audiotrails.podcasts.tasks.parse_feed",
-        autospec=True,
-    )
-    def test_no_podcast_found(self, mock_parse_feed: Mock) -> None:
+class TestSyncPodcastFeed:
+    @pytest.fixture
+    def mock_parse_feed(self, mocker):
+        return mocker.patch(
+            "audiotrails.podcasts.tasks.parse_feed",
+            autospec=True,
+        )
+
+    def test_no_podcast_found(self, db, mock_parse_feed):
         tasks.sync_podcast_feed(12345)
         mock_parse_feed.assert_not_called()
 
-    @patch(
-        "audiotrails.podcasts.tasks.parse_feed",
-        autospec=True,
-    )
-    def test_ok(self, mock_parse_feed: Mock) -> None:
-        podcast = PodcastFactory()
+    def test_ok(self, db, podcast, mock_parse_feed):
         tasks.sync_podcast_feed(podcast.rss, counter=10, total=100)
         mock_parse_feed.assert_called()
