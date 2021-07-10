43b4c2ed2ac431ac7864a1074930160c8f4d9817
Dan Jacob <danjac2018@gmail.com>
2021-07-10 14:27:08 +0300

    Set default itunes limit

--- audiotrails/podcasts/itunes.py
+++ audiotrails/podcasts/itunes.py
@@ -4,6 +4,7 @@ import dataclasses
 
 import requests
 
+from django.conf import settings
 from django.core.cache import cache
 from django.db.models import Q
 from django.template.defaultfilters import striptags
@@ -38,7 +39,7 @@ class SearchResult:
 
 
 def fetch_itunes_genre(
-    genre_id: int, num_results: int = 20
+    genre_id: int, num_results: int = settings.DEFAULT_ITUNES_LIMIT
 ) -> tuple[list[SearchResult], list[Podcast]]:
     """Fetch top rated results for genre"""
     return _get_or_create_podcasts(
@@ -54,7 +55,7 @@ def fetch_itunes_genre(
 
 
 def search_itunes(
-    search_term, num_results=12
+    search_term: str, num_results: int = settings.DEFAULT_ITUNES_LIMIT
 ) -> tuple[list[SearchResult], list[Podcast]]:
     """Does a search query on the iTunes API."""
 
