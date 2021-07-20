112ffc7f2a12a6ba37c9add4783e033bf56858a7
Dan Jacob <danjac2018@gmail.com>
2021-07-20 16:40:33 +0300

    Added with_hx_trigger helper

---
+++ jcasts/shared/tests/test_response.py
@@ -0,0 +1,41 @@
+import json
+
+import pytest
+
+from django.http import HttpResponse
+
+from jcasts.shared.response import with_hx_trigger
+
+
+class TestWithHxTrigger:
+    @pytest.fixture
+    def response(self) -> HttpResponse:
+        return HttpResponse()
+
+    def test_no_data(self, response):
+        response = with_hx_trigger(response, "")
+        assert "HX-Trigger" not in response
+
+    def test_string(self, response):
+        response = with_hx_trigger(response, "reload-queue")
+        assert json.loads(response["HX-Trigger"]) == {"reload-queue": ""}
+
+    def test_dict(self, response):
+        response = with_hx_trigger(response, {"remove-queue-item": 1})
+        assert json.loads(response["HX-Trigger"]) == {"remove-queue-item": 1}
+
+    def test_append_to_string(self, response):
+        response["HX-Trigger"] = "testme"
+        response = with_hx_trigger(response, {"remove-queue-item": 1})
+        assert json.loads(response["HX-Trigger"]) == {
+            "remove-queue-item": 1,
+            "testme": "",
+        }
+
+    def test_append_to_dict(self, response):
+        response["HX-Trigger"] = json.dumps({"testme": "test"})
+        response = with_hx_trigger(response, {"remove-queue-item": 1})
+        assert json.loads(response["HX-Trigger"]) == {
+            "remove-queue-item": 1,
+            "testme": "test",
+        }
