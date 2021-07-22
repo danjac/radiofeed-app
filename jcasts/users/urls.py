4bd0612c1819baac067146d1ff82bc54a075413d
Dan Jacob <danjac@users.noreply.github.com>
2021-07-22 09:29:57 +0300

    Update urls.py

--- jcasts/users/urls.py
+++ jcasts/users/urls.py
@@ -3,9 +3,10 @@ from django.urls import include, path
 from jcasts.users import views
 
 urlpatterns = [
-    path("preferences/", views.user_preferences, name="user_preferences"),
-    path("stats/", views.user_stats, name="user_stats"),
-    path("~export/", views.export_podcast_feeds, name="export_podcast_feeds"),
-    path("~delete/", views.delete_account, name="delete_account"),
+    path("~accept_cookies", views.accept_cookies, name="accept_cookies"),
+    path("account/preferences/", views.user_preferences, name="user_preferences"),
+    path("account/stats/", views.user_stats, name="user_stats"),
+    path("account/~export/", views.export_podcast_feeds, name="export_podcast_feeds"),
+    path("account/~delete/", views.delete_account, name="delete_account"),
     path("", include("allauth.urls")),
 ]
