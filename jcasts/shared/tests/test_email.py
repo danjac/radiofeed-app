047780b6db05e3ff7b56441bed7068267e148366
Dan Jacob <danjac2018@gmail.com>
2021-07-29 16:51:38 +0300

    Email test fix

--- jcasts/shared/tests/test_email.py
+++ jcasts/shared/tests/test_email.py
@@ -4,10 +4,12 @@ from jcasts.shared.email import RqBackend
 
 
 class TestRqBackend:
-    def test_send_messages(self):
+    def test_send_messages(self, settings, mailoutbox):
+        settings.RQ_EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
         msg = EmailMessage(
             subject="test",
             to=["tester@gmail.com"],
             body="Hi",
         )
         assert RqBackend().send_messages([msg]) == 1
+        assert len(mailoutbox) == 1
