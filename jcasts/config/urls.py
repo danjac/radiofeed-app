f413e52a68bf3d4a7967de1aed2ae3b519e54d09
Dan Jacob <danjac2018@gmail.com>
2021-08-03 13:55:32 +0300

    Remove compat

--- jcasts/config/urls.py
+++ jcasts/config/urls.py
@@ -32,6 +32,5 @@ urlpatterns = [
     ),
     path("account/", include("allauth.urls")),
     path("rq/", include("django_rq.urls")),
-    path(settings.HIJACK_URL, include("hijack.urls", namespace="hijack")),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
