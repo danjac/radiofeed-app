66f41b6a99d3eccb3a70aa67d80b2d97c0421dcb
Dan Jacob <danjac2018@gmail.com>
2021-08-15 23:53:51 +0300

    Scheduler tweaks

--- jcasts/podcasts/scheduler.py
+++ jcasts/podcasts/scheduler.py
@@ -96,6 +96,6 @@ def schedule(
     # add 5% of freq to current time (min 1 hour)
     # e.g. 7 days - try again in about 8 hours
 
-    diff = max(now - podcast.pub_date, MIN_FREQ)
+    diff = timedelta(seconds=(now - podcast.pub_date).total_seconds() * 0.05)
 
-    return now + timedelta(seconds=(diff.total_seconds() * 0.05))
+    return now + max(diff, MIN_FREQ)
