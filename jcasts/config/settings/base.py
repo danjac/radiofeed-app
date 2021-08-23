571a4b18cb50d3414e613f3c374fd8b15cbe1d14
Dan Jacob <danjac2018@gmail.com>
2021-08-23 16:56:03 +0300

    Add twitter support

--- jcasts/config/settings/base.py
+++ jcasts/config/settings/base.py
@@ -104,6 +104,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.twitter",
     "django_extensions",
     "django_htmx",
     "widget_tweaks",
