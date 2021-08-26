a3884a33af873b2ca7003d2ab9197632e5a51b11
Dan Jacob <danjac2018@gmail.com>
2021-08-27 00:35:28 +0300

    Removing hints

--- jcasts/episodes/views/__init__.py
+++ jcasts/episodes/views/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from django.conf import settings
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
