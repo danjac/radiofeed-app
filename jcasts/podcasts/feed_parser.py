8161f35784a0b6df44d0895a18e91596cf4a59e7
Dan Jacob <danjac2018@gmail.com>
2021-08-27 21:04:23 +0300

    Add bandit pre-commit

--- jcasts/podcasts/feed_parser.py
+++ jcasts/podcasts/feed_parser.py
@@ -1,5 +1,5 @@
 import http
-import random
+import secrets
 import traceback
 
 from dataclasses import dataclass
@@ -258,7 +258,7 @@ def extract_text(podcast, categories, items):
 def get_feed_headers(podcast, force_update=False):
     headers: dict[str, str] = {
         "Accept": ACCEPT_HEADER,
-        "User-Agent": random.choice(USER_AGENTS),
+        "User-Agent": secrets.choice(USER_AGENTS),
     }
 
     # ignore any modified/etag headers
