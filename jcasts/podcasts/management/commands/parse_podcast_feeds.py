2174a13460204df66e8d23f2fd7e26415ccfb05f
Dan Jacob <danjac2018@gmail.com>
2021-08-27 15:38:25 +0300

    Remove limit from command

--- jcasts/podcasts/management/commands/parse_podcast_feeds.py
+++ jcasts/podcasts/management/commands/parse_podcast_feeds.py
@@ -12,15 +12,10 @@ class Command(BaseCommand):
             action="store_true",
             help="Update all feeds (including unscheduled)",
         )
-        parser.add_argument(
-            "--limit",
-            type=int,
-            help="Set limit",
-        )
 
     def handle(self, *args, **options):
 
         num_feeds = feed_parser.parse_podcast_feeds(
-            force_update=options["force_update"], limit=options["limit"]
+            force_update=options["force_update"]
         )
         self.stdout.write(self.style.SUCCESS(f"{num_feeds} feed(s) to be pulled"))
