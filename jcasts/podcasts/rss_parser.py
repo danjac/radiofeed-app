8625b57bec8358e0cf203aa3aa8631d8b803381e
Dan Jacob <danjac2018@gmail.com>
2021-08-28 11:30:46 +0300

    Refactor finder

--- jcasts/podcasts/rss_parser.py
+++ jcasts/podcasts/rss_parser.py
@@ -20,35 +20,6 @@ class RssParserError(ValueError):
     ...
 
 
-def parse_rss(content):
-
-    for _, element in lxml.etree.iterparse(
-        io.BytesIO(content),
-        encoding="utf-8",
-        no_network=True,
-        resolve_entities=False,
-        recover=True,
-        events=("end",),
-    ):
-        if element.tag == "channel":
-            try:
-                return parse_channel(element)
-            finally:
-                element.clear()
-
-    raise RssParserError("<channel /> not found in RSS feed")
-
-
-def parse_channel(channel):
-    try:
-        feed = Feed.parse_obj(parse_feed(channel))
-    except ValidationError as e:
-        raise RssParserError from e
-    if not (items := [*parse_items(channel)]):
-        raise RssParserError("no items found in RSS feed")
-    return feed, items
-
-
 class Item(BaseModel):
 
     guid: str
@@ -120,23 +91,52 @@ class Feed(BaseModel):
         return value[:2]
 
 
+def parse_rss(content):
+
+    for _, element in lxml.etree.iterparse(
+        io.BytesIO(content),
+        encoding="utf-8",
+        no_network=True,
+        resolve_entities=False,
+        recover=True,
+        events=("end",),
+    ):
+        if element.tag == "channel":
+            try:
+                return parse_channel(element)
+            finally:
+                element.clear()
+
+    raise RssParserError("<channel /> not found in RSS feed")
+
+
+def parse_channel(channel):
+    try:
+        feed = Feed.parse_obj(parse_feed(channel))
+    except ValidationError as e:
+        raise RssParserError from e
+    if not (items := [*parse_items(channel)]):
+        raise RssParserError("no items found in RSS feed")
+    return feed, items
+
+
 def parse_feed(channel):
+    finder = XPathFinder(channel, namespaces=NAMESPACES)
     return {
-        "title": xfind(channel, "title/text()"),
-        "link": xfind(channel, "link/text()", default=""),
-        "language": xfind(channel, "language/text()", default="en"),
-        "explicit": xfind(channel, "itunes:explicit/text()"),
-        "cover_url": xfind(channel, "itunes:image/@href", "image/url/text()"),
-        "description": xfind(
-            channel, "description/text()", "itunes:summary/text()", default=""
+        "title": finder.find("title/text()"),
+        "link": finder.find("link/text()", default=""),
+        "language": finder.find("language/text()", default="en"),
+        "explicit": finder.find("itunes:explicit/text()"),
+        "cover_url": finder.find("itunes:image/@href", "image/url/text()"),
+        "description": finder.find(
+            "description/text()", "itunes:summary/text()", default=""
         ),
-        "owner": xfind(
-            channel,
+        "owner": finder.find(
             "itunes:author/text()",
             "itunes:owner/itunes:name/text()",
             default="",
         ),
-        "categories": xfindall(channel, "//itunes:category/@text"),
+        "categories": finder.findall("//itunes:category/@text"),
     }
 
 
@@ -152,42 +152,52 @@ def parse_items(channel):
 
 
 def parse_item(item):
+    finder = XPathFinder(item, namespaces=NAMESPACES)
     return {
-        "guid": xfind(item, "guid/text()"),
-        "title": xfind(item, "title/text()"),
-        "pub_date": xfind(item, "pubDate/text()"),
-        "media_url": xfind(item, "enclosure//@url"),
-        "media_type": xfind(item, "enclosure//@type"),
-        "length": xfind(item, "enclosure//@length"),
-        "cover_url": xfind(item, "itunes:image/@href"),
-        "explicit": xfind(item, "itunes:explicit/text()"),
-        "episode": xfind(item, "itunes:episode/text()"),
-        "season": xfind(item, "itunes:season/text()"),
-        "description": xfind(
-            item,
+        "guid": finder.find("guid/text()"),
+        "title": finder.find("title/text()"),
+        "pub_date": finder.find("pubDate/text()"),
+        "media_url": finder.find("enclosure//@url"),
+        "media_type": finder.find("enclosure//@type"),
+        "length": finder.find("enclosure//@length"),
+        "cover_url": finder.find("itunes:image/@href"),
+        "explicit": finder.find("itunes:explicit/text()"),
+        "episode": finder.find("itunes:episode/text()"),
+        "season": finder.find("itunes:season/text()"),
+        "description": finder.find(
             "content:encoded/text()",
             "description/text()",
             "itunes:summary/text()",
             default="",
         ),
-        "duration": xfind(item, "itunes:duration/text()", default=""),
-        "episode_type": xfind(item, "itunes:episodetype/text()", default="full"),
-        "keywords": xfindall(item, "category/text()"),
+        "duration": finder.find("itunes:duration/text()", default=""),
+        "episode_type": finder.find("itunes:episodetype/text()", default="full"),
+        "keywords": finder.findall("category/text()"),
     }
 
 
-def xfind(element, *paths, default=None):
-    for path in paths:
+class XPathFinder:
+    def __init__(self, element, namespaces=None):
+        self.element = element
+        self.namespaces = namespaces
+
+    def find(self, *paths, default=None):
+        """Find single attribute or text value. Returns
+        first matching value."""
+        for path in paths:
+            try:
+                if values := self.element.xpath(path, namespaces=self.namespaces):
+                    return values[0].strip()
+            except UnicodeDecodeError:
+                pass
+        return default
+
+    def findall(self, path):
+        """Returns list of attributes or text values"""
         try:
-            if values := element.xpath(path, namespaces=NAMESPACES):
-                return values[0].strip()
-        except UnicodeDecodeError:
-            pass
-    return default
-
-
-def xfindall(element, path):
-    try:
-        return [value.strip() for value in element.xpath(path, namespaces=NAMESPACES)]
-    except UnicodeDecodeError:  # pragma: no cover
-        return []
+            return [
+                value.strip()
+                for value in self.element.xpath(path, namespaces=self.namespaces)
+            ]
+        except UnicodeDecodeError:  # pragma: no cover
+            return []
