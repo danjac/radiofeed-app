5e01d3066acefcd7a0b7d39233e52751776ca889
Dan Jacob <danjac2018@gmail.com>
2021-09-04 19:17:41 +0300

    Item tweaks

--- jcasts/episodes/templatetags/player.py
+++ jcasts/episodes/templatetags/player.py
@@ -5,18 +5,6 @@ from jcasts.episodes.models import AudioLog
 register = template.Library()
 
 
-@register.inclusion_tag("episodes/_play_toggle.html", takes_context=True)
-def play_toggle(context, episode, is_detail=False):
-
-    request = context["request"]
-    return {
-        "episode": episode,
-        "user": request.user,
-        "is_detail": is_detail,
-        "is_playing": request.player.has(episode.id),
-    }
-
-
 @register.inclusion_tag("episodes/_player.html", takes_context=True)
 def audio_player(context):
     request = context["request"]
