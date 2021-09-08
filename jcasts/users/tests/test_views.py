460f07a4bae1eb637a4c70de6d7fd2ad259f772d
Dan Jacob <danjac2018@gmail.com>
2021-09-08 14:24:00 +0300

    Add query check to tests

--- jcasts/users/tests/test_views.py
+++ jcasts/users/tests/test_views.py
@@ -9,18 +9,20 @@ from jcasts.shared.assertions import assert_ok
 class TestUserPreferences:
     url = reverse_lazy("users:preferences")
 
-    def test_get(self, client, auth_user):
-        resp = client.get(self.url)
+    def test_get(self, client, auth_user, django_assert_num_queries):
+        with django_assert_num_queries(3):
+            resp = client.get(self.url)
         assert_ok(resp)
 
-    def test_post(self, client, auth_user):
-        resp = client.post(
-            self.url,
-            {
-                "send_recommendations_email": False,
-                "autoplay": True,
-            },
-        )
+    def test_post(self, client, auth_user, django_assert_num_queries):
+        with django_assert_num_queries(4):
+            resp = client.post(
+                self.url,
+                {
+                    "send_recommendations_email": False,
+                    "autoplay": True,
+                },
+            )
         assert resp.url == self.url
 
         auth_user.refresh_from_db()
@@ -30,7 +32,7 @@ class TestUserPreferences:
 
 
 class TestUserStats:
-    def test_stats(self, client, auth_user, podcast):
+    def test_stats(self, client, auth_user, podcast, django_assert_num_queries):
 
         FollowFactory(podcast=podcast, user=auth_user)
         AudioLogFactory(episode=EpisodeFactory(podcast=podcast), user=auth_user)
@@ -38,7 +40,8 @@ class TestUserStats:
         AudioLogFactory(user=auth_user)
         FavoriteFactory(user=auth_user)
 
-        resp = client.get(reverse("users:stats"))
+        with django_assert_num_queries(9):
+            resp = client.get(reverse("users:stats"))
         assert_ok(resp)
         assert resp.context["stats"]["follows"] == 1
         assert resp.context["stats"]["listened"] == 3
@@ -47,16 +50,19 @@ class TestUserStats:
 class TestExportPodcastFeeds:
     url = reverse_lazy("users:export_podcast_feeds")
 
-    def test_get(self, client, auth_user):
-        assert_ok(client.get(self.url))
+    def test_get(self, client, auth_user, django_assert_num_queries):
+        with django_assert_num_queries(3):
+            assert_ok(client.get(self.url))
 
-    def test_export_opml(self, client, follow):
-        resp = client.post(self.url, {"format": "opml"})
+    def test_export_opml(self, client, follow, django_assert_num_queries):
+        with django_assert_num_queries(4):
+            resp = client.post(self.url, {"format": "opml"})
         assert_ok(resp)
         assert resp["Content-Type"] == "application/xml"
 
-    def test_export_csv(self, client, follow):
-        resp = client.post(self.url, {"format": "csv"})
+    def test_export_csv(self, client, follow, django_assert_num_queries):
+        with django_assert_num_queries(4):
+            resp = client.post(self.url, {"format": "csv"})
         assert_ok(resp)
         assert resp["Content-Type"] == "text/csv"
 
@@ -64,18 +70,25 @@ class TestExportPodcastFeeds:
 class TestDeleteAccount:
     url = reverse_lazy("users:delete_account")
 
-    def test_get(self, client, auth_user, django_user_model):
+    def test_get(self, client, auth_user, django_user_model, django_assert_num_queries):
         # make sure we don't accidentally delete account on get request
-        resp = client.get(self.url)
+        with django_assert_num_queries(3):
+            resp = client.get(self.url)
         assert_ok(resp)
         assert django_user_model.objects.exists()
 
-    def test_post_unconfirmed(self, client, auth_user, django_user_model):
-        resp = client.post(self.url)
+    def test_post_unconfirmed(
+        self, client, auth_user, django_user_model, django_assert_num_queries
+    ):
+        with django_assert_num_queries(3):
+            resp = client.post(self.url)
         assert_ok(resp)
         assert django_user_model.objects.exists()
 
-    def test_post_confirmed(self, client, auth_user, django_user_model):
-        resp = client.post(self.url, {"confirm-delete": True})
+    def test_post_confirmed(
+        self, client, auth_user, django_user_model, django_assert_num_queries
+    ):
+        with django_assert_num_queries(16):
+            resp = client.post(self.url, {"confirm-delete": True})
         assert resp.url == settings.HOME_URL
         assert not django_user_model.objects.exists()
