a79272ff9f92f27769d306e6a9c96fb12726aa46
Dan Jacob <danjac2018@gmail.com>
2021-09-28 15:41:59 +0300

    Fix internal ips

--- jcasts/config/settings/local.py
+++ jcasts/config/settings/local.py
@@ -15,4 +15,4 @@ INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS
 # docker internal ips
 
 _, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+INTERNAL_IPS = ips
