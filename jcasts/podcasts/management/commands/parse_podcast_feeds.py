6cc2526ade4a264e70b38ef82583aca010ba1990
Dan Jacob <danjac2018@gmail.com>
2021-10-02 22:26:50 +0300

    Add reset option

--- jcasts/podcasts/management/commands/parse_podcast_feeds.py
+++ jcasts/podcasts/management/commands/parse_podcast_feeds.py
@@ -1,11 +1,20 @@
 from django.core.management.base import BaseCommand
 
 from jcasts.podcasts import feed_parser
+from jcasts.podcasts.models import Podcast
 
 
 class Command(BaseCommand):
     help = "Run podcast feed updates"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--reset",
+            action="store_true",
+        )
+
     def handle(self, *args, **options):
+        if options["reset"]:
+            Podcast.objects.update(queued=None)
         jobs = [*feed_parser.parse_podcast_feeds()]
         self.stdout.write(self.style.SUCCESS(f"{len(jobs)} feed(s) to be pulled"))
