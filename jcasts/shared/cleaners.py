426497fa61dce6aa916ce339c65585fc86fc182c
Dan Jacob <danjac2018@gmail.com>
2021-10-23 13:05:56 +0300

    Models

--- jcasts/shared/cleaners.py
+++ jcasts/shared/cleaners.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import html
 import re
 
@@ -7,7 +9,7 @@ import markdown
 from django.template.defaultfilters import striptags
 from html5lib.filters import whitespace
 
-ALLOWED_TAGS = [
+ALLOWED_TAGS: list[str] = [
     "a",
     "abbr",
     "acronym",
@@ -50,7 +52,7 @@ ALLOWED_TAGS = [
     "ul",
 ]
 
-ALLOWED_ATTRS = {
+ALLOWED_ATTRS: dict[str, list[str]] = {
     "a": ["href", "target", "title"],
 }
 
@@ -64,32 +66,32 @@ cleaner = bleach.Cleaner(
 )
 
 
-def linkify_callback(attrs, new=False):
+def linkify_callback(attrs: dict[tuple[str | None, str]], new=False):
     attrs[(None, "target")] = "_blank"
     attrs[(None, "rel")] = "noopener noreferrer nofollow"
     return attrs
 
 
-def clean(value):
+def clean(value: str | None) -> str:
     return bleach.linkify(cleaner.clean(value), [linkify_callback]) if value else ""  # type: ignore
 
 
-def strip_whitespace(value):
+def strip_whitespace(value: str | None) -> str:
     return (value or "").strip()
 
 
-def strip_html(value):
+def strip_html(value: str | None) -> str:
     """Removes all HTML tags and entities"""
     return html.unescape(striptags(strip_whitespace(value)))
 
 
-def as_html(value):
+def as_html(value: str) -> str:
     if HTML_RE.match(value):
         return value
     return markdown.markdown(value)
 
 
-def markup(value):
+def markup(value: str | None) -> str:
     """Parses Markdown and/or html and returns cleaned result."""
     if value := strip_whitespace(value):
         return html.unescape(clean(as_html(value)))
