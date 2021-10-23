871a4696a7b483aaef65a75391c552b70af10b8c
Dan Jacob <danjac2018@gmail.com>
2021-10-23 23:57:39 +0300

    Exact search fix

--- jcasts/podcasts/factories.py
+++ jcasts/podcasts/factories.py
@@ -20,7 +20,7 @@ class FeedFactory(DictFactory):
 
 class ItemFactory(DictFactory):
 
-    guid = LazyFunction(lambda: uuid.uuid4().hex)
+    guid: str = LazyFunction(lambda: uuid.uuid4().hex)
     pub_date = LazyFunction(lambda: timezone.now().isoformat())
     title = Faker("text")
     description = Faker("text")
