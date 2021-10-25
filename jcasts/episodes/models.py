7474a1a729494c3559101838d5f2592cac023bdf
Dan Jacob <danjac2018@gmail.com>
2021-10-25 16:22:25 +0300

    Use podcast image in metadata

--- jcasts/episodes/models.py
+++ jcasts/episodes/models.py
@@ -292,7 +292,7 @@ class Episode(models.Model):
 
     def get_media_metadata(self) -> dict:
         # https://developers.google.com/web/updates/2017/02/media-session
-        cover_url = self.get_cover_url() or static("img/podcast-icon.png")
+        cover_url = self.podcast.cover_url or static("img/podcast-icon.png")
         cover_url_type, _ = mimetypes.guess_type(urlparse(cover_url).path)
 
         return {
