b0cb11535f4a2a9b1605fb7538f102f803afbd56
Dan Jacob <danjac2018@gmail.com>
2021-11-19 11:02:55 +0200

    Seed randomized values

--- jcasts/podcasts/scheduler.py
+++ jcasts/podcasts/scheduler.py
@@ -111,8 +111,6 @@ def get_frequency(pub_dates: list[datetime]) -> timedelta:
 
     # get a randomized sample of frequencies
 
-    numpy.random.seed(1)
-
     frequency = statistics.mean(
         numpy.random.normal(
             statistics.mean(intervals),
