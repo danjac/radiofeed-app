5d63ab951eb50e2000393cb73d469a54e4195b82
Dan Jacob <danjac2018@gmail.com>
2021-11-19 13:37:34 +0200

    Schedule fix if < now

--- jcasts/podcasts/management/commands/schedule_podcast_feeds.py
+++ jcasts/podcasts/management/commands/schedule_podcast_feeds.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from django.core.management.base import BaseCommand
+from django.db.models import QuerySet
+from django_rq import job
 
 from jcasts.podcasts import scheduler
 from jcasts.podcasts.models import Podcast
@@ -10,34 +12,30 @@ class Command(BaseCommand):
     help = "Re-schedule podcast feeds"
 
     def handle(self, *args, **options) -> None:
-        podcasts = (
-            Podcast.objects.active()
-            .filter(pub_date__isnull=False, queued__isnull=True)
-            .order_by("-pub_date")
-        )
-        for_update = []
+        podcasts = get_queryset().order_by("-pub_date")
 
-        for podcast in podcasts.iterator():
-            pub_dates = list(podcast.episode_set.values_list("pub_date", flat=True))
-            (
-                podcast.scheduled,
-                podcast.frequency,
-                podcast.frequency_modifier,
-            ) = scheduler.schedule(podcast.pub_date, pub_dates)
+        for podcast_id in podcasts.values_list("pk", flat=True).iterator():
+            schedule_podcast.delay(podcast_id)
 
-            self.stdout.write(
-                f"Podcast {podcast.title}: {podcast.scheduled} {podcast.frequency}"
-            )
 
-            for_update.append(podcast)
+@job("default")
+def schedule_podcast(podcast_id: int) -> None:
+    try:
+        podcast = get_queryset().get(pk=podcast_id)
+    except Podcast.DoesNotExist:
+        return
 
-        Podcast.objects.bulk_update(
-            for_update,
-            fields=[
-                "frequency",
-                "frequency_modifier",
-                "scheduled",
-            ],
-            batch_size=500,
-        )
-        self.stdout.write(self.style.SUCCESS(f"{len(for_update)} podcasts updated"))
+    scheduled, frequency, modifier = scheduler.schedule(
+        podcast.pub_date,
+        list(podcast.episode_set.values_list("pub_date", flat=True)),
+    )
+
+    Podcast.objects.filter(pk=podcast_id,).update(
+        scheduled=scheduled,
+        frequency=frequency,
+        frequency_modifier=modifier,
+    )
+
+
+def get_queryset() -> QuerySet:
+    return Podcast.objects.active().filter(pub_date__isnull=False, queued__isnull=True)
