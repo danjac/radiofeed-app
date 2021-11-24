62123f727d95f13c7bdf61d97c862febfad80860
Dan Jacob <danjac2018@gmail.com>
2021-11-24 23:57:49 +0200

    Parse changes

--- jcasts/podcasts/management/commands/schedule_podcast_feeds.py
+++ jcasts/podcasts/management/commands/schedule_podcast_feeds.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import secrets
+
 from datetime import timedelta
 
 from django.core.management.base import BaseCommand
@@ -19,6 +21,8 @@ class Command(BaseCommand):
         for_update = []
         now = timezone.now()
 
+        time_selection = range(0, 24 * 60 * 60)
+
         for counter, podcast in enumerate(
             Podcast.objects.active().fresh().order_by("-pub_date").iterator()
         ):
@@ -26,9 +30,10 @@ class Command(BaseCommand):
             podcast.frequency = scheduler.get_frequency(
                 list(podcast.episode_set.values_list("pub_date", flat=True))
             )
-            podcast.parsed = now - timedelta(seconds=10 * counter)
 
-            self.stdout.write(podcast.title)
+            podcast.parsed = now - timedelta(minutes=secrets.choice(time_selection))
+
+            self.stdout.write(f"{counter}:{podcast.title}")
 
             for_update.append(podcast)
 
