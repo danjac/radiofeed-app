7e2f38b6f75f67af67f8affd71a9cd7024c48292
Dan Jacob <danjac2018@gmail.com>
2021-11-24 22:22:15 +0200

    Disable cron

--- jcasts/podcasts/management/commands/schedule_podcast_feeds.py
+++ jcasts/podcasts/management/commands/schedule_podcast_feeds.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from jcasts.podcasts import scheduler
 from jcasts.podcasts.models import Podcast
@@ -14,15 +15,21 @@ class Command(BaseCommand):
         Podcast.objects.filter(frequency__isnull=False).update(frequency=None)
 
         for_update = []
+        now = timezone.now()
 
         for podcast in Podcast.objects.active().fresh():
 
             podcast.frequency = scheduler.get_frequency(
                 list(podcast.episode_set.values_list("pub_date", flat=True))
             )
+            podcast.parsed = now
 
             self.stdout.write(podcast.title)
 
             for_update.append(podcast)
 
-        Podcast.objects.bulk_update(for_update, fields=["frequency"], batch_size=500)
+        Podcast.objects.bulk_update(
+            for_update,
+            fields=["frequency", "parsed"],
+            batch_size=500,
+        )
