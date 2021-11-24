b926019ca3a863852bc3dbc1004f54bf0584c1c9
Dan Jacob <danjac2018@gmail.com>
2021-11-24 20:18:53 +0200

    Parse changes

---
+++ jcasts/podcasts/management/commands/schedule_podcast_feeds.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from django.core.management.base import BaseCommand
+
+from jcasts.podcasts import scheduler
+from jcasts.podcasts.models import Podcast
+
+
+class Command(BaseCommand):
+    help = "Schedule podcast feeds"
+
+    def handle(self, *args, **options) -> None:
+
+        Podcast.objects.filter(frequency__isnull=False).update(frequency=None)
+
+        for_update = []
+
+        for podcast in Podcast.objects.active().fresh():
+
+            podcast.frequency = scheduler.get_frequency(
+                list(podcast.episode_set.values_list("pub_date", flat=True))
+            )
+
+            self.stdout.write(podcast.title)
+
+            for_update.append(podcast)
+
+        Podcast.objects.bulk_update(for_update, fields=["frequency"], batch_size=500)
