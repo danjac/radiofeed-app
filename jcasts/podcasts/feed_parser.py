9dab03545123908895350e207c0ace3c7d6f9409
Dan Jacob <danjac2018@gmail.com>
2021-12-09 11:37:50 +0200

    Reduce PARSE_ERROR_LIMIT

--- jcasts/podcasts/feed_parser.py
+++ jcasts/podcasts/feed_parser.py
@@ -31,7 +31,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
 ]
 
-PARSE_ERROR_LIMIT = 12  # max errors before podcast is "dead"
+PARSE_ERROR_LIMIT = 3  # max errors before podcast is "dead"
 
 
 class NotModified(requests.RequestException):
