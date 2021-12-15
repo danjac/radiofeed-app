7fcd4692af2d1e466f95fa966494ef4ab6fcf1ac
Dan Jacob <danjac2018@gmail.com>
2021-12-15 10:19:11 +0200

    Additional tests

--- jcasts/podcasts/tests/test_scheduler.py
+++ jcasts/podcasts/tests/test_scheduler.py
@@ -79,7 +79,7 @@ class TestEnqueue:
         )
 
 
-class TestSchedulePrimaryFeeds:
+class TestSchedulePodcastFeeds:
     @pytest.mark.parametrize(
         "pub_date,after,before,success",
         [
@@ -107,11 +107,11 @@ class TestSchedulePrimaryFeeds:
 
         now = timezone.now()
 
-        podcast = PodcastFactory(
-            pub_date=now - pub_date if pub_date else None, promoted=True
-        )
+        podcast = PodcastFactory(pub_date=now - pub_date if pub_date else None)
 
-        count = scheduler.schedule_primary_feeds(after=after, before=before)
+        count = scheduler.schedule_podcast_feeds(
+            Podcast.objects.all(), after=after, before=before
+        )
 
         assert Podcast.objects.filter(queued__isnull=False).exists() is success, (
             pub_date,
@@ -138,7 +138,7 @@ class TestSchedulePrimaryFeeds:
 
         podcast = PodcastFactory(active=active, promoted=True, pub_date=timezone.now())
 
-        count = scheduler.schedule_primary_feeds()
+        count = scheduler.schedule_podcast_feeds(Podcast.objects.all())
 
         Podcast.objects.filter(queued__isnull=False).exists() is success
 
@@ -149,6 +149,29 @@ class TestSchedulePrimaryFeeds:
             assert count == 0
             assert podcast.id not in mock_feed_queue.enqueued
 
+    @pytest.mark.parametrize(
+        "queued,success",
+        [
+            (False, True),
+            (True, False),
+        ],
+    )
+    def test_queued(self, db, mock_feed_queue, queued, success):
+        podcast = PodcastFactory(
+            queued=timezone.now() if queued else None, promoted=True
+        )
+
+        count = scheduler.schedule_podcast_feeds(Podcast.objects.all())
+
+        if success:
+            assert count == 1
+            assert podcast.id in mock_feed_queue.enqueued
+        else:
+            assert count == 0
+            assert podcast.id not in mock_feed_queue.enqueued
+
+
+class TestSchedulePrimaryFeeds:
     @pytest.mark.parametrize(
         "promoted,followed,success",
         [
@@ -183,99 +206,8 @@ class TestSchedulePrimaryFeeds:
             assert count == 0
             assert podcast.id not in mock_feed_queue.enqueued
 
-    @pytest.mark.parametrize(
-        "queued,success",
-        [
-            (False, True),
-            (True, False),
-        ],
-    )
-    def test_queued(self, db, mock_feed_queue, queued, success):
-        podcast = PodcastFactory(
-            queued=timezone.now() if queued else None, promoted=True
-        )
-
-        count = scheduler.schedule_primary_feeds()
-
-        if success:
-            assert count == 1
-            assert podcast.id in mock_feed_queue.enqueued
-        else:
-            assert count == 0
-            assert podcast.id not in mock_feed_queue.enqueued
-
 
 class TestScheduleSecondaryFeeds:
-    @pytest.mark.parametrize(
-        "pub_date,after,before,success",
-        [
-            (None, None, None, True),
-            (None, timedelta(days=14), None, True),
-            (None, None, timedelta(days=7), True),
-            (timedelta(days=7), None, None, True),
-            (timedelta(days=7), timedelta(days=14), None, True),
-            (timedelta(days=7), timedelta(days=14), None, True),
-            (timedelta(days=30), timedelta(days=14), None, False),
-            (timedelta(days=7), timedelta(days=14), timedelta(hours=3), True),
-            (timedelta(days=7), timedelta(days=14), timedelta(days=10), False),
-            (timedelta(days=30), None, timedelta(days=14), True),
-            (timedelta(days=9), None, timedelta(days=14), False),
-        ],
-    )
-    def test_time_values(
-        self,
-        db,
-        mock_feed_queue,
-        pub_date,
-        after,
-        before,
-        success,
-    ):
-
-        now = timezone.now()
-
-        podcast = PodcastFactory(
-            pub_date=now - pub_date if pub_date else None,
-        )
-
-        count = scheduler.schedule_secondary_feeds(after=after, before=before)
-
-        assert Podcast.objects.filter(queued__isnull=False).exists() is success, (
-            pub_date,
-            after,
-            before,
-            success,
-        )
-
-        if success:
-            assert count == 1
-            assert podcast.id in mock_feed_queue.enqueued
-        else:
-            assert count == 0
-            assert podcast.id not in mock_feed_queue.enqueued
-
-    @pytest.mark.parametrize(
-        "active,success",
-        [
-            (True, True),
-            (False, False),
-        ],
-    )
-    def test_active(self, db, mock_feed_queue, active, success):
-
-        podcast = PodcastFactory(active=active, pub_date=None)
-
-        count = scheduler.schedule_secondary_feeds()
-
-        Podcast.objects.filter(queued__isnull=False).exists() is success
-
-        if success:
-            assert count == 1
-            assert podcast.id in mock_feed_queue.enqueued
-        else:
-            assert count == 0
-            assert podcast.id not in mock_feed_queue.enqueued
-
     @pytest.mark.parametrize(
         "promoted,followed,success",
         [
@@ -302,27 +234,6 @@ class TestScheduleSecondaryFeeds:
         count = scheduler.schedule_secondary_feeds()
 
         Podcast.objects.filter(queued__isnull=False).exists() is success
-
-        if success:
-            assert count == 1
-            assert podcast.id in mock_feed_queue.enqueued
-        else:
-            assert count == 0
-            assert podcast.id not in mock_feed_queue.enqueued
-
-    @pytest.mark.parametrize(
-        "queued,success",
-        [
-            (False, True),
-            (True, False),
-        ],
-    )
-    def test_queued(self, db, mock_feed_queue, queued, success):
-        podcast = PodcastFactory(
-            queued=timezone.now() if queued else None, pub_date=None
-        )
-
-        count = scheduler.schedule_secondary_feeds()
 
         if success:
             assert count == 1
