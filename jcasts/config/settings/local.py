1aa037a1821a6cf68a61039d6734d3392df96f55
Dan Jacob <danjac2018@gmail.com>
2021-12-19 22:32:42 +0200

    Remove redundant docker settings

--- jcasts/config/settings/local.py
+++ jcasts/config/settings/local.py
@@ -1,5 +1,3 @@
-import socket
-
 from split_settings.tools import include
 
 from jcasts.config.settings.base import INSTALLED_APPS, TEMPLATES
@@ -11,7 +9,3 @@ THUMBNAIL_DEBUG = True
 TEMPLATES[0]["OPTIONS"]["debug"] = True
 
 INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS
-
-# docker internal ips
-
-_, _, INTERNAL_IPS = socket.gethostbyname_ex(socket.gethostname())  # type: ignore
