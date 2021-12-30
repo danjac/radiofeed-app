50b3e35a7a75cd1bb165c5eef36ebe8c52524911
Dan Jacob <danjac2018@gmail.com>
2021-12-30 19:44:03 +0200

    Replace Docker settings

--- jcasts/config/settings/local.py
+++ jcasts/config/settings/local.py
@@ -1,3 +1,5 @@
+import socket
+
 from split_settings.tools import include
 
 from jcasts.config.settings.base import INSTALLED_APPS, TEMPLATES
@@ -9,3 +11,7 @@ THUMBNAIL_DEBUG = True
 TEMPLATES[0]["OPTIONS"]["debug"] = True
 
 INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS
+
+# internal Docker IP
+
+_, _, INTERNAL_IPS = socket.gethostbyname_ex(socket.gethostname())  # type: ignore
