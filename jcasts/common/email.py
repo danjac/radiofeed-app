7782c42d03102bb56b50abd18a886372a981a16e
danjac <danjac2018@gmail.com>
2022-02-09 13:38:14 +0200

    Add typing to email module

--- jcasts/common/email.py
+++ jcasts/common/email.py
@@ -1,11 +1,13 @@
+from __future__ import annotations
+
 from django.conf import settings
-from django.core.mail import get_connection
+from django.core.mail import EmailMessage, get_connection
 from django.core.mail.backends.base import BaseEmailBackend
 from django_rq import job
 
 
 class RqBackend(BaseEmailBackend):
-    def send_messages(self, email_messages):
+    def send_messages(self, email_messages: list[EmailMessage]) -> int:
 
         for message in email_messages:
             send_rq_message.delay(message)
@@ -14,6 +16,6 @@ class RqBackend(BaseEmailBackend):
 
 
 @job(settings.RQ_EMAIL_QUEUE)
-def send_rq_message(message):
+def send_rq_message(message: EmailMessage) -> None:
     message.connection = get_connection(settings.RQ_EMAIL_BACKEND, fail_silently=False)
     message.send(fail_silently=False)
