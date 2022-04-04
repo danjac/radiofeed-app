a6200f8a98b66e42839a89f20dfe45ca191bfebf
Dan Jacob <danjac2018@gmail.com>
2022-04-04 19:12:10 +0300

    Remove dokku content

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: python manage.py migrate
 web: gunicorn --workers=5 --max-requests=1000 --max-requests-jitter=50 jcasts.wsgi
 worker: python manage.py rqworker mail default feeds feeds:frequent feeds:sporadic
--- roles/pg_vacuum/files/pg-vacuum.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for v in `docker ps --format '{{.Names}}\t{{.Status}}' | grep postgres | awk '{ print $1 }'`
-do
-  echo $v
-  NNN=$v
-  docker exec -it $NNN vacuumdb --all -U postgres --full --analyze
-done
