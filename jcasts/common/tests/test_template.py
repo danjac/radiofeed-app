7863c21c1059e8bbf02e1955f6f349f79f36f396
Dan Jacob <danjac2018@gmail.com>
2022-04-04 23:20:50 +0300

    Formatting fix

--- jcasts/common/tests/test_template.py
+++ jcasts/common/tests/test_template.py
@@ -1,3 +1,5 @@
+import pytest
+
 from django.urls import reverse
 
 from jcasts.common.template import (
@@ -81,21 +83,19 @@ class TestAbsoluteUri:
 
 
 class TestFormatDuration:
-    def test_format_duration_if_empty(self):
-        assert format_duration(None) == ""
-        assert format_duration(0) == ""
-
-    def test_format_duration_if_less_than_one_minute(self):
-        assert format_duration(30) == ""
-
-    def test_format_duration_if_less_than_ten_minutes(self):
-        assert format_duration(540) == "9min"
-
-    def test_format_duration_if_less_than_one_hour(self):
-        assert format_duration(2400) == "40min"
-
-    def test_format_duration_if_more_than_one_hour(self):
-        assert format_duration(9000) == "2h 30min"
+    @pytest.mark.parametrize(
+        "duration,expected",
+        [
+            (None, ""),
+            (0, ""),
+            (30, ""),
+            (540, "9min"),
+            (2400, "40min"),
+            (9000, "2h 30min"),
+        ],
+    )
+    def test_format_duration(self, duration, expected):
+        assert format_duration(duration) == expected
 
 
 class TestIcon:
