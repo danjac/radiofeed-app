23f26cb4c9c96d676576d84f4fd4c99895a1909e
Dan Jacob <danjac2018@gmail.com>
2022-04-05 11:00:39 +0300

    Settings tidyup

--- jcasts/settings/local.py
+++ jcasts/settings/local.py
@@ -2,13 +2,15 @@ import socket
 
 from split_settings.tools import include
 
-from jcasts.settings.base import ADMIN_SITE_HEADER, INSTALLED_APPS
+from jcasts.settings.base import ADMIN_SITE_HEADER, INSTALLED_APPS, TEMPLATES
 
 include("base.py")
 
 ADMIN_SITE_HEADER += " [LOCAL DEVELOPMENT]"
 
 DEBUG = True
+
+TEMPLATES[0]["OPTIONS"]["debug"] = True
 
 INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS
 
