b1c01eefc65ca741d6b033034401a169fffc3a04
Dan Jacob <danjac2018@gmail.com>
2022-04-30 10:31:12 +0300

    Remove redundant test

--- podtracker/podcasts/tests/test_emails.py
+++ podtracker/podcasts/tests/test_emails.py
@@ -24,19 +24,3 @@ class TestSendRecommendationEmail:
         assert len(mailoutbox) == 1
         assert mailoutbox[0].to == [user.email]
         assert user.recommended_podcasts.count() == 3
-
-    def test_send_if_sufficient_recommendations_and_episodes(self, user, mailoutbox):
-
-        first = SubscriptionFactory(user=user).podcast
-        second = SubscriptionFactory(user=user).podcast
-        third = SubscriptionFactory(user=user).podcast
-
-        RecommendationFactory(podcast=first)
-        RecommendationFactory(podcast=second)
-        RecommendationFactory(podcast=third)
-
-        send_recommendations_email(user)
-
-        assert len(mailoutbox) == 1
-        assert mailoutbox[0].to == [user.email]
-        assert user.recommended_podcasts.count() == 3
