a5f88d86ddc2312c781778305a469bc3a7572a63
Dan Jacob <danjac2018@gmail.com>
2022-05-02 14:58:41 +0300

    Add recommendations job

--- podtracker/episodes/emails.py
+++ podtracker/episodes/emails.py
@@ -9,6 +9,12 @@ from podtracker.users.emails import send_user_notification_email
 from podtracker.users.models import User
 
 
+@job
+def send_new_episodes_emails(since: timedelta = timedelta(days=7)):
+    for user in User.objects.filter(send_email_notifications=True, is_active=True):
+        send_new_episodes_email.delay(user, since)
+
+
 @job("mail")
 def send_new_episodes_email(user: User, since: timedelta) -> None:
     """Sends email with new episodes added to user's collection."""
--- podtracker/episodes/management/commands/send_new_episodes_emails.py
+++ podtracker/episodes/management/commands/send_new_episodes_emails.py
@@ -1,16 +1,12 @@
 from __future__ import annotations
 
-from datetime import timedelta
-
 from django.core.management.base import BaseCommand
 
-from podtracker.episodes.emails import send_new_episodes_email
-from podtracker.users.models import User
+from podtracker.episodes.emails import send_new_episodes_emails
 
 
 class Command(BaseCommand):
     help = "Email podcast recommendations to users"
 
     def handle(self, *args, **options) -> None:
-        for user in User.objects.filter(send_email_notifications=True, is_active=True):
-            send_new_episodes_email.delay(user, timedelta(days=7))
+        send_new_episodes_emails()
