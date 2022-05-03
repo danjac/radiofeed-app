ae992f47f19e240aee9615555995c1d09ad673f6
Dan Jacob <danjac2018@gmail.com>
2022-05-03 10:41:04 +0300

    Define all tasks

--- podtracker/episodes/tasks.py
+++ podtracker/episodes/tasks.py
@@ -2,10 +2,23 @@ from __future__ import annotations
 
 from datetime import timedelta
 
-from podtracker.episodes.emails import send_new_episodes_email
+from huey import crontab
+from huey.contrib.djhuey import db_periodic_task, db_task
+
+from podtracker.episodes import emails
 from podtracker.users.models import User
 
 
-def send_new_episodes_emails(since: timedelta = timedelta(days=7)):
+@db_periodic_task(crontab(hour=6, minute=17, day_of_week=3))
+def send_new_episodes_emails():
+    since = timedelta(days=7)
     for user in User.objects.filter(send_email_notifications=True, is_active=True):
-        send_new_episodes_email(user, since)
+        send_new_episodes_email(user, since)()
+
+
+@db_task()
+def send_new_episodes_email(user_id: int, since: timedelta) -> None:
+    try:
+        emails.send_new_episodes_email(User.objects.get(pk=user_id), since)
+    except User.DoesNotExist:
+        pass
--- podtracker/podcasts/tasks.py
+++ podtracker/podcasts/tasks.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from huey import crontab
 from huey.contrib.djhuey import db_periodic_task, db_task
 
@@ -6,14 +8,49 @@ from podtracker.podcasts.parsers import feed_parser
 from podtracker.users.models import User
 
 
-@db_task()
+@db_periodic_task(crontab(hour=3, minute=20))
 def recommend() -> None:
     recommender.recommend()
 
 
-@db_task()
+@db_periodic_task(crontab(hour=5, minute=12))
 def crawl_itunes() -> None:
     itunes.crawl()
+
+
+@db_periodic_task(crontab(hour=9, minute=12, day_of_week=1))
+def send_recommendations_emails() -> None:
+    for user_id in User.objects.filter(
+        send_email_notifications=True, is_active=True
+    ).values_list("pk", flat=True):
+        send_recommendations_email(user_id)()
+
+
+@db_periodic_task(crontab(minute="*/12"))
+def schedule_primary_feeds() -> None:
+    for podcast_id in scheduler.schedule_primary_feeds(limit=300):
+        parse_podcast_feed(podcast_id)()
+
+
+@db_periodic_task(crontab(minute="*/6"))
+def schedule_frequent_feeds() -> None:
+    for podcast_id in scheduler.schedule_secondary_feeds(
+        after=timedelta(hours=336), before=timedelta(hours=3), limit=300
+    ):
+        parse_podcast_feed(podcast_id)()
+
+
+@db_periodic_task(crontab(minute="15,45"))
+def schedule_sporadic_feeds() -> None:
+    for podcast_id in scheduler.schedule_secondary_feeds(
+        before=timedelta(hours=336), limit=300
+    ):
+        parse_podcast_feed(podcast_id)()
+
+
+@db_task()
+def parse_podcast_feed(podcast_id: int) -> None:
+    feed_parser.parse_podcast_feed(podcast_id)
 
 
 @db_task()
@@ -22,29 +59,3 @@ def send_recommendations_email(user_id: int) -> None:
         emails.send_recommendations_email(User.objects.get(pk=user_id))
     except User.DoesNotExist:
         pass
-
-
-@db_task()
-def send_recommendations_emails() -> None:
-    for user_id in User.objects.filter(
-        send_email_notifications=True, is_active=True
-    ).values_list("pk", flat=True):
-        send_recommendations_email(user_id)()
-
-
-@db_task()
-def parse_podcast_feed(podcast_id: int) -> None:
-    print("parsing podcast feed for real...", podcast_id)
-    feed_parser.parse_podcast_feed(podcast_id)
-
-
-@db_periodic_task(crontab(minute="*/10"))
-def schedule_primary_feeds(**kwargs) -> None:
-    for podcast_id in scheduler.schedule_primary_feeds(**kwargs):
-        parse_podcast_feed(podcast_id)()
-
-
-@db_periodic_task(crontab(hour="*/1", minute="0"))
-def schedule_secondary_feeds(**kwargs) -> None:
-    for podcast_id in scheduler.schedule_secondary_feeds(**kwargs):
-        parse_podcast_feed(podcast_id)()
