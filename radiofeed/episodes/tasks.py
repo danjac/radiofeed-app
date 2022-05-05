9934634f8025207da3617b15d2be0f659f135d28
Dan Jacob <danjac2018@gmail.com>
2022-05-05 11:27:05 +0300

    Add comments to tasks

--- radiofeed/episodes/tasks.py
+++ radiofeed/episodes/tasks.py
@@ -11,6 +11,11 @@ from radiofeed.users.models import User
 
 @db_periodic_task(crontab(hour=6, minute=17, day_of_week=3))
 def send_new_episodes_emails():
+    """
+    Sends new episodes from users' subscribed podcasts
+
+    Runs 6:17 UTC every Wednesday
+    """
     since = timedelta(days=7)
     for user in User.objects.filter(send_email_notifications=True, is_active=True):
         send_new_episodes_email(user.id, since)()
--- radiofeed/podcasts/tasks.py
+++ radiofeed/podcasts/tasks.py
@@ -16,17 +16,32 @@ logger = logging.getLogger(__name__)
 
 @db_periodic_task(crontab(hour=3, minute=20))
 def recommend() -> None:
+    """
+    Generates podcast recommendations
+
+    Runs 3:20 UTC every day
+    """
     recommender.recommend()
 
 
 @db_periodic_task(crontab(hour=5, minute=12))
 def crawl_itunes() -> None:
+    """
+    Crawls iTunes site for new podcasts
+
+    Runs at 5:12 UTC every day
+    """
     for feed in itunes.crawl():
         logger.debug(feed)
 
 
 @db_periodic_task(crontab(hour=9, minute=12, day_of_week=1))
 def send_recommendations_emails() -> None:
+    """
+    Sends recommended podcasts to users
+
+    Runs at 9:12 UTC every Monday
+    """
     for user_id in User.objects.filter(
         send_email_notifications=True, is_active=True
     ).values_list("pk", flat=True):
@@ -35,12 +50,22 @@ def send_recommendations_emails() -> None:
 
 @db_periodic_task(crontab(minute="*/12"))
 def schedule_primary_feeds() -> None:
+    """
+    Schedule all subscribed or promoted feeds
+
+    Runs every 12 minutes
+    """
     for podcast_id in scheduler.schedule_primary_feeds(limit=300):
         parse_podcast_feed(podcast_id)()
 
 
 @db_periodic_task(crontab(minute="*/6"))
 def schedule_frequent_feeds() -> None:
+    """
+    Schedule podcast feeds newer than 2 weeks
+
+    Runs every 6 minutes
+    """
     for podcast_id in scheduler.schedule_secondary_feeds(
         after=timedelta(hours=336), before=timedelta(hours=3), limit=300
     ):
@@ -49,6 +74,11 @@ def schedule_frequent_feeds() -> None:
 
 @db_periodic_task(crontab(minute="15,45"))
 def schedule_sporadic_feeds() -> None:
+    """
+    Schedule podcast feeds older than 2 weeks
+
+    Runs at 15 and 45 minutes past the hour
+    """
     for podcast_id in scheduler.schedule_secondary_feeds(
         before=timedelta(hours=336), limit=300
     ):
