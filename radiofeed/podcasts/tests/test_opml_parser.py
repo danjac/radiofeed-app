e6b7c6519b6502d2db5278bd059096610b7cb2cf
Dan Jacob <danjac2018@gmail.com>
2022-05-11 17:56:13 +0300

    Add tests

---
+++ radiofeed/podcasts/tests/test_opml_parser.py
@@ -0,0 +1,20 @@
+import pathlib
+
+import pytest
+
+from radiofeed.podcasts.parsers import opml_parser
+
+
+class TestOpmlParser:
+    def test_empty(self):
+        with pytest.raises(opml_parser.OpmlParserError):
+            list(opml_parser.parse_opml(b""))
+
+    def test_invalid(self):
+        outlines = list(opml_parser.parse_opml(b"not opml content"))
+        assert len(outlines) == 0
+
+    def test_ok(self):
+        content = (pathlib.Path(__file__).parent / "mocks" / "feeds.opml").read_bytes()
+        outlines = list(opml_parser.parse_opml(content))
+        assert len(outlines) == 12
