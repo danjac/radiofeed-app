04d84f3dc9568bf97b96120c8eac51a0b99439fa
Dan Jacob <danjac2018@gmail.com>
2022-05-19 00:47:24 +0300

    Tidy up error render

--- radiofeed/podcasts/parsers/feed_parser.py
+++ radiofeed/podcasts/parsers/feed_parser.py
@@ -356,7 +356,7 @@ class FeedParser:
 
         return min(
             timedelta(seconds=seconds + (seconds * 0.1)),
-            timedelta(hours=24),
+            timedelta(hours=6),
         )
 
 
