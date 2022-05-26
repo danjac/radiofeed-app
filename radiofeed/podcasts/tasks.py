33be6b8f4bb325ee49f00479248ab4141e9fab1a
Dan Jacob <danjac2018@gmail.com>
2022-05-26 13:07:17 +0300

    Increase throughput

--- radiofeed/podcasts/tasks.py
+++ radiofeed/podcasts/tasks.py
@@ -40,7 +40,7 @@ def send_recommendations_emails() -> None:
 
 
 @db_periodic_task(crontab(minute="*/6"))
-def schedule_podcast_feeds(limit: int = 120) -> None:
+def schedule_podcast_feeds(limit: int = 360) -> None:
     """Schedules podcast feeds for update
 
     Runs every 6 minutes
