b5666065ec7bce3830bad3a89f1b43bdd4ad6351
Dan Jacob <danjac2018@gmail.com>
2022-06-04 11:51:36 +0300

    Add podcast tasks

--- radiofeed/podcasts/tasks.py
+++ radiofeed/podcasts/tasks.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from radiofeed.podcasts import emails, recommender  # , scheduler
+from celery import shared_task
+
+from radiofeed.podcasts import emails, recommender, scheduler
 from radiofeed.podcasts.models import Podcast
 from radiofeed.podcasts.parsers import feed_parser
 from radiofeed.users.models import User
 
 
+@shared_task
 def recommend() -> None:
     """
     Generates podcast recommendations
@@ -15,31 +18,35 @@ def recommend() -> None:
     recommender.recommend()
 
 
+@shared_task
 def send_recommendations_emails() -> None:
     """
     Sends recommended podcasts to users
 
     Runs at 09:12 UTC every Monday
     """
-    # send_recommendations_email.map(
-    # User.objects.filter(
-    # send_email_notifications=True,
-    # is_active=True,
-    # ).values_list("pk")
-    # )
+    for user_id in User.objects.filter(
+        send_email_notifications=True,
+        is_active=True,
+    ).values_list("pk", flat=True):
+        send_recommendations_email.delay(user_id)
 
 
+@shared_task
 def schedule_podcast_feeds(limit: int = 300) -> None:
     """Schedules podcast feeds for update.
 
     Runs every 6 minutes
     """
+    for podcast_id in (
+        scheduler.schedule_podcasts_for_update()
+        .values_list("pk", flat=True)
+        .distinct()[:limit]
+    ):
+        parse_podcast_feed.delay(podcast_id)
 
-    # parse_podcast_feed.map(
-    # scheduler.schedule_podcasts_for_update().values_list("pk").distinct()[:limit]
-    # )
 
-
+@shared_task
 def parse_podcast_feed(podcast_id: int) -> None:
     try:
         feed_parser.parse_podcast_feed(Podcast.objects.get(pk=podcast_id))
@@ -47,6 +54,7 @@ def parse_podcast_feed(podcast_id: int) -> None:
         pass
 
 
+@shared_task
 def send_recommendations_email(user_id: int) -> None:
     try:
         emails.send_recommendations_email(User.objects.get(pk=user_id))
