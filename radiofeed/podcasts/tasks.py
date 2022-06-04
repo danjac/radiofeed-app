61de52392421830c4708ed0df97bc19c59ac7f3e
Dan Jacob <danjac2018@gmail.com>
2022-06-04 15:24:10 +0300

    Run every 6 minutes

--- radiofeed/podcasts/tasks.py
+++ radiofeed/podcasts/tasks.py
@@ -34,11 +34,11 @@ def send_recommendations_emails() -> None:
     )
 
 
-@db_periodic_task(crontab(minute="*/12"))
-def schedule_podcast_feeds(limit: int = 300) -> None:
+@db_periodic_task(crontab(minute="*/6"))
+def schedule_podcast_feeds(limit: int = 180) -> None:
     """Schedules podcast feeds for update.
 
-    Runs every 12 minutes
+    Runs every 6 minutes
     """
 
     parse_podcast_feed.map(
