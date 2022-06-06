2dc86ef440c4686a1799e99236e6d24b9edbd2e1
Dan Jacob <danjac2018@gmail.com>
2022-06-06 22:55:56 +0300

    Prioritize promoted or subscribed

--- radiofeed/podcasts/scheduler.py
+++ radiofeed/podcasts/scheduler.py
@@ -22,34 +22,40 @@ def schedule_podcasts_for_update() -> models.QuerySet[Podcast]:
     """
     now = timezone.now()
 
-    return Podcast.objects.filter(
-        models.Q(parsed__isnull=True)
-        | models.Q(pub_date__isnull=True)
-        | models.Q(
-            pub_date__lt=now - timedelta(days=14),
-            parsed__lt=now - timedelta(days=7),
-        )
-        | models.Q(
-            pub_date__range=(
-                now - timedelta(days=14),
-                now - timedelta(days=7),
+    return (
+        Podcast.objects.annotate(subscribers=models.Count("subscription"))
+        .filter(
+            models.Q(parsed__isnull=True)
+            | models.Q(pub_date__isnull=True)
+            | models.Q(
+                pub_date__lt=now - timedelta(days=14),
+                parsed__lt=now - timedelta(days=7),
+            )
+            | models.Q(
+                pub_date__range=(
+                    now - timedelta(days=14),
+                    now - timedelta(days=7),
+                ),
+                parsed__lt=now - timedelta(hours=24),
+            )
+            | models.Q(
+                pub_date__range=(
+                    now - timedelta(days=7),
+                    now - timedelta(hours=24),
+                ),
+                parsed__lt=now - timedelta(hours=3),
+            )
+            | models.Q(
+                pub_date__gt=now - timedelta(hours=24),
+                parsed__lt=now - timedelta(hours=1),
             ),
-            parsed__lt=now - timedelta(hours=24),
+            active=True,
         )
-        | models.Q(
-            pub_date__range=(
-                now - timedelta(days=7),
-                now - timedelta(hours=24),
-            ),
-            parsed__lt=now - timedelta(hours=3),
+        .order_by(
+            models.F("subscribers").desc(),
+            models.F("promoted").desc(),
+            models.F("parsed").asc(nulls_first=True),
+            models.F("pub_date").desc(nulls_first=True),
+            models.F("created").desc(),
         )
-        | models.Q(
-            pub_date__gt=now - timedelta(hours=24),
-            parsed__lt=now - timedelta(hours=1),
-        ),
-        active=True,
-    ).order_by(
-        models.F("parsed").asc(nulls_first=True),
-        models.F("pub_date").desc(nulls_first=True),
-        models.F("created").desc(),
     )
