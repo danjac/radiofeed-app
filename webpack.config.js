9f2f47106e521badc71d5078aef5b77d365d028a
Dan Jacob <danjac2018@gmail.com>
2022-06-15 19:14:40 +0300

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    main: "./static/js/app.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, './static/dist'),
+    filename: "bundle.js" // <--- Will be compiled to this single file
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
