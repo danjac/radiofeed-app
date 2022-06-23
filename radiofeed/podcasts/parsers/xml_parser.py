e4d30ae07671602fcf0f7d097ddbab2f7036422f
Dan Jacob <danjac2018@gmail.com>
2022-06-23 09:13:51 +0300

    Refactor converters

--- radiofeed/podcasts/parsers/xml_parser.py
+++ radiofeed/podcasts/parsers/xml_parser.py
@@ -46,7 +46,7 @@ class XPath:
     def first(
         self,
         *paths: str,
-        converter: Callable[[str | None], T | str] | None = None,
+        converter: Callable[[str | None], T] | None = None,
         default: T | str = "",
         required: bool = False,
     ) -> T | str:
