70868414ecc4115847dbd44339a65536270257e9
Dan Jacob <danjac2018@gmail.com>
2022-06-24 23:34:50 +0300

    Remove typescript

--- radiofeed/podcasts/tasks.py
+++ radiofeed/podcasts/tasks.py
@@ -6,12 +6,12 @@ from radiofeed.users.models import User
 
 
 @db_task()
-def feed_update(podcast_id: int) -> None:
+def feed_update(podcast_id):
     feed_updater.FeedUpdater(Podcast.objects.get(pk=podcast_id)).update()
 
 
 @db_task()
-def send_recommendations_email(user_id: int) -> None:
+def send_recommendations_email(user_id):
     emails.send_recommendations_email(
         User.objects.email_notification_recipients().get(pk=user_id)
     )
