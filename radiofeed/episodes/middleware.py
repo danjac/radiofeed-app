09758437c6e7a3aff6e8d058415646396eba12bc
Dan Jacob <danjac2018@gmail.com>
2022-06-24 23:17:58 +0300

    Remove annotations

--- radiofeed/episodes/middleware.py
+++ radiofeed/episodes/middleware.py
@@ -1,4 +1,3 @@
-from django.http import HttpRequest, HttpResponse
 from django.utils.functional import SimpleLazyObject
 
 from radiofeed.common.middleware import BaseMiddleware
@@ -9,23 +8,23 @@ class Player:
 
     session_key = "player_episode"
 
-    def __init__(self, request: HttpRequest):
+    def __init__(self, request):
         self.request = request
 
-    def get(self) -> int | None:
+    def get(self):
         return self.request.session.get(self.session_key)
 
-    def set(self, episode_id: int) -> None:
+    def set(self, episode_id):
         self.request.session[self.session_key] = episode_id
 
-    def has(self, episode_id: int) -> bool:
+    def has(self, episode_id):
         return self.get() == episode_id
 
-    def pop(self) -> int | None:
+    def pop(self):
         return self.request.session.pop(self.session_key, None)
 
 
 class PlayerMiddleware(BaseMiddleware):
-    def __call__(self, request: HttpRequest) -> HttpResponse:
+    def __call__(self, request):
         request.player = SimpleLazyObject(lambda: Player(request))
         return self.get_response(request)
