391896ba478c345b6edd9d41afcd84c94730cb2c
Dan Jacob <danjac2018@gmail.com>
2022-06-25 23:34:05 +0300

    Tidyup

--- radiofeed/podcasts/feed_updater.py
+++ radiofeed/podcasts/feed_updater.py
@@ -38,6 +38,54 @@ class FeedUpdater:
         except Exception as e:
             return self.handle_failure(e)
 
+    def parse_rss(self):
+        response = requests.get(
+            self.podcast.rss,
+            headers=self.get_feed_headers(),
+            allow_redirects=True,
+            timeout=10,
+        )
+
+        response.raise_for_status()
+
+        if response.status_code == http.HTTPStatus.NOT_MODIFIED:
+            raise NotModified(response=response)
+
+        # check if another feed already has new URL
+        if (
+            response.url != self.podcast.rss
+            and Podcast.objects.filter(rss=response.url).exists()
+        ):
+            raise DuplicateFeed(response=response)
+
+        # check if content has changed (feed is not checking etag etc)
+        if (
+            content_hash := make_content_hash(response.content)
+        ) == self.podcast.content_hash:
+            raise NotModified(response=response)
+
+        # check if another feed has exact same content
+        if (
+            Podcast.objects.exclude(pk=self.podcast.id).filter(
+                content_hash=content_hash, active=True
+            )
+        ).exists():
+            raise DuplicateFeed(response=response)
+
+        return response, rss_parser.parse_rss(response.content), content_hash
+
+    def get_feed_headers(self):
+        headers = {
+            "Accept": ACCEPT_HEADER,
+            "User-Agent": get_user_agent(),
+        }
+
+        if self.podcast.etag:
+            headers["If-None-Match"] = quote_etag(self.podcast.etag)
+        if self.podcast.modified:
+            headers["If-Modified-Since"] = http_date(self.podcast.modified.timestamp())
+        return headers
+
     def handle_success(self, response, feed, content_hash):
 
         # taxonomy
@@ -80,6 +128,21 @@ class FeedUpdater:
 
         return True
 
+    def extract_text(self, feed, categories):
+        text = " ".join(
+            value
+            for value in [
+                self.podcast.title,
+                self.podcast.description,
+                self.podcast.keywords,
+                self.podcast.owner,
+            ]
+            + [c.name for c in categories]
+            + [item.title for item in feed.items][:6]
+            if value
+        )
+        return " ".join(text_parser.extract_keywords(self.podcast.language, text))
+
     def handle_failure(self, exc):
         try:
             http_status = exc.response.status_code
@@ -103,43 +166,6 @@ class FeedUpdater:
 
         self.save_podcast(active=active, http_status=http_status)
         return False
-
-    def parse_rss(self):
-
-        response = requests.get(
-            self.podcast.rss,
-            headers=self.get_feed_headers(),
-            allow_redirects=True,
-            timeout=10,
-        )
-
-        response.raise_for_status()
-
-        if response.status_code == http.HTTPStatus.NOT_MODIFIED:
-            raise NotModified(response=response)
-
-        # check if another feed already has new URL
-        if (
-            response.url != self.podcast.rss
-            and Podcast.objects.filter(rss=response.url).exists()
-        ):
-            raise DuplicateFeed(response=response)
-
-        # check if content has changed (feed is not checking etag etc)
-        if (
-            content_hash := make_content_hash(response.content)
-        ) == self.podcast.content_hash:
-            raise NotModified(response=response)
-
-        # check if another feed has exact same content
-        if (
-            Podcast.objects.exclude(pk=self.podcast.id).filter(
-                content_hash=content_hash, active=True
-            )
-        ).exists():
-            raise DuplicateFeed(response=response)
-
-        return response, rss_parser.parse_rss(response.content), content_hash
 
     def save_podcast(self, **fields):
         now = timezone.now()
@@ -208,33 +234,6 @@ class FeedUpdater:
             podcast=self.podcast,
             **attrs.asdict(item),
         )
-
-    def extract_text(self, feed, categories):
-        text = " ".join(
-            value
-            for value in [
-                self.podcast.title,
-                self.podcast.description,
-                self.podcast.keywords,
-                self.podcast.owner,
-            ]
-            + [c.name for c in categories]
-            + [item.title for item in feed.items][:6]
-            if value
-        )
-        return " ".join(text_parser.extract_keywords(self.podcast.language, text))
-
-    def get_feed_headers(self):
-        headers = {
-            "Accept": ACCEPT_HEADER,
-            "User-Agent": get_user_agent(),
-        }
-
-        if self.podcast.etag:
-            headers["If-None-Match"] = quote_etag(self.podcast.etag)
-        if self.podcast.modified:
-            headers["If-Modified-Since"] = http_date(self.podcast.modified.timestamp())
-        return headers
 
 
 @functools.lru_cache
