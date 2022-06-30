0a0923e7320572849b5947f4eb96cea6231d19b7
Dan Jacob <danjac2018@gmail.com>
2022-06-30 12:29:19 +0300

    Add comments

--- radiofeed/common/html.py
+++ radiofeed/common/html.py
@@ -66,6 +66,14 @@ def linkify_callback(attrs, new=False):
 
 
 def clean(value):
+    """Runs Bleach through value and scrubs any unwanted HTML tags and attributes.
+
+    Args:
+        value (str)
+
+    Returns:
+        str
+    """
     return (
         bleach.linkify(
             bleach.clean(
@@ -82,20 +90,52 @@ def clean(value):
 
 
 def strip_whitespace(value):
+    """Removes all trailing whitespace. Any None value just returns an empty string.
+
+    Args:
+        value (str | None)
+
+    Returns:
+        str
+    """
     return (value or "").strip()
 
 
 def strip_html(value):
-    """Removes all HTML tags and entities"""
+    """Removes all HTML tags and entities from text.
+
+    Args:
+        value (str | None): text to be cleaned
+
+    Returns:
+        str
+    """
     return html.unescape(striptags(strip_whitespace(value)))
 
 
 def as_html(value):
+    """Checks if content contains any HTML tags. If not, will try and
+    parse Markdown from text.
+
+    Args:
+        value (str)
+
+    Returns:
+        str: HTML content
+    """
     return value if HTML_RE.match(value) else markdown.markdown(value)
 
 
 def markup(value):
-    """Parses Markdown and/or html and returns cleaned result."""
+    """Parses Markdown and/or html and returns cleaned result.
+
+    Args:
+        value (str | None)
+
+    Returns:
+        str
+
+    """
     if value := strip_whitespace(value):
         return html.unescape(clean(as_html(value)))
     return ""
