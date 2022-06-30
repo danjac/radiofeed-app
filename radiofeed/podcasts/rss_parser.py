627889ab9e7c907c4d30ee327bca199f2724c012
Dan Jacob <danjac2018@gmail.com>
2022-06-30 19:04:04 +0300

    Change module names

---
+++ radiofeed/podcasts/rss_parser.py
@@ -0,0 +1,349 @@
+from datetime import datetime
+
+import attrs
+import lxml.etree
+
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+from django.utils import timezone
+
+from radiofeed.common.parsers import date_parser, xml_parser
+
+NAMESPACES = {
+    "atom": "http://www.w3.org/2005/Atom",
+    "content": "http://purl.org/rss/1.0/modules/content/",
+    "itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd",
+    "media": "http://search.yahoo.com/mrss/",
+    "podcast": "https://podcastindex.org/namespace/1.0",
+}
+
+
+AUDIO_MIMETYPES = (
+    "audio/aac",
+    "audio/aacp",
+    "audio/basic",
+    "audio/m4a",
+    "audio/midi",
+    "audio/mp3",
+    "audio/mp4",
+    "audio/mp4a-latm",
+    "audio/mp4a-latm",
+    "audio/mpef",
+    "audio/mpeg",
+    "audio/mpeg3",
+    "audio/mpeg4",
+    "audio/mpg",
+    "audio/ogg",
+    "audio/video",
+    "audio/vnd.dlna.adts",
+    "audio/vnd.wave",
+    "audio/wav",
+    "audio/wave",
+    "audio/x-aac",
+    "audio/x-aiff",
+    "audio/x-aiff",
+    "audio/x-hx-aac-adts",
+    "audio/x-m4a",
+    "audio/x-m4a",
+    "audio/x-m4b",
+    "audio/x-m4v",
+    "audio/x-mov",
+    "audio/x-mp3",
+    "audio/x-mpeg",
+    "audio/x-mpg",
+    "audio/x-ms-wma",
+    "audio/x-pn-realaudio",
+    "audio/x-wav",
+)
+
+######## VALIDATORS ########
+
+# ensure integer falls within PostgreSQL INTEGER range
+
+pg_integer = attrs.validators.and_(
+    attrs.validators.gt(-2147483648),
+    attrs.validators.lt(2147483647),
+)
+
+
+_url_validator = URLValidator(["http", "https"])
+
+
+def required(instance, attr, value):
+    if not value:
+        raise ValueError(f"{attr=} cannot be empty or None")
+
+
+def url(instance, attr, value):
+    try:
+        _url_validator(value)
+    except ValidationError as e:
+        raise ValueError from e
+
+
+######## CONVERTERS ########
+
+
+def explicit(value):
+    if value and value.casefold() in ("clean", "yes"):
+        return True
+    return False
+
+
+def duration(value):
+    if not value:
+        return ""
+
+    try:
+        # plain seconds value
+        return str(int(value))
+    except ValueError:
+        pass
+
+    try:
+        return ":".join(
+            [
+                str(v)
+                for v in [int(v) for v in value.split(":")[:3]]
+                if v in range(0, 60)
+            ]
+        )
+    except ValueError:
+        return ""
+
+
+######## MODELS ########
+
+
+@attrs.define(kw_only=True, frozen=True)
+class Item:
+    """Individual item or episode in RSS or Atom podcast feed"""
+
+    guid: str = attrs.field(validator=required)
+    title: str = attrs.field(validator=required)
+
+    pub_date: datetime = attrs.field(converter=date_parser.parse_date)
+
+    media_url: str = attrs.field(validator=url)
+
+    media_type: str = attrs.field(validator=attrs.validators.in_(AUDIO_MIMETYPES))
+
+    link: str | None = attrs.field(
+        validator=attrs.validators.optional(url),
+        default=None,
+    )
+
+    explicit: bool = attrs.field(converter=explicit, default=False)
+
+    length: int | None = attrs.field(
+        converter=attrs.converters.optional(
+            attrs.converters.pipe(float, int),
+        ),
+        default=None,
+    )
+
+    season: int | None = attrs.field(
+        converter=attrs.converters.optional(
+            attrs.converters.pipe(float, int),
+        ),
+        validator=attrs.validators.optional(pg_integer),
+        default=None,
+    )
+
+    episode: int | None = attrs.field(
+        converter=attrs.converters.optional(
+            attrs.converters.pipe(float, int),
+        ),
+        validator=attrs.validators.optional(pg_integer),
+        default=None,
+    )
+
+    cover_url: str | None = attrs.field(
+        validator=attrs.validators.optional(url),
+        default=None,
+    )
+
+    duration: str = attrs.field(converter=duration, default=None)
+
+    episode_type: str = attrs.field(
+        converter=attrs.converters.default_if_none("full"),
+        default=None,
+    )
+
+    description: str = attrs.field(
+        converter=attrs.converters.default_if_none(""),
+        default=None,
+    )
+
+    keywords: str = attrs.field(
+        converter=attrs.converters.default_if_none(""),
+        default=None,
+    )
+
+    @pub_date.validator
+    def _pub_date_ok(self, attribute, value):
+        if value is None:
+            raise ValueError("pub_date cannot be null")
+        if value > timezone.now():
+            raise ValueError("pub_date cannot be in future")
+
+
+@attrs.define(kw_only=True, frozen=True)
+class Feed:
+    """RSS or Atom podcast feed"""
+
+    title: str = attrs.field(validator=required)
+
+    owner: str = attrs.field(
+        converter=attrs.converters.default_if_none(""),
+        default=None,
+    )
+    description: str = attrs.field(
+        converter=attrs.converters.default_if_none(""),
+        default=None,
+    )
+
+    language: str = attrs.field(
+        converter=attrs.converters.pipe(
+            attrs.converters.default_if_none("en"),
+            lambda value: value[:2],
+        ),
+        default=None,
+    )
+
+    link: str | None = attrs.field(
+        validator=attrs.validators.optional(url),
+        default=None,
+    )
+
+    cover_url: str | None = attrs.field(
+        validator=attrs.validators.optional(url),
+        default=None,
+    )
+
+    complete: bool = attrs.field(
+        converter=attrs.converters.pipe(
+            attrs.converters.default_if_none(False),
+            attrs.converters.to_bool,
+        ),
+        default=False,
+    )
+
+    explicit: bool = attrs.field(converter=explicit, default=False)
+
+    funding_text: str = attrs.field(
+        converter=attrs.converters.default_if_none(""), default=None
+    )
+
+    funding_url: str | None = attrs.field(
+        validator=attrs.validators.optional(url),
+        default=None,
+    )
+
+    categories: list[str] = attrs.field(default=attrs.Factory(list))
+
+    items: list[Item] = attrs.field(
+        default=attrs.Factory(list),
+        validator=required,
+    )
+
+    pub_date: datetime | None = attrs.field()
+
+    @pub_date.default
+    def _default_pub_date(self):
+        return max([item.pub_date for item in self.items])
+
+
+class FeedParserError(ValueError):
+    ...
+
+
+def parse_rss(content):
+    """Parses RSS or Atom feed and returns the feed details and individual episodes.
+
+    Args:
+        content (bytes): the body of the RSS or Atom feed
+
+    Raises:
+        FeedParserError: if XML content is invalid, or the feed is otherwise invalid or empty
+
+    Returns:
+        Feed
+    """
+
+    try:
+        channel = next(xml_parser.iterparse(content, "channel"))
+        with xml_parser.xpath(channel, NAMESPACES) as xpath:
+            return Feed(
+                title=xpath.first("title/text()"),
+                language=xpath.first("language/text()"),
+                complete=xpath.first("itunes:complete/text()"),
+                explicit=xpath.first("itunes:explicit/text()"),
+                cover_url=xpath.first(
+                    "itunes:image/@href",
+                    "image/url/text()",
+                ),
+                link=xpath.first("link/text()"),
+                funding_url=xpath.first("podcast:funding/@url"),
+                funding_text=xpath.first(
+                    "podcast:funding/text()",
+                ),
+                description=xpath.first(
+                    "description/text()",
+                    "itunes:summary/text()",
+                ),
+                owner=xpath.first(
+                    "itunes:author/text()",
+                    "itunes:owner/itunes:name/text()",
+                ),
+                categories=list(xpath.iter("//itunes:category/@text")),
+                items=list(parse_items(channel)),
+            )
+
+    except (StopIteration, TypeError, ValueError, lxml.etree.XMLSyntaxError) as e:
+        raise FeedParserError from e
+
+
+def parse_items(channel):
+    for item in channel.iterfind("item"):
+        try:
+            yield parse_item(item)
+        except (TypeError, ValueError):
+            continue
+
+
+def parse_item(item):
+    with xml_parser.xpath(item, NAMESPACES) as xpath:
+        return Item(
+            guid=xpath.first("guid/text()"),
+            title=xpath.first("title/text()"),
+            pub_date=xpath.first(
+                "pubDate/text()",
+                "pubdate/text()",
+            ),
+            media_url=xpath.first(
+                "enclosure//@url",
+                "media:content//@url",
+            ),
+            media_type=xpath.first(
+                "enclosure//@type",
+                "media:content//@type",
+            ),
+            cover_url=xpath.first("itunes:image/@href"),
+            link=xpath.first("link/text()"),
+            explicit=xpath.first("itunes:explicit/text()"),
+            duration=xpath.first("itunes:duration/text()"),
+            length=xpath.first(
+                "enclosure//@length",
+                "media:content//@fileSize",
+            ),
+            episode=xpath.first("itunes:episode/text()"),
+            season=xpath.first("itunes:season/text()"),
+            episode_type=xpath.first("itunes:episodetype/text()"),
+            description=xpath.first(
+                "content:encoded/text()",
+                "description/text()",
+                "itunes:summary/text()",
+            ),
+            keywords=" ".join(xpath.iter("category/text()")),
+        )
---
+++ radiofeed/podcasts/tests/test_rss_parser.py
@@ -0,0 +1,246 @@
+import pathlib
+
+from datetime import timedelta
+
+import pytest
+
+from django.utils import timezone
+
+from radiofeed.podcasts import rss_parser
+
+
+class TestExplicit:
+    def test_true(self):
+        assert rss_parser.explicit("yes") is True
+
+    def test_false(self):
+        assert rss_parser.explicit("no") is False
+
+    def test_none(self):
+        assert rss_parser.explicit(None) is False
+
+
+class TestDuration:
+    @pytest.mark.parametrize(
+        "value,expected",
+        [
+            (None, ""),
+            ("", ""),
+            ("invalid", ""),
+            ("300", "300"),
+            ("10:30", "10:30"),
+            ("10:30:59", "10:30:59"),
+            ("10:30:99", "10:30"),
+        ],
+    )
+    def test_parse_duration(self, value, expected):
+        assert rss_parser.duration(value) == expected
+
+
+class TestNotEmpty:
+    @pytest.mark.parametrize(
+        "value,raises",
+        [
+            ("ok", False),
+            ("", True),
+            (None, True),
+        ],
+    )
+    def test_required(self, value, raises):
+
+        if raises:
+            with pytest.raises(ValueError):
+                rss_parser.required(None, None, value)
+        else:
+            rss_parser.required(None, None, value)
+
+
+class TestUrl:
+    @pytest.mark.parametrize(
+        "value,raises",
+        [
+            ("http://example.com", False),
+            ("https://example.com", False),
+            ("example", True),
+        ],
+    )
+    def test_url(self, value, raises):
+        if raises:
+            with pytest.raises(ValueError):
+                rss_parser.url(None, None, value)
+        else:
+            rss_parser.url(None, None, value)
+
+
+class TestItem:
+    def test_pub_date_none(self):
+        with pytest.raises(ValueError):
+            rss_parser.Item(
+                guid="test",
+                title="test",
+                media_url="https://example.com/",
+                media_type="audio/mpeg",
+                pub_date=None,
+            )
+
+    def test_pub_date_in_future(self):
+        with pytest.raises(ValueError):
+            rss_parser.Item(
+                guid="test",
+                title="test",
+                media_url="https://example.com/",
+                media_type="audio/mpeg",
+                pub_date=timezone.now() + timedelta(days=1),
+            )
+
+    def test_not_audio_mimetype(self):
+        with pytest.raises(ValueError):
+            rss_parser.Item(
+                guid="test",
+                title="test",
+                media_url="https://example.com/",
+                media_type="video/mpeg",
+                pub_date=timezone.now() - timedelta(days=1),
+            )
+
+    def test_defaults(self):
+        item = rss_parser.Item(
+            guid="test",
+            title="test",
+            media_url="https://example.com/",
+            media_type="audio/mpeg",
+            pub_date=timezone.now() - timedelta(days=1),
+        )
+
+        assert item.explicit is False
+        assert item.episode_type == "full"
+
+
+class TestFeed:
+    @pytest.fixture
+    def item(self):
+        return rss_parser.Item(
+            guid="test",
+            title="test",
+            media_url="https://example.com/",
+            media_type="audio/mpeg",
+            pub_date=timezone.now() - timedelta(days=1),
+        )
+
+    def test_language(self, item):
+        feed = rss_parser.Feed(
+            title="test",
+            language="fr-CA",
+            items=[item],
+        )
+        assert feed.language == "fr"
+
+    def test_no_items(self):
+        with pytest.raises(ValueError):
+            rss_parser.Feed(
+                title="test",
+                items=[],
+            )
+
+    def test_not_complete(self, item):
+        feed = rss_parser.Feed(
+            title="test",
+            items=[item],
+            complete="no",
+        )
+
+        assert feed.complete is False
+
+    def test_complete(self, item):
+        feed = rss_parser.Feed(
+            title="test",
+            items=[item],
+            complete="yes",
+        )
+
+        assert feed.complete is True
+
+    def test_defaults(self, item):
+        feed = rss_parser.Feed(
+            title="test",
+            items=[item],
+        )
+
+        assert feed.complete is False
+        assert feed.explicit is False
+        assert feed.language == "en"
+        assert feed.description == ""
+        assert feed.categories == []
+        assert feed.pub_date == item.pub_date
+
+
+class TestParseRss:
+    def read_mock_file(self, mock_filename):
+        return (pathlib.Path(__file__).parent / "mocks" / mock_filename).read_bytes()
+
+    def test_empty(self):
+        with pytest.raises(rss_parser.FeedParserError):
+            rss_parser.parse_rss(b"")
+
+    def test_invalid_xml(self):
+        with pytest.raises(rss_parser.FeedParserError):
+            rss_parser.parse_rss(b"junk string")
+
+    def test_missing_channel(self):
+        with pytest.raises(rss_parser.FeedParserError):
+            rss_parser.parse_rss(b"<rss />")
+
+    def test_invalid_feed_channel(self):
+        with pytest.raises(rss_parser.FeedParserError):
+            rss_parser.parse_rss(b"<rss><channel /></rss>")
+
+    def test_with_bad_chars(self):
+        content = self.read_mock_file("rss_mock.xml").decode("utf-8")
+        content = content.replace("&amp;", "&")
+        feed = rss_parser.parse_rss(bytes(content.encode("utf-8")))
+
+        assert len(feed.items) == 20
+        assert feed.title == "Mysterious Universe"
+
+    @pytest.mark.parametrize(
+        "filename,title,num_items",
+        [
+            ("rss_missing_enc_length.xml", "The Vanilla JS Podcast", 71),
+            (
+                "rss_bad_pub_date.xml",
+                "Old Time Radio Mystery Theater",
+                69,
+            ),
+            (
+                "rss_mock_large.xml",
+                "AAA United Public Radio & UFO Paranormal Radio Network",
+                8641,
+            ),
+            ("rss_mock_iso_8859-1.xml", "Thunder & Lightning", 643),
+            (
+                "rss_mock_small.xml",
+                "ABC News Update",
+                1,
+            ),
+            (
+                "rss_mock.xml",
+                "Mysterious Universe",
+                20,
+            ),
+            ("rss_invalid_duration.xml", "At The Races with Steve Byk", 450),
+            (
+                "rss_bad_cover_urls.xml",
+                "TED Talks Daily",
+                327,
+            ),
+            (
+                "rss_superfeedr.xml",
+                "The Chuck ToddCast: Meet the Press",
+                296,
+            ),
+        ],
+    )
+    def test_parse_rss(self, filename, title, num_items):
+        feed = rss_parser.parse_rss(self.read_mock_file(filename))
+        assert feed.title == title
+        assert len(feed.items) == num_items
