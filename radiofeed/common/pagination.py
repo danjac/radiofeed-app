bae60b9416f98d157b1374a9756eeea5b470189f
Dan Jacob <danjac2018@gmail.com>
2022-06-30 10:07:41 +0300

    Add comments

--- radiofeed/common/pagination.py
+++ radiofeed/common/pagination.py
@@ -15,7 +15,62 @@ def pagination_response(
     param="page",
     **pagination_kwargs,
 ):
-    """Renders a pagination_responsed response"""
+    """
+    Creates a TemplateResponse for a paginated QuerySet or list.
+
+    If the request has the HX-Request header and matching HTMX target,
+    will select the pagination template instead
+    of the default template. The pagination template should be included
+    in the default template and ensure the correct hx-target is also included.
+
+    The following items are added to the template context:
+
+        page_obj: Page instance
+        pagination_template: pagination template name
+        pagination_target: HTMX target
+
+    Example:
+
+    default template:
+
+        ```html
+        {% include pagination_template %}
+        ```
+
+    pagination template:
+
+        ```html
+        <div id="{{ pagination_target }}">
+
+            {% include "includes/pagination_links.html" %}
+
+            {% for podcast in page_obj.object_list %}
+                {% include "podcasts/podcast.html" %}
+            {% empty %}
+                {% include "includes/empty.html" %}
+            {% endfor %}
+
+            {% include "includes/pagination_links.html" %}
+        </dv>
+        ```
+
+    Args:
+        request (HttpRequest)
+        object_list (Iterable): QuerySet or list of items
+        template_name (str): default template used if no matching HTMX headers
+        pagination_template_name (str): template used if matching HTMX headers
+        extra_context (dict | None): extra template context
+        target (str): HTMX target
+        page_size (int): number of items per page
+        param (str): query string parameter for page
+        **pagination_kwargs: other Paginator kwargs
+
+    Raises:
+        Http404: invalid page
+
+    Returns:
+        TemplateResponse
+    """
 
     try:
         page_obj = Paginator(object_list, page_size, **pagination_kwargs).page(
