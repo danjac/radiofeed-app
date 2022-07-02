4cd7ba601be0fc8409e295201a6f47f9add2a70d
Dan Jacob <danjac2018@gmail.com>
2022-07-02 21:37:29 +0300

    Add sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,16 @@ import sys
 
 import django
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiofeed.settings.test")
 django.setup()
 
 # -- Project information -----------------------------------------------------
 
 project = "radiofeed"
+copyright = "2022, Dan Jacob"
 author = "Dan Jacob"
 
 
@@ -30,31 +33,26 @@ author = "Dan Jacob"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-    "**/migrations/*",
-    "**/tests/*",
-    "**/settings/*",
-    "*/wsgi.py",
-]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
