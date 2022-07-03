9a35a52b9e80ff90b2fa14d01a408efb96f24c14
Dan Jacob <danjac2018@gmail.com>
2022-07-03 13:29:23 +0300

    docstring linter fixes

--- radiofeed/common/middleware.py
+++ radiofeed/common/middleware.py
@@ -18,6 +18,8 @@ class BaseMiddleware:
 class Search:
     """Encapsulates generic search query.
 
+    Attributes:
+        search_param (str): query string parameter
     Args:
         request (HttpRequest)
     """
@@ -28,14 +30,24 @@ class Search:
         self.request = request
 
     def __str__(self):
+        """Returns search query value.
+
+        Returns:
+            str
+        """
         return self.value
 
     def __bool__(self):
+        """Returns `True` if search in query and has a non-empty value.
+
+        Returns:
+            bool
+        """
         return bool(self.value)
 
     @cached_property
     def value(self):
-        """Returns the search query value, if any
+        """Returns the search query value, if any.
 
         Returns:
             str
@@ -44,7 +56,7 @@ class Search:
 
     @cached_property
     def qs(self):
-        """Returns encoded query string value, if any
+        """Returns encoded query string value, if any.
 
         Returns:
             str
@@ -56,18 +68,33 @@ class SearchMiddleware(BaseMiddleware):
     """Adds Search instance to the request as `request.search`."""
 
     def __call__(self, request):
+        """Resolves middleware.
+
+        Args:
+            request (HttpRequest)
+
+        Returns:
+            HttpResponse
+        """
         request.search = SimpleLazyObject(lambda: Search(request))
         return self.get_response(request)
 
 
 class CacheControlMiddleware(BaseMiddleware):
-    """
-    Workaround for https://github.com/bigskysoftware/htmx/issues/497
+    """Workaround for https://github.com/bigskysoftware/htmx/issues/497.
 
     Place after HtmxMiddleware
     """
 
     def __call__(self, request):
+        """Resolves middleware.
+
+        Args:
+            request (HttpRequest)
+
+        Returns:
+            HttpResponse
+        """
         response = self.get_response(request)
         if request.htmx:
             # don't override if cache explicitly set
