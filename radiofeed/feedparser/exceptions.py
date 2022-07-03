3659b2a772143b1904607231865b305721cc732b
Dan Jacob <danjac2018@gmail.com>
2022-07-03 12:31:41 +0300

    docstring linter fixes

--- radiofeed/feedparser/exceptions.py
+++ radiofeed/feedparser/exceptions.py
@@ -2,12 +2,12 @@ import requests
 
 
 class NotModified(requests.RequestException):
-    ...
+    """RSS feed has not been modified since last update."""
 
 
 class DuplicateFeed(requests.RequestException):
-    ...
+    """Another identical podcast exists in the database."""
 
 
 class RssParserError(ValueError):
-    ...
+    """Broken XML syntax or missing/invalid required attributes."""
