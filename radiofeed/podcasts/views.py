93813e0ca9cdff316262a1a4f5ebadd5d946108b
Dan Jacob <danjac2018@gmail.com>
2022-07-05 14:33:35 +0300

    Add XMLFinder.to_dict method

--- radiofeed/podcasts/views.py
+++ radiofeed/podcasts/views.py
@@ -19,8 +19,6 @@ from radiofeed.episodes.models import Episode
 from radiofeed.podcasts import itunes
 from radiofeed.podcasts.models import Category, Podcast, Recommendation, Subscription
 
-# test commit
-
 
 @require_http_methods(["GET"])
 def index(request):
