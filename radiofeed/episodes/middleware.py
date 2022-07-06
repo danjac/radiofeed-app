758095686cba93aded6f5232dcff62acc6af7132
Dan Jacob <danjac2018@gmail.com>
2022-07-06 22:21:19 +0300

    Adding dependencies

--- radiofeed/episodes/middleware.py
+++ radiofeed/episodes/middleware.py
@@ -1,60 +1,38 @@
+from django.http import HttpRequest, HttpResponse
 from django.utils.functional import SimpleLazyObject
 
 from radiofeed.common.middleware import BaseMiddleware
 
 
 class Player:
-    """Tracks current player episode in session
+    """Tracks current player episode in session."""
 
-    Args:
-        request (HttpRequest)
-    """
+    _session_key: str = "player_episode"
 
-    session_key = "player_episode"
+    def __init__(self, request: HttpRequest):
+        self._request = request
 
-    def __init__(self, request):
-        self.request = request
+    def get(self) -> int | None:
+        """Returns primary key of episode in player, if any in session."""
+        return self._request.session.get(self._session_key)
 
-    def get(self):
-        """Returns primary key of episode in player, if any in session.
+    def set(self, episode_id: int) -> None:
+        """Adds episode PK to player in session."""
+        self._request.session[self._session_key] = episode_id
 
-        Returns:
-            int | None: episode PK
-        """
-        return self.request.session.get(self.session_key)
-
-    def set(self, episode_id):
-        """Adds episode PK to player in session.
-
-        Args:
-            episode_id (int): Episode Pk
-        """
-        self.request.session[self.session_key] = episode_id
-
-    def has(self, episode_id):
-        """Checks if episode matching ID is in player.
-
-        Args:
-            episode_id (int): Episode Pk
-
-        Returns:
-            bool
-        """
+    def has(self, episode_id: int) -> bool:
+        """Checks if episode matching ID is in player."""
         return self.get() == episode_id
 
-    def pop(self):
-        """Returns primary key of episode in player, if any in session,
-        and removes the episode ID from the session.
-
-        Returns:
-            int | None: episode PK
-        """
-        return self.request.session.pop(self.session_key, None)
+    def pop(self) -> int | None:
+        """Returns primary key of episode in player, if any in session, and removes the episode ID from the session."""
+        return self._request.session.pop(self._session_key, None)
 
 
 class PlayerMiddleware(BaseMiddleware):
     """Adds Player instance to request as `request.player`."""
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        """Adds Player instance to request."""
         request.player = SimpleLazyObject(lambda: Player(request))
         return self.get_response(request)
