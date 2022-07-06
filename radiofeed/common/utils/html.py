1af172720957af859798f34930000336bb7d2c49
Dan Jacob <danjac2018@gmail.com>
2022-07-06 21:11:03 +0300

    Adding dependencies

--- radiofeed/common/utils/html.py
+++ radiofeed/common/utils/html.py
@@ -6,7 +6,7 @@ import markdown
 
 from django.template.defaultfilters import striptags
 
-_allowed_tags = [
+_allowed_tags: list[str] = [
     "a",
     "abbr",
     "acronym",
@@ -59,21 +59,8 @@ _allowed_attrs = {
 _html_re = re.compile(r"^(<\/?[a-zA-Z][\s\S]*>)+", re.UNICODE)
 
 
-def _linkify_callback(attrs, new=False):
-    attrs[(None, "target")] = "_blank"
-    attrs[(None, "rel")] = "noopener noreferrer nofollow"
-    return attrs
-
-
-def clean(value):
-    """Runs Bleach through value and scrubs any unwanted HTML tags and attributes.
-
-    Args:
-        value (str)
-
-    Returns:
-        str
-    """
+def clean(value: str) -> str:
+    """Runs Bleach through value and scrubs any unwanted HTML tags and attributes."""
     return (
         bleach.linkify(
             bleach.clean(
@@ -82,58 +69,36 @@ def clean(value):
                 attributes=_allowed_attrs,
                 strip=True,
             ),
-            [_linkify_callback],
+            [_linkify_callback],  # type: ignore
         )
         if value
         else ""
     )
 
 
-def strip_whitespace(value):
-    """Removes all trailing whitespace. Any None value just returns an empty string.
-
-    Args:
-        value (str | None)
-
-    Returns:
-        str
-    """
+def strip_whitespace(value: str | None) -> str:
+    """Removes all trailing whitespace. Any None value just returns an empty string."""
     return (value or "").strip()
 
 
-def strip_html(value):
-    """Scrubs all HTML tags and entities from text.
-
-    Args:
-        value (str | None): text to be cleaned
-
-    Returns:
-        str
-    """
+def strip_html(value: str | None) -> str:
+    """Scrubs all HTML tags and entities from text."""
     return html.unescape(striptags(strip_whitespace(value)))
 
 
-def as_html(value):
-    """Checks if content contains any HTML tags. If not, will try and parse Markdown from text.
-
-    Args:
-        value (str)
-
-    Returns:
-        str: HTML content
-    """
+def as_html(value: str) -> str:
+    """Checks if content contains any HTML tags. If not, will try and parse Markdown from text."""
     return value if _html_re.match(value) else markdown.markdown(value)
 
 
-def markup(value):
-    """Parses Markdown and/or html and returns cleaned result.
-
-    Args:
-        value (str | None)
-
-    Returns:
-        str
-    """
+def markup(value: str | None) -> str:
+    """Parses Markdown and/or html and returns cleaned result."""
     if value := strip_whitespace(value):
         return html.unescape(clean(as_html(value)))
     return ""
+
+
+def _linkify_callback(attrs: dict, new: bool = False) -> dict:
+    attrs[(None, "target")] = "_blank"
+    attrs[(None, "rel")] = "noopener noreferrer nofollow"
+    return attrs
