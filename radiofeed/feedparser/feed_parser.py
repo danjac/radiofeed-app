80309664201b2fbcc8ac5cf65ed656d6a01da018
Dan Jacob <danjac2018@gmail.com>
2022-07-08 21:21:55 +0300

    Set line length to 88

--- radiofeed/feedparser/feed_parser.py
+++ radiofeed/feedparser/feed_parser.py
@@ -73,15 +73,24 @@ class FeedParser:
             raise NotModified(response=response)
 
         # check if another feed already has new URL
-        if response.url != self._podcast.rss and Podcast.objects.filter(rss=response.url).exists():
+        if (
+            response.url != self._podcast.rss
+            and Podcast.objects.filter(rss=response.url).exists()
+        ):
             raise DuplicateFeed(response=response)
 
         # check if content has changed (feed is not checking etag etc)
-        if (content_hash := make_content_hash(response.content)) == self._podcast.content_hash:
+        if (
+            content_hash := make_content_hash(response.content)
+        ) == self._podcast.content_hash:
             raise NotModified(response=response)
 
         # check if another feed has exact same content
-        if (Podcast.objects.exclude(pk=self._podcast.id).filter(content_hash=content_hash, active=True)).exists():
+        if (
+            Podcast.objects.exclude(pk=self._podcast.id).filter(
+                content_hash=content_hash, active=True
+            )
+        ).exists():
             raise DuplicateFeed(response=response)
 
         return response, parse_rss(response.content), content_hash
@@ -98,18 +107,28 @@ class FeedParser:
             headers["If-Modified-Since"] = http_date(self._podcast.modified.timestamp())
         return headers
 
-    def _handle_success(self, response: requests.Response, feed: Feed, content_hash: str) -> bool:
+    def _handle_success(
+        self, response: requests.Response, feed: Feed, content_hash: str
+    ) -> bool:
 
         # taxonomy
         categories_dct = Category.objects.in_bulk(field_name="name")
 
-        categories = [categories_dct[category] for category in feed.categories if category in categories_dct]
+        categories = [
+            categories_dct[category]
+            for category in feed.categories
+            if category in categories_dct
+        ]
 
-        keywords = " ".join(category for category in feed.categories if category not in categories_dct)
+        keywords = " ".join(
+            category for category in feed.categories if category not in categories_dct
+        )
 
         self._save_podcast(
             active=not (feed.complete),
-            parse_result=Podcast.ParseResult.COMPLETE if feed.complete else Podcast.ParseResult.SUCCESS,
+            parse_result=Podcast.ParseResult.COMPLETE
+            if feed.complete
+            else Podcast.ParseResult.SUCCESS,
             content_hash=content_hash,
             rss=response.url,
             etag=response.headers.get("ETag", ""),
@@ -166,7 +185,9 @@ class FeedParser:
         )
         return False
 
-    def _extract_text(self, feed: Feed, categories: list[Category], keywords: str) -> str:
+    def _extract_text(
+        self, feed: Feed, categories: list[Category], keywords: str
+    ) -> str:
         text = " ".join(
             value
             for value in [
@@ -226,7 +247,9 @@ class FeedParser:
         for batch in batcher(self._episodes_for_insert(feed, guids), batch_size):
             Episode.objects.bulk_create(batch, ignore_conflicts=True)
 
-    def _episodes_for_update(self, feed: Feed, guids: dict[str, int]) -> Iterator[Episode]:
+    def _episodes_for_update(
+        self, feed: Feed, guids: dict[str, int]
+    ) -> Iterator[Episode]:
 
         episode_ids = set()
 
@@ -235,8 +258,12 @@ class FeedParser:
                 yield self._make_episode(item, episode_id)
                 episode_ids.add(episode_id)
 
-    def _episodes_for_insert(self, feed: Feed, guids: dict[str, int]) -> Iterator[Episode]:
-        return (self._make_episode(item) for item in feed.items if item.guid not in guids)
+    def _episodes_for_insert(
+        self, feed: Feed, guids: dict[str, int]
+    ) -> Iterator[Episode]:
+        return (
+            self._make_episode(item) for item in feed.items if item.guid not in guids
+        )
 
     def _make_episode(self, item: Item, episode_id: int | None = None) -> Episode:
         return Episode(
