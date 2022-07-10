a50903644648ad79515e2cd807e5f0ffad38f801
Dan Jacob <danjac2018@gmail.com>
2022-07-10 18:57:10 +0300

    Add eslint

--- static/js/player.js
+++ static/js/player.js
@@ -119,7 +119,7 @@ document.addEventListener("alpine:init", () => {
             skipForward() {
                 this.skipTo(10);
             },
-            shortcuts(event) {
+            shortcuts(even) {
                 if (event.target.tagName.match(/INPUT|TEXTAREA/)) {
                     return;
                 }
