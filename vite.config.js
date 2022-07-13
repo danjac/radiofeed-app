5cf2ce19584abdac7f8a2d32090feb2ca2b7f54b
Dan Jacob <danjac2018@gmail.com>
2022-07-13 23:13:08 +0300

    Vite config changes

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import path from "path";
 
 export default defineConfig({
     build: {
-        outDir: path.resolve(__dirname, "static", "dist"),
-        assetsDir: ".",
-        sourcemap: true,
+        outDir: path.resolve(__dirname, "static"),
         emptyOutDir: false,
+        sourcemap: true,
         rollupOptions: {
             input: {
                 main: path.resolve("static", "js", "app.js"),
             },
             output: {
-                entryFileNames: "bundle.js",
+                entryFileNames: "dist/bundle.js",
             },
         },
     },
