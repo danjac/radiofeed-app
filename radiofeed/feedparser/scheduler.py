c74bddd909a1dec45524daf0b4a908b5573b75fa
Dan Jacob <danjac2018@gmail.com>
2022-08-03 15:16:52 +0300

    Handle zero intervals

--- radiofeed/feedparser/scheduler.py
+++ radiofeed/feedparser/scheduler.py
@@ -50,7 +50,7 @@ def schedule(feed: Feed) -> timedelta:
         timedelta(seconds=_calc_median_interval(intervals))
         if (intervals := _calc_intervals(feed, now - timedelta(days=90)))
         else Podcast.DEFAULT_FREQUENCY
-    )
+    ) or Podcast.MIN_FREQUENCY
 
     # increment until pub date + freq > current time
 
@@ -89,8 +89,9 @@ def _calc_intervals(feed: Feed, since: datetime) -> list[float]:
 
 
 def _calc_median_interval(intervals: list[float]) -> float:
-    # remove any outliers and calculate median interval
+    # remove any outliers and zeros and calculate median interval
     df = pandas.DataFrame(intervals, columns=["intervals"])
+    df = df[df["intervals"] != 0]
     df["zscore"] = zscore(df["intervals"])
     df["outlier"] = df["zscore"].apply(lambda score: score <= 0.96 and score >= 1.96)
     return df[~df["outlier"]]["intervals"].median()
