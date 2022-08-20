bd9fabb6ed32383985fcb9e44fd8c1c90831d99a
Dan Jacob <danjac2018@gmail.com>
2022-08-20 11:45:16 +0300

    Add htmx plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,39 @@
 /* eslint-disable */
 
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-    mode: 'jit',
-    darkMode: 'media',
+    mode: "jit",
+    darkMode: "media",
     future: {
         removeDeprecatedGapUtilities: true,
         purgeLayersByDefault: true,
     },
     content: [
-        './templates/**/*.html',
-        './static/js/**/*.js',
-        './tailwind-safelist.txt',
+        "./templates/**/*.html",
+        "./static/js/**/*.js",
+        "./tailwind-safelist.txt",
     ],
     keyframes: true,
     variants: {
-        textColor: ['responsive', 'hover', 'focus', 'visited', 'dark'],
+        textColor: ["responsive", "hover", "focus", "visited", "dark"],
     },
     plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
+        // https://www.crocodile.dev/blog/css-transitions-with-tailwind-and-htmx
+        plugin(function ({ addVariant }) {
+            addVariant("htmx-settling", [
+                "&.htmx-settling",
+                ".htmx-settling &",
+            ]);
+            addVariant("htmx-request", ["&.htmx-request", ".htmx-request &"]);
+            addVariant("htmx-swapping", [
+                "&.htmx-swapping",
+                ".htmx-swapping &",
+            ]);
+            addVariant("htmx-added", ["&.htmx-added", ".htmx-added &"]);
+        }),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/line-clamp"),
     ],
 };
