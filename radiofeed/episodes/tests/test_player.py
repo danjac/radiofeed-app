0d970219a974c9ff3420449bf1b758e4e03c5960
Dan Jacob <danjac2018@gmail.com>
2022-09-14 20:59:21 +0300

    Tidy up

--- radiofeed/episodes/tests/test_player.py
+++ radiofeed/episodes/tests/test_player.py
@@ -36,9 +36,7 @@ class TestPlayer:
 
     def test_has_false(self, player):
         assert self.episode_id not in player
-        assert not player.has(self.episode_id)
 
     def test_has_true(self, player):
         player.set(self.episode_id)
         assert self.episode_id in player
-        assert player.has(self.episode_id)
