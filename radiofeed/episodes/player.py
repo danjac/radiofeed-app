f27e82c30b12c533cbcabd7861f97481680af456
Dan Jacob <danjac2018@gmail.com>
2022-10-08 13:11:13 +0300

    Refactoring middleware

--- radiofeed/episodes/player.py
+++ radiofeed/episodes/player.py
@@ -1,19 +1,16 @@
 from __future__ import annotations
 
+import dataclasses
+
 from django.http import HttpRequest
 
 
+@dataclasses.dataclass(frozen=True)
 class Player:
-    """Tracks current player episode in session.
+    """Tracks current player episode in session."""
 
-    Attributes:
-        session_key: session key for current episode id
-    """
-
+    request: HttpRequest
     session_key: str = "player_episode"
-
-    def __init__(self, request: HttpRequest):
-        self._request = request
 
     def __contains__(self, episode_id: int) -> bool:
         """Checks if episode matching ID is in player."""
@@ -21,12 +18,12 @@ class Player:
 
     def get(self) -> int | None:
         """Returns primary key of episode in player, if any in session."""
-        return self._request.session.get(self.session_key)
+        return self.request.session.get(self.session_key)
 
     def set(self, episode_id: int) -> None:
         """Adds episode PK to player in session."""
-        self._request.session[self.session_key] = episode_id
+        self.request.session[self.session_key] = episode_id
 
     def pop(self) -> int | None:
         """Returns primary key of episode in player, if any in session, and removes the episode ID from the session."""
-        return self._request.session.pop(self.session_key, None)
+        return self.request.session.pop(self.session_key, None)
