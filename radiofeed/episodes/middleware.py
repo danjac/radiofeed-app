b25324e439550bef37f5fc62742bd2ab4456a400
Dan Jacob <danjac2018@gmail.com>
2022-10-08 12:08:38 +0300

    Tidy middleware

--- radiofeed/episodes/middleware.py
+++ radiofeed/episodes/middleware.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import cast
-
 from django.http import HttpRequest, HttpResponse
 from django.utils.functional import SimpleLazyObject
 
@@ -14,5 +12,5 @@ class PlayerMiddleware(BaseMiddleware):
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
         """Adds Player instance to request."""
-        request.player = cast(Player, SimpleLazyObject(lambda: Player(request)))
+        request.player = SimpleLazyObject(lambda: Player(request))
         return self.get_response(request)
