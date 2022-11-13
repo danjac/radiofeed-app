6e38f183bc8682d147a102f8d6026871e57fb6f5
Dan Jacob <danjac2018@gmail.com>
2022-11-13 12:02:50 +0200

    Remove parallel option from tests

--- radiofeed/podcasts/translation.py
+++ radiofeed/podcasts/translation.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from modeltranslation.translator import TranslationOptions, translator
 
-from .models import Category
+from radiofeed.podcasts.models import Category
 
 
 class CategoryTranslationOptions(TranslationOptions):
+    """Translations of all category names."""
+
     fields = ("name",)
 
 
