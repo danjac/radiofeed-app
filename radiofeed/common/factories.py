b46b61a69db68a7e8ee485431781bd7420f83a0c
Dan Jacob <danjac2018@gmail.com>
2022-11-27 17:44:29 +0200

    Using functional factories

--- radiofeed/common/factories.py
+++ radiofeed/common/factories.py
@@ -33,11 +33,11 @@ def create_batch(factory: Callable[..., T], count: int, /, **kwargs) -> list[T]:
     return [factory(**kwargs) for i in range(0, count)]
 
 
-notset_name = functools.partial(notset, default_value=_faker.name)
 notset_username = functools.partial(notset, default_value=_faker.unique.user_name)
 notset_email = functools.partial(notset, default_value=_faker.unique.email)
 notset_url = functools.partial(notset, default_value=_faker.unique.url)
 notset_password = functools.partial(notset, default_value=_faker.password)
+notset_name = functools.partial(notset, default_value=_faker.name)
 notset_text = functools.partial(notset, default_value=_faker.text)
 notset_datetime = functools.partial(notset, default_value=timezone.now)
 notset_guid = functools.partial(notset, default_value=lambda: uuid.uuid4().hex)
