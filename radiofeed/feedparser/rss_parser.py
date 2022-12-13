0c68de48fbb6b22c36b308bfd2d5e3ed1c2785f5
Dan Jacob <danjac2018@gmail.com>
2022-12-13 15:29:39 +0200

    Removed old models

--- radiofeed/feedparser/rss_parser.py
+++ radiofeed/feedparser/rss_parser.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import functools
-
 from typing import Iterator
 
 import lxml.etree  # nosec
@@ -21,84 +19,86 @@ def parse_rss(content: bytes) -> Feed:
         RssParserError: if XML content is unparseable, or the feed is otherwise invalid or empty
     """
     try:
-        return _parse_feed(next(xml_iterparse(content, "channel")))
+        return _rss_parser.parse(next(xml_iterparse(content, "channel")))
     except StopIteration:
         raise RssParserError("Document does not contain <channel /> element")
     except lxml.etree.XMLSyntaxError as e:
         raise RssParserError from e
 
 
-_xpath_finder = functools.partial(
-    xpath_finder,
-    namespaces={
+class RssParser:
+    """Parses RSS XML feeds."""
+
+    _namespaces = {
         "atom": "http://www.w3.org/2005/Atom",
         "content": "http://purl.org/rss/1.0/modules/content/",
         "googleplay": "http://www.google.com/schemas/play-podcasts/1.0",
         "itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd",
         "media": "http://search.yahoo.com/mrss/",
         "podcast": "https://podcastindex.org/namespace/1.0",
-    },
-)
+    }
 
-
-def _parse_feed(channel: lxml.etree.Element) -> Feed:
-    try:
-        with _xpath_finder(channel) as finder:
-            return Feed(
-                items=list(_parse_items(channel)),
-                categories=finder.aslist(
-                    "//googleplay:category/@text",
-                    "//itunes:category/@text",
-                    "//media:category/@label",
-                    "//media:category/text()",
-                ),
-                **finder.asdict(
-                    complete="itunes:complete/text()",
-                    cover_url=("itunes:image/@href", "image/url/text()"),
-                    description=("description/text()", "itunes:summary/text()"),
-                    explicit="itunes:explicit/text()",
-                    funding_text="podcast:funding/text()",
-                    funding_url="podcast:funding/@url",
-                    language="language/text()",
-                    link="link/text()",
-                    owner=(
-                        "itunes:author/text()",
-                        "itunes:owner/itunes:name/text()",
+    def parse(self, channel: lxml.etree.Element) -> Feed:
+        """Parse a RSS XML feed."""
+        try:
+            with xpath_finder(channel, self._namespaces) as finder:
+                return Feed(
+                    items=list(self._parse_items(channel)),
+                    categories=finder.aslist(
+                        "//googleplay:category/@text",
+                        "//itunes:category/@text",
+                        "//media:category/@label",
+                        "//media:category/text()",
                     ),
-                    title="title/text()",  # type: ignore
-                ),
-            )
-    except (TypeError, ValueError) as e:
-        raise RssParserError from e
-
-
-def _parse_items(channel: lxml.etree.Element) -> Iterator[Item]:
-    for item in channel.iterfind("item"):
-        with _xpath_finder(item) as finder:
-            try:
-                yield Item(
-                    categories=finder.aslist("category/text()"),
                     **finder.asdict(
-                        cover_url="itunes:image/@href",
-                        description=(
-                            "content:encoded/text()",
-                            "description/text()",
-                            "itunes:summary/text()",
-                        ),
-                        duration="itunes:duration/text()",
-                        episode="itunes:episode/text()",
-                        episode_type="itunes:episodetype/text()",
+                        complete="itunes:complete/text()",
+                        cover_url=("itunes:image/@href", "image/url/text()"),
+                        description=("description/text()", "itunes:summary/text()"),
                         explicit="itunes:explicit/text()",
-                        guid="guid/text()",
-                        length=("enclosure//@length", "media:content//@fileSize"),
+                        funding_text="podcast:funding/text()",
+                        funding_url="podcast:funding/@url",
+                        language="language/text()",
                         link="link/text()",
-                        media_type=("enclosure//@type", "media:content//@type"),
-                        media_url=("enclosure//@url", "media:content//@url"),
-                        pub_date=("pubDate/text()", "pubdate/text()"),
-                        season="itunes:season/text()",
+                        owner=(
+                            "itunes:author/text()",
+                            "itunes:owner/itunes:name/text()",
+                        ),
                         title="title/text()",  # type: ignore
                     ),
                 )
-            except (TypeError, ValueError):
-                # invalid item, just continue
-                continue
+        except (TypeError, ValueError) as e:
+            raise RssParserError from e
+
+    def _parse_items(self, channel: lxml.etree.Element) -> Iterator[Item]:
+        for item in channel.iterfind("item"):
+            with xpath_finder(item, self._namespaces) as finder:
+                try:
+                    yield Item(
+                        categories=finder.aslist("category/text()"),
+                        **finder.asdict(
+                            cover_url="itunes:image/@href",
+                            description=(
+                                "content:encoded/text()",
+                                "description/text()",
+                                "itunes:summary/text()",
+                            ),
+                            duration="itunes:duration/text()",
+                            episode="itunes:episode/text()",
+                            episode_type="itunes:episodetype/text()",
+                            explicit="itunes:explicit/text()",
+                            guid="guid/text()",
+                            length=("enclosure//@length", "media:content//@fileSize"),
+                            link="link/text()",
+                            media_type=("enclosure//@type", "media:content//@type"),
+                            media_url=("enclosure//@url", "media:content//@url"),
+                            pub_date=("pubDate/text()", "pubdate/text()"),
+                            season="itunes:season/text()",
+                            title="title/text()",  # type: ignore
+                        ),
+                    )
+                except (TypeError, ValueError):
+                    # invalid item, just continue
+                    continue
+
+
+_rss_parser = RssParser()
