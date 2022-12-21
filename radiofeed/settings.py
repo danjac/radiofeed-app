b23ff9c32ce781720a10422098fc03a0e9f62824
Dan Jacob <danjac2018@gmail.com>
2022-12-21 16:45:30 +0200

    Tidy up

--- radiofeed/settings.py
+++ radiofeed/settings.py
@@ -134,7 +134,7 @@ AUTHENTICATION_BACKENDS = [
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },  # noqa
+    },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
