dba314eb064d238692f265af714d9dc7e48ac8b5
Dan Jacob <danjac2018@gmail.com>
2022-12-21 13:44:10 +0200

    Use single settings file

--- manage.py
+++ manage.py
@@ -9,7 +9,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiofeed.config.settings.local")
+    """Django management entry point."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiofeed.config.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- radiofeed/config/settings.py
+++ radiofeed/config/settings.py
@@ -3,14 +3,16 @@ from __future__ import annotations
 import pathlib
 
 from email.utils import getaddresses
+from typing import Literal
 
 import environ
 
 from django.contrib.messages import constants as messages
 from django.urls import reverse_lazy
 
-BASE_DIR = pathlib.Path(__file__).resolve(strict=True).parents[3]
+Environments = Literal["development", "production", "test"]
 
+BASE_DIR = pathlib.Path(__file__).resolve(strict=True).parents[2]
 
 env = environ.Env()
 
@@ -271,3 +273,128 @@ ADMIN_SITE_HEADER = env("ADMIN_SITE_HEADER", default="Radiofeed Admin")
 CONTACT_EMAIL = env("CONTACT_EMAIL", default="admin@localhost")
 
 DEBUG_ERROR_PAGES = False
+
+# Environments
+
+ENVIRONMENT: Environments = env("ENVIRONMENT", default="development")
+
+match ENVIRONMENT:
+
+    case "development":
+
+        DEBUG = True
+
+        ADMIN_SITE_HEADER += " [LOCAL]"
+
+        INSTALLED_APPS = [
+            "whitenoise.runserver_nostatic",
+            "debug_toolbar",
+        ] + INSTALLED_APPS
+
+        MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+        INTERNAL_IPS = ["127.0.0.1"]
+
+        DEBUG_ERROR_PAGES = True
+
+    case "testing":
+
+        LOGGING = None
+
+        PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+        ALLOWED_HOSTS += [".example.com"]
+
+        CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
+
+        CACHEOPS_ENABLED = False
+
+        EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+        # django-coverage-plugin
+        TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore
+
+        DEBUG_ERROR_PAGES = True
+
+    case "production":
+
+        ADMIN_SITE_HEADER += " [PRODUCTION]"
+
+        STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        STATIC_ROOT = BASE_DIR / "staticfiles"
+
+        # Permissions Policy
+
+        # https://pypi.org/project/django-permissions-policy/
+
+        PERMISSIONS_POLICY: dict[str, list] = {
+            "accelerometer": [],
+            "ambient-light-sensor": [],
+            "camera": [],
+            "document-domain": [],
+            "encrypted-media": [],
+            "fullscreen": [],
+            "geolocation": [],
+            "gyroscope": [],
+            "magnetometer": [],
+            "microphone": [],
+            "payment": [],
+            "usb": [],
+        }
+
+        # Security
+
+        SECURE_BROWSER_XSS_FILTER = True
+        SECURE_CONTENT_TYPE_NOSNIFF = True
+        SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+        SECURE_HSTS_PRELOAD = True
+        SECURE_HSTS_SECONDS = 15768001  # 6 months
+        SECURE_SSL_REDIRECT = True
+
+        SESSION_COOKIE_SECURE = True
+        CSRF_COOKIE_SECURE = True
+
+        SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+        # Sentry
+
+        if SENTRY_URL := env("SENTRY_URL", default=None):
+            import sentry_sdk
+
+            from sentry_sdk.integrations.django import DjangoIntegration
+            from sentry_sdk.integrations.logging import ignore_logger
+
+            ignore_logger("django.security.DisallowedHost")
+
+            sentry_sdk.init(
+                dsn=SENTRY_URL,
+                integrations=[DjangoIntegration()],
+                traces_sample_rate=0.5,
+                # If you wish to associate users to errors (assuming you are using
+                # django.contrib.auth) you may enable sending PII data.
+                send_default_pii=True,
+            )
+
+        # Mailgun
+
+        MAILGUN_API_KEY = env("MAILGUN_API_KEY", default=None)
+        MAILGUN_SENDER_DOMAIN = env("MAILGUN_SENDER_DOMAIN", default=None)
+
+        if MAILGUN_API_KEY and MAILGUN_SENDER_DOMAIN:
+
+            INSTALLED_APPS += ["anymail"]
+
+            EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+
+            MAILGUN_API_URL = env(
+                "MAILGUN_API_URL", default="https://api.mailgun.net/v3"
+            )
+
+            ANYMAIL = {
+                "MAILGUN_API_KEY": MAILGUN_API_KEY,
+                "MAILGUN_API_URL": MAILGUN_API_URL,
+                "MAILGUN_SENDER_DOMAIN": MAILGUN_SENDER_DOMAIN,
+            }
+
+            SERVER_EMAIL = f"errors@{MAILGUN_SENDER_DOMAIN}"
+            DEFAULT_FROM_EMAIL = f"support@{MAILGUN_SENDER_DOMAIN}"
