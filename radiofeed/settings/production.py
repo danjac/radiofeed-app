e5f99c57b5eb456f5b6ef87ca97c40e8f6f74202
Dan Jacob <danjac2018@gmail.com>
2023-01-09 20:03:31 +0200

    Secure settings

--- radiofeed/settings/production.py
+++ radiofeed/settings/production.py
@@ -6,46 +6,13 @@ from radiofeed.settings.base import ADMIN_SITE_HEADER, BASE_DIR
 
 include("base.py")
 include("mailgun.py")
+include("secure.py")
 include("sentry.py")
 include("templates.py")
 
 ADMIN_SITE_HEADER += " [PRODUCTION]"
 
-# Sessions and cookies
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
 # Static files
-#
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-# Secure settings
-
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 15768001  # 6 months
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = True
-
-# Permissions Policy
-
-# https://pypi.org/project/django-permissions-policy/
-
-PERMISSIONS_POLICY: dict[str, list] = {
-    "accelerometer": [],
-    "ambient-light-sensor": [],
-    "camera": [],
-    "document-domain": [],
-    "encrypted-media": [],
-    "fullscreen": [],
-    "geolocation": [],
-    "gyroscope": [],
-    "magnetometer": [],
-    "microphone": [],
-    "payment": [],
-    "usb": [],
-}
