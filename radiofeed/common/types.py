fc091a325ca528ff3b723991126d06d00213289a
Dan Jacob <danjac2018@gmail.com>
2023-01-12 12:22:56 +0200

    Comment change

--- radiofeed/common/types.py
+++ radiofeed/common/types.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable, TypeAlias, TypeVar
+from collections.abc import Callable
+from typing import TYPE_CHECKING, TypeAlias, TypeVar
 
 from django.contrib import admin
 from django.db.models import Model, QuerySet
 from django.http import HttpRequest, HttpResponse
 
+# Return type for Django middleware
 GetResponse: TypeAlias = Callable[[HttpRequest], HttpResponse]
 
 if TYPE_CHECKING:  # pragma: no cover
