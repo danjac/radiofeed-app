57c0bc39e932879ef90ff72e0263146f62e4c789
Dan Jacob <danjac2018@gmail.com>
2023-01-12 11:45:14 +0200

    Add lazy_object_middleware

--- radiofeed/common/middleware.py
+++ radiofeed/common/middleware.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 from django.http import HttpRequest, HttpResponse
-from django.utils.functional import SimpleLazyObject
 
-from radiofeed.common.decorators import middleware
+from radiofeed.common.decorators import lazy_object_middleware, middleware
 from radiofeed.common.paginator import Paginator
 from radiofeed.common.search import Search
 from radiofeed.common.sorter import Sorter
@@ -26,33 +25,10 @@ def cache_control_middleware(
     return response
 
 
-@middleware
-def paginator_middleware(
-    request: HttpRequest, get_response: GetResponse
-) -> HttpResponse:
-    """Adds Sorter instance to request as `request.sorter`."""
-    request.paginator = SimpleLazyObject(lambda: Paginator(request))
-    return get_response(request)
+paginator_middleware = lazy_object_middleware("paginator")(Paginator)
 
+search_middleware = lazy_object_middleware("search")(Search)
 
-@middleware
-def search_middleware(request: HttpRequest, get_response: GetResponse) -> HttpResponse:
-    """Adds Search instance to request as `request.search`."""
-    request.search = SimpleLazyObject(lambda: Search(request))
-    return get_response(request)
+sorter_middleware = lazy_object_middleware("sorter")(Sorter)
 
-
-@middleware
-def sorter_middleware(request: HttpRequest, get_response: GetResponse) -> HttpResponse:
-    """Adds Sorter instance to request as `request.sorter`."""
-    request.sorter = SimpleLazyObject(lambda: Sorter(request))
-    return get_response(request)
-
-
-@middleware
-def user_agent_middleware(
-    request: HttpRequest, get_response: GetResponse
-) -> HttpResponse:
-    """Adds user_agent string to request."""
-    request.user_agent = SimpleLazyObject(lambda: user_agent(request))
-    return get_response(request)
+user_agent_middleware = lazy_object_middleware("user_agent")(user_agent)
--- radiofeed/episodes/middleware.py
+++ radiofeed/episodes/middleware.py
@@ -1,15 +1,6 @@
 from __future__ import annotations
 
-from django.http import HttpRequest, HttpResponse
-from django.utils.functional import SimpleLazyObject
-
-from radiofeed.common.decorators import middleware
-from radiofeed.common.types import GetResponse
+from radiofeed.common.decorators import lazy_object_middleware
 from radiofeed.episodes.player import Player
 
-
-@middleware
-def player_middleware(request: HttpRequest, get_response: GetResponse) -> HttpResponse:
-    """Adds Player instance to request as `request.player`."""
-    request.player = SimpleLazyObject(lambda: Player(request))
-    return get_response(request)
+player_middleware = lazy_object_middleware("player")(Player)
