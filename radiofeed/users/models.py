1695a6a0c42177110b6a7126a12c10be18027db9
Dan Jacob <danjac2018@gmail.com>
2023-01-12 12:24:12 +0200

    Pygupgrade update

--- radiofeed/users/models.py
+++ radiofeed/users/models.py
@@ -61,7 +61,7 @@ class User(AbstractUser):
 
     send_email_notifications: bool = models.BooleanField(default=True)
 
-    objects: models.Manager["User"] = UserManager()
+    objects: models.Manager[User] = UserManager()
 
     def get_email_addresses(self) -> set[str]:
         """Get set of all emails belonging to user."""
