f3e40a79132331af6dde57d22260fa34bf583100
Dan Jacob <danjac2018@gmail.com>
2023-01-15 00:35:23 +0200

    Made utils pkg

--- radiofeed/feedparser/tests/test_feed_parser.py
+++ radiofeed/feedparser/tests/test_feed_parser.py
@@ -10,7 +10,6 @@ import pytest
 
 from django.utils import timezone
 
-from radiofeed.common.date_parser import parse_date
 from radiofeed.episodes.factories import create_episode
 from radiofeed.episodes.models import Episode
 from radiofeed.feedparser.exceptions import (
@@ -28,6 +27,7 @@ from radiofeed.feedparser.feed_parser import (
 )
 from radiofeed.podcasts.factories import create_category, create_podcast
 from radiofeed.podcasts.models import Podcast
+from radiofeed.utils.date_parser import parse_date
 
 
 class MockClient:
