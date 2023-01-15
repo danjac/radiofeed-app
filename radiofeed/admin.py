f431ab58ed1e127b309a2b8851122c861f926e8b
Dan Jacob <danjac2018@gmail.com>
2023-01-15 14:55:59 +0200

    db module

--- radiofeed/admin.py
+++ radiofeed/admin.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from django.db import connections
 from django.http import HttpRequest
 
 from radiofeed.db.fast_count import FastCounter, FastCountPaginator
@@ -17,18 +16,3 @@ class FastCountAdminMixin(T_ModelAdmin):
         qs = super().get_queryset(request)
         qs.count = qs.fast_count
         return qs
-
-
-def get_reltuple_count(db: str, table: str) -> int:
-    """Returns result of SELECT reltuples.
-
-    Args:
-        db: database name
-        table: table name
-
-    Returns:
-        number of rows
-    """
-    cursor = connections[db].cursor()
-    cursor.execute("SELECT reltuples FROM pg_class WHERE relname = %s", [table])
-    return int(cursor.fetchone()[0])
