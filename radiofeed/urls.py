09b1c73858ed10dcc1b3678a29ce9a91f62c797c
Dan Jacob <danjac2018@gmail.com>
2023-01-15 12:55:00 +0200

    Moving modules

--- radiofeed/urls.py
+++ radiofeed/urls.py
@@ -5,14 +5,28 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from radiofeed import views
+
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 
+
 urlpatterns = [
-    path("", include("radiofeed.common.urls")),
     path("", include("radiofeed.episodes.urls")),
     path("", include("radiofeed.podcasts.urls")),
     path("", include("radiofeed.users.urls")),
     path("account/", include("allauth.urls")),
+    path("about/", views.about_page, name="about"),
+    path("accept-cookies/", views.accept_cookies, name="accept_cookies"),
+    path(
+        "covers/<str:encoded_url>/<int:size>/cover.webp",
+        views.cover_image,
+        name="cover_image",
+    ),
+    path("robots.txt", views.robots, name="robots"),
+    path("service-worker.js", views.service_worker, name="service_worker"),
+    path("manifest.json", views.manifest, name="manifest"),
+    path("favicon.ico", views.favicon, name="favicon"),
+    path(".well-known/security.txt", views.security, name="security"),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
