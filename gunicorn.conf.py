5811d7f7c3a1957d92c7066a1def3afd319ef58d
Dan Jacob <danjac2018@gmail.com>
2023-01-18 20:33:25 +0200

    Add gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,5 @@ from __future__ import annotations
 
 import multiprocessing
 
-# bind = "127.0.0.1:8000"
-
+# https://docs.gunicorn.org/en/stable/configure.html#configuration-file
 workers = multiprocessing.cpu_count() * 2 + 1
