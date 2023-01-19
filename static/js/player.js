6eb0563b95f4630410199e1bca03c4e73fc7c5f7
Dan Jacob <danjac2018@gmail.com>
2023-01-19 14:05:56 +0200

    Player js fix

--- static/js/player.js
+++ static/js/player.js
@@ -150,7 +150,7 @@ document.addEventListener("alpine:init", () => {
                     headers: {
                         "X-CSRFToken": this.csrfToken,
                     },
-                    body: new URLSearch({
+                    body: new URLSearchParams({
                         current_time: this.runtime,
                     }),
                 });
