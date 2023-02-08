af5b020ac9d9d974b6fd6a8dab6f34274211f08a
Dan Jacob <danjac2018@gmail.com>
2023-02-08 11:53:40 +0200

    Add websub app

---
+++ radiofeed/websub/apps.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class WebsubConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "websub"
---
+++ radiofeed/websub/tests.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+# Create your tests here.
