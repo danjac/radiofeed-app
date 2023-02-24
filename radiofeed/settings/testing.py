2f69d44a7764e170aa4ab4e4c259bcb0198edd13
Dan Jacob <danjac2018@gmail.com>
2023-02-24 18:00:11 +0200

    Config admin site header

--- radiofeed/settings/testing.py
+++ radiofeed/settings/testing.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from radiofeed.settings.base import *  # noqa
 from radiofeed.settings.base import configure_databases, configure_templates
 
-DEBUG = True
-
 DATABASES = configure_databases(conn_max_age=0)
 
 TEMPLATES = configure_templates(debug=True)
