fffb07acb4911b7f5e74aedb5652c369560880a0
Dan Jacob <danjac2018@gmail.com>
2023-02-24 17:40:35 +0200

    Remove defaults

--- radiofeed/settings/databases.py
+++ radiofeed/settings/databases.py
@@ -4,18 +4,16 @@ import dj_database_url
 
 from radiofeed.settings.base import config
 
-DATABASE_URL = config(
-    "DATABASE_URL",
-    default="postgresql://postgres:password@localhost:5432/postgres",
-)
-
 
 def configure_databases(*, conn_max_age: int) -> dict:
     """Build DATABASES configuration."""
     return {
         "default": {
             **dj_database_url.parse(
-                DATABASE_URL,
+                config(
+                    "DATABASE_URL",
+                    default="postgresql://postgres:password@localhost:5432/postgres",
+                ),
                 conn_max_age=conn_max_age,
                 conn_health_checks=conn_max_age > 0,
             ),
