9696656faddecf1928eadb309403dfa7b72976c8
Dan Jacob <danjac2018@gmail.com>
2023-04-05 23:53:04 +0300

    Cache setting fix

--- radiofeed/config/settings.py
+++ radiofeed/config/settings.py
@@ -105,15 +105,17 @@ DATABASES = {
 REDIS_URL = config("REDIS_URL", default="redis://127.0.0.1:6379/0")
 
 CACHES: dict = {
-    "BACKEND": "django_redis.cache.RedisCache",
-    "LOCATION": REDIS_URL,
-    "OPTIONS": {
-        "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        # Mimicing memcache behavior.
-        # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
-        "IGNORE_EXCEPTIONS": True,
-        "PARSER_CLASS": "redis.connection.HiredisParser",
-    },
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # Mimicing memcache behavior.
+            # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
+            "IGNORE_EXCEPTIONS": True,
+            "PARSER_CLASS": "redis.connection.HiredisParser",
+        },
+    }
 }
 
 # Templates
