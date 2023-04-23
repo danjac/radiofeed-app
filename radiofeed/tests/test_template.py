7934040f16aee64e971bdd5994e45a8c84f72002
Dan Jacob <danjac2018@gmail.com>
2023-04-23 15:52:37 +0300

    refactor: move form snippets into default form template

--- radiofeed/tests/test_template.py
+++ radiofeed/tests/test_template.py
@@ -190,7 +190,7 @@ class TestPaginationLinks:
 class TestFormFields:
     @pytest.fixture
     def tmpl(self):
-        return get_template("forms/field.html")
+        return get_template("forms/_field.html")
 
     def test_is_hidden(self, tmpl, mocker):
         field = mocker.Mock()
