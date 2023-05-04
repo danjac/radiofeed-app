0e00b2b1a56054aa9db98f9525cb4c65aa91114a
Dan Jacob <danjac2018@gmail.com>
2023-05-04 21:16:02 +0300

    test: add general fixtures

--- conftest.py
+++ conftest.py
@@ -1,43 +1,8 @@
 from __future__ import annotations
 
-import pytest
-from django.core.cache import cache
-from django.http import HttpResponse
-from faker import Faker
-
 pytest_plugins = [
-    "radiofeed.podcasts.fixtures",
+    "radiofeed.fixtures",
     "radiofeed.episodes.fixtures",
+    "radiofeed.podcasts.fixtures",
     "radiofeed.users.fixtures",
 ]
-
-
-@pytest.fixture(autouse=True)
-def settings_overrides(settings):
-    """Default settings for all tests."""
-    settings.CACHES = {
-        "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}
-    }
-    settings.LOGGING = None
-    settings.PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-
-
-@pytest.fixture(scope="session")
-def faker():
-    faker = Faker()
-    yield faker
-    faker.unique.clear()
-
-
-@pytest.fixture
-def locmem_cache(settings):
-    settings.CACHES = {
-        "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}
-    }
-    yield
-    cache.clear()
-
-
-@pytest.fixture(scope="session")
-def get_response():
-    return lambda req: HttpResponse()
---
+++ radiofeed/fixtures.py
@@ -0,0 +1,35 @@
+import pytest
+from django.core.cache import cache
+from django.http import HttpResponse
+from faker import Faker
+
+
+@pytest.fixture(autouse=True)
+def settings_overrides(settings):
+    """Default settings for all tests."""
+    settings.CACHES = {
+        "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}
+    }
+    settings.LOGGING = None
+    settings.PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+
+@pytest.fixture(scope="session")
+def faker():
+    faker = Faker()
+    yield faker
+    faker.unique.clear()
+
+
+@pytest.fixture
+def locmem_cache(settings):
+    settings.CACHES = {
+        "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}
+    }
+    yield
+    cache.clear()
+
+
+@pytest.fixture(scope="session")
+def get_response():
+    return lambda req: HttpResponse()
