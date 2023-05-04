48ca365f5a0aec8b1e92f5fef21ad8a230063c29
Dan Jacob <danjac2018@gmail.com>
2023-05-04 22:45:23 +0300

    refactor: use USE_HTTPS setting for protocol

--- radiofeed/podcasts/emails.py
+++ radiofeed/podcasts/emails.py
@@ -66,7 +66,7 @@ def send_recommendations_email(
         "recipient": user,
         "podcasts": podcasts,
         "site": Site.objects.get_current(),
-        "protocol": "https" if settings.SECURE_SSL_REDIRECT else "http",
+        "protocol": "https" if settings.USE_HTTPS else "http",
     }
 
     send_mail(
