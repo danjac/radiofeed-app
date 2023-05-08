934fbf16ace308fe4e678db6486b20fbc00abfc6
Dan Jacob <danjac2018@gmail.com>
2023-05-08 17:49:00 +0300

    fix: EMAIL_USE_SSL used wrong environment variable

--- config/settings.py
+++ config/settings.py
@@ -191,8 +191,8 @@ else:
     EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default=None)
     EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default=None)
 
+    EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
     EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
-    EMAIL_USE_SSL = env.bool("EMAIL_USE_TLS", default=False)
 
 ADMINS = getaddresses(env.list("ADMINS", default=[]))
 
