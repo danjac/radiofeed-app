5906797c67347439cef6512ebc9cc8f207bfbe83
Dan Jacob <danjac2018@gmail.com>
2023-05-10 20:46:11 +0300

    refactor: optimize feed parsing in podping

--- radiofeed/feedparser/management/commands/podping.py
+++ radiofeed/feedparser/management/commands/podping.py
@@ -76,7 +76,6 @@ class Command(BaseCommand):
         )
 
     def _parse_feeds(self, urls: set[str], rewind_from: datetime) -> None:
-        self.stdout.write(f"Parsing {len(urls)} urls")
         for podcast in Podcast.objects.filter(
             Q(parsed__isnull=True) | Q(parsed__lt=rewind_from),
             rss__in=urls,
