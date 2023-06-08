4c298e5fc1f44d3b3df61da53ddeb543ac9b6ed7
Dan Jacob <danjac2018@gmail.com>
2023-06-08 13:48:32 +0300

    feat: add websub admin

--- radiofeed/websub/admin.py
+++ radiofeed/websub/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from radiofeed.websub.models import Subscription
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Django admin for Subscription model."""
+
+    list_display = ("podcast", "mode", "requested", "verified")
+    raw_id_fields = ("podcast",)
