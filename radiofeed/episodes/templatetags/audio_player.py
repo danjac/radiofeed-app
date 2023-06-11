af9565f8ac7edf960a5ed64c9fc03149266c1339
Dan Jacob <danjac2018@gmail.com>
2023-06-11 11:31:28 +0300

    refactor: ruff fixes

--- radiofeed/episodes/templatetags/audio_player.py
+++ radiofeed/episodes/templatetags/audio_player.py
@@ -7,16 +7,15 @@ register = template.Library()
 @register.inclusion_tag("episodes/_audio_player.html", takes_context=True)
 def audio_player(context: RequestContext) -> dict:
     """Returns details of current episode in player."""
-    if episode_id := context.request.player.get():
-        if (
-            audio_log := context.request.user.audio_logs.filter(episode__pk=episode_id)
-            .select_related("episode", "episode__podcast")
-            .first()
-        ):
-            return {
-                "request": context.request,
-                "audio_log": audio_log,
-                "is_playing": True,
-            }
+    if (episode_id := context.request.player.get()) and (
+        audio_log := context.request.user.audio_logs.filter(episode__pk=episode_id)
+        .select_related("episode", "episode__podcast")
+        .first()
+    ):
+        return {
+            "request": context.request,
+            "audio_log": audio_log,
+            "is_playing": True,
+        }
 
     return {}
