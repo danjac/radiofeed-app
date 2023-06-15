346dec37dd2c3fff85992399b5579a1a578236d9
Dan Jacob <danjac2018@gmail.com>
2023-06-15 18:12:42 +0300

    refactor: import fixes

--- radiofeed/futures.py
+++ radiofeed/futures.py
@@ -1,24 +1,23 @@
 import itertools
 from collections.abc import Callable, Iterable
-from concurrent.futures import Future
-from concurrent.futures import ThreadPoolExecutor as BaseThreadPoolExecutor
+from concurrent import futures
 
 from django.db import connections
 
 
-class ThreadPoolExecutor(BaseThreadPoolExecutor):
+class ThreadPoolExecutor(futures.ThreadPoolExecutor):
     """ThreadPoolExecutor with safemap() and safesubmit() methods."""
 
-    def _close_db_connections(self, future: Future) -> None:
+    def _close_db_connections(self, future: futures.Future) -> None:
         connections.close_all()
 
-    def safesubmit(self, fn: Callable, *args, **kwargs) -> Future:
+    def safesubmit(self, fn: Callable, *args, **kwargs) -> futures.Future:
         """Runs submit() on function, closing the DB connections when done."""
         future = self.submit(fn, *args, **kwargs)
         future.add_done_callback(self._close_db_connections)
         return future
 
-    def safemap(self, fn: Callable, *iterables: Iterable) -> list[Future]:
+    def safemap(self, fn: Callable, *iterables: Iterable) -> list[futures.Future]:
         """Runs safesubmit() on each item of iterators, returning list of futures."""
         return [
             self.safesubmit(fn, item)
