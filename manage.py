82a813cbc5026e160cfaeff9b7e05c6e4c7f7c5f
Dan Jacob <danjac2018@gmail.com>
2023-06-16 21:35:37 +0300

    refactor: tidy up manage.py

--- manage.py
+++ manage.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
-"""Django's command-line utility for administrative tasks."""
+"Django's command-line utility for administrative tasks."
 
 
-# Standard Library
 import os
 import sys
+from typing import Final
+
+_IMPORT_ERR_MSG: Final = (
+    "Couldn't import Django. Are you sure it's installed and "
+    "available on your PYTHONPATH environment variable? Did you "
+    "forget to activate a virtual environment?"
+)
 
 
-def main():
+def main() -> None:
     """Django management entry point."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiofeed.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError(_IMPORT_ERR_MSG) from exc
     execute_from_command_line(sys.argv)
 
 
