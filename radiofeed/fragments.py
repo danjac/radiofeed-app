423ea19c9f16d52695c0c1050c3f12cc2647c4c1
Dan Jacob <danjac2018@gmail.com>
2023-06-20 17:07:20 +0300

    refactor: move function to own module

---
+++ radiofeed/fragments.py
@@ -0,0 +1,38 @@
+from django.http import HttpRequest, HttpResponse
+from render_block import render_block_to_string
+
+
+def render_template_fragments(
+    request: HttpRequest,
+    template_name: str,
+    context: dict | None = None,
+    *,
+    use_blocks: list[str] | None = None,
+    status: int | None = None,
+) -> HttpResponse:
+    """Renders HTMX fragments.
+
+    Individual template blocks in the template are rendered instead of the whole template.
+
+    A list of the blocks rendered is passed into the template context as `template_blocks`.
+    """
+
+    use_blocks = use_blocks or []
+
+    context = {
+        **(context or {}),
+        "template_blocks": use_blocks,
+    }
+
+    return HttpResponse(
+        [
+            render_block_to_string(
+                template_name,
+                block,
+                context,
+                request=request,
+            )
+            for block in use_blocks
+        ],
+        status=status,
+    )
