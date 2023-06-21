d1942cbdabfe078766daae624fd73b3a1229664d
Dan Jacob <danjac2018@gmail.com>
2023-06-21 19:32:39 +0300

    refactor: private form changes

---
+++ radiofeed/podcasts/tests/test_forms.py
@@ -0,0 +1,46 @@
+import pytest
+
+from radiofeed.podcasts.factories import create_podcast, create_subscription
+from radiofeed.podcasts.forms import PrivateFeedForm
+from radiofeed.podcasts.models import Subscription
+
+
+class TestPrivateFeedForm:
+    rss = "https://example.com/rss"
+
+    @pytest.mark.django_db()
+    def test_new_feed(self, user):
+        form = PrivateFeedForm(user, {"rss": self.rss})
+        assert form.is_valid()
+
+        podcast = form.save()
+        assert podcast.private
+        assert podcast.rss == self.rss
+
+        assert Subscription.objects.filter(podcast=podcast, subscriber=user).exists()
+
+    @pytest.mark.django_db()
+    def test_feed_exists(self, user):
+        create_podcast(private=True, rss=self.rss)
+        form = PrivateFeedForm(user, {"rss": self.rss})
+        assert form.is_valid()
+
+        podcast = form.save()
+        assert podcast.private
+        assert podcast.rss == self.rss
+
+        assert Subscription.objects.filter(podcast=podcast, subscriber=user).exists()
+
+    @pytest.mark.django_db()
+    def test_feed_not_private(self, user):
+        create_podcast(private=False, rss=self.rss)
+        form = PrivateFeedForm(user, {"rss": self.rss})
+        assert not form.is_valid()
+
+    @pytest.mark.django_db()
+    def test_user_subscribed(self):
+        user = create_subscription(
+            podcast=create_podcast(private=True, rss=self.rss)
+        ).subscriber
+        form = PrivateFeedForm(user, {"rss": self.rss})
+        assert not form.is_valid()
