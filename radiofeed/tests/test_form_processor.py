60616229a635308d79caf80653d4d7d69b8d0f3c
Dan Jacob <danjac2018@gmail.com>
2023-06-28 18:26:55 +0300

    refactor: form_processor module

---
+++ radiofeed/tests/test_form_processor.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from radiofeed.form_processor import process_form
+
+
+class MyForm(forms.Form):
+    name = forms.CharField(required=True, min_length=1)
+
+
+class TestProcessForm:
+    def test_get(self, rf):
+        req = rf.get("/")
+        form, success = process_form(MyForm, req)
+        assert isinstance(form, MyForm)
+        assert success is False
+        assert not form.errors
+
+    def test_post_ok(self, rf):
+        req = rf.post("/", {"name": "test"})
+        form, success = process_form(MyForm, req)
+        assert isinstance(form, MyForm)
+        assert success is True
+        assert not form.errors
+
+    def test_post_invalid(self, rf):
+        req = rf.post("/")
+        form, success = process_form(MyForm, req)
+        assert isinstance(form, MyForm)
+        assert success is False
+        assert form.errors
