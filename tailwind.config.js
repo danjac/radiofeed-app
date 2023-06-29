0bdc15bd2079a1c552a8e1a2957819570d4ad163
Dan Jacob <danjac2018@gmail.com>
2023-06-29 14:26:56 +0300

    feat: message styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ module.exports = {
         purgeLayersByDefault: true,
     },
     content: ["./templates/**/*.html"],
-    safelist: [
-        "message-debug",
-        "message-error",
-        "message-info",
-        "message-success",
-        "message-warning",
-    ],
     keyframes: true,
     variants: {
         textColor: ["responsive", "hover", "focus", "visited", "dark"],
