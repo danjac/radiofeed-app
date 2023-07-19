ea9e56e002f6b87692a782f5cf4cf4afe97ccca2
Dan Jacob <danjac2018@gmail.com>
2023-07-19 19:34:45 +0300

    Tidy up email stuff

--- radiofeed/podcasts/emails.py
+++ radiofeed/podcasts/emails.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.db.models import Q
 from django.template import loader
 
 from radiofeed.episodes.models import AudioLog, Bookmark
@@ -7,15 +8,14 @@ from radiofeed.podcasts.models import Podcast, Recommendation, Subscription
 from radiofeed.users.models import User
 
 
-def send_recommendations_email(user: User, max_podcasts: int = 6) -> None:
+def send_recommendations_email(user: User, num_podcasts: int = 6) -> None:
     """Sends email to user with a list of recommended podcasts.
 
     Recommendations based on their subscriptions and listening history, or latest promoted podcasts.
 
-    Recommended podcasts are saved to the database, so the user is not recommended the
-    same podcasts more than once.
+    Recommended podcasts are saved to the database, so the user is not recommended the same podcasts more than once.
 
-    If there are fewer than `min_podcasts` then no email will be sent.
+    If no matching podcasts are found, no email is sent.
     """
 
     # listened, bookmark, subscribed
@@ -38,36 +38,34 @@ def send_recommendations_email(user: User, max_podcasts: int = 6) -> None:
         )
     )
 
-    # recommended already or already listened etc.
-
-    exclude_podcast_ids = podcast_ids | set(
-        user.recommended_podcasts.values_list("pk", flat=True)
-    )
-
     # pick highest matches
 
-    podcasts = {
-        recommendation.recommended
-        for recommendation in Recommendation.objects.filter(podcast__pk__in=podcast_ids)
-        .exclude(recommended__pk__in=exclude_podcast_ids)
+    recommended_ids = set(
+        Recommendation.objects.filter(podcast__pk__in=podcast_ids)
         .select_related("recommended")
         .order_by(
             "-similarity",
             "-frequency",
-            "-recommended__pub_date",
-        )[:max_podcasts]
-    }
-
-    # latest promotions
-
-    if remainder := max_podcasts - len(podcasts):
-        podcasts = podcasts | set(
-            Podcast.objects.filter(promoted=True, private=False)
-            .exclude(pk__in=exclude_podcast_ids)
-            .order_by("-pub_date")[:remainder]
         )
+        .values_list("recommended", flat=True)[:num_podcasts]
+    )
 
-    if podcasts:
+    # include recommended + promoted
+    # exclude any podcasts already recommended/subscribed/listened etc
+
+    podcasts = (
+        Podcast.objects.filter(
+            Q(pk__in=recommended_ids) | Q(promoted=True),
+            private=False,
+        )
+        .exclude(
+            pk__in=podcast_ids
+            | set(user.recommended_podcasts.values_list("pk", flat=True))
+        )
+        .order_by("-pub_date")
+    )[:num_podcasts]
+
+    if podcasts.exists():
         user.recommended_podcasts.add(*podcasts)
 
         context = {
