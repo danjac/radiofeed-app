d583fc21637ec9f93d0004b5b18b179c0c5ae8c4
Dan Jacob <danjac2018@gmail.com>
2023-07-25 07:57:07 +0300

    Tidy up JS code

--- static/js/app.js
+++ static/js/app.js
@@ -15,13 +15,10 @@ window.Alpine = Alpine;
 Alpine.start();
 
 document.body.addEventListener("htmx:beforeSwap", function (event) {
-    switch (event.detail.xhr.status) {
-        case 400:
-        case 422:
-            // allow invalid form responses to swap as we are using this as a signal that
-            // a form was submitted with bad data and want to rerender with the errors
-            event.detail.shouldSwap = true;
-            event.detail.isError = false;
-            break;
+    // allow invalid form responses to swap as we are using this as a signal that
+    // a form was submitted with bad data and want to rerender with the errors
+    if (event.detail.xhr.status === 422) {
+        event.detail.shouldSwap = true;
+        event.detail.isError = false;
     }
 });
