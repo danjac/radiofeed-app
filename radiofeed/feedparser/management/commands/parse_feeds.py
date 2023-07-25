aa29dbeff25089a08f2af5cbfba9acac365d9841
Dan Jacob <danjac2018@gmail.com>
2023-07-25 21:32:15 +0300

    Test commit

--- radiofeed/feedparser/management/commands/parse_feeds.py
+++ radiofeed/feedparser/management/commands/parse_feeds.py
@@ -33,7 +33,6 @@ class Command(BaseCommand):
 
     def handle(self, **options) -> None:
         """Command handler implementation."""
-
         while True:
             with DatabaseSafeThreadPoolExecutor() as executor:
                 wait(
