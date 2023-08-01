a95c287988e776e3359e85ddc553510f69c635dc
Dan Jacob <danjac2018@gmail.com>
2023-08-01 18:10:11 +0300

    Tidy up

--- radiofeed/client.py
+++ radiofeed/client.py
@@ -5,9 +5,12 @@ from django.conf import settings
 def http_client(
     *, headers: dict | None = None, timeout: int = 10, **kwargs
 ) -> httpx.Client:
-    """Returns HTTP client instance"""
+    """Returns HTTP client instance."""
     return httpx.Client(
-        headers={"User-Agent": settings.USER_AGENT, **(headers or {})},
+        headers={
+            "User-Agent": settings.USER_AGENT,
+            **(headers or {}),
+        },
         timeout=timeout,
         **kwargs,
     )
