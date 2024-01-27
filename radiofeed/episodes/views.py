ed5852392dc6f7a666a6fa2d626f4d712f0c310e
Dan Jacob <danjac2018@gmail.com>
2024-01-27 23:30:32 +0200

    Search URL fixes

--- radiofeed/episodes/views.py
+++ radiofeed/episodes/views.py
@@ -50,7 +50,6 @@ def index(request: HttpRequest) -> HttpResponse:
         {
             "promoted": promoted,
             "has_subscriptions": has_subscriptions,
-            "search_url": reverse("episodes:search_episodes"),
         },
     )
 
