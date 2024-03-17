24cb84521fc4ff6040495699eb760f2985dcd5df
Dan Jacob <danjac2018@gmail.com>
2024-03-17 12:26:32 +0200

    Remove old validators

--- radiofeed/feedparser/tests/test_models.py
+++ radiofeed/feedparser/tests/test_models.py
@@ -4,8 +4,64 @@ import pytest
 from django.utils import timezone
 from pydantic import ValidationError
 
-from radiofeed.feedparser.models import Feed, Item
+from radiofeed.feedparser.models import Feed, Item, duration, explicit, language, url
 from radiofeed.feedparser.tests.factories import create_feed, create_item
+
+
+class TestLanguage:
+    def test_full_locale(self):
+        assert language("en-US") == "en"
+
+    def test_uppercase(self):
+        assert language("FI") == "fi"
+
+
+class TestExplicit:
+    def test_true(self):
+        assert explicit("yes") is True
+
+    def test_false(self):
+        assert explicit("no") is False
+
+    def test_none(self):
+        assert explicit(None) is False
+
+
+class TestUrl:
+    def test_ok(self):
+        assert (
+            url("http://yhanewashington.wixsite.com/1972")
+            == "http://yhanewashington.wixsite.com/1972"
+        )
+
+    def test_domain_only(self):
+        assert (
+            url("yhanewashington.wixsite.com/1972")
+            == "http://yhanewashington.wixsite.com/1972"
+        )
+
+    def test_bad_url(self):
+        assert url("yhanewashington") is None
+
+    def test_none(self):
+        assert url(None) is None
+
+
+class TestDuration:
+    @pytest.mark.parametrize(
+        ("value", "expected"),
+        [
+            pytest.param(None, "", id="none"),
+            pytest.param("", "", id="empty"),
+            pytest.param("invalid", "", id="invalid"),
+            pytest.param("300", "300", id="seconds only"),
+            pytest.param("10:30", "10:30", id="minutes and seconds"),
+            pytest.param("10:30:59", "10:30:59", id="hours, minutes and seconds"),
+            pytest.param("10:30:99", "10:30", id="hours, minutes and invalid seconds"),
+        ],
+    )
+    def test_parse_duration(self, value, expected):
+        assert duration(value) == expected
 
 
 class TestItem:
