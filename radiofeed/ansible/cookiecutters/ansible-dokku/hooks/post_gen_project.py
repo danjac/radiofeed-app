bb8d969243da1c1c1d8f7f24ff08f3fda9014f76
Dan Jacob <danjac2018@gmail.com>
2024-03-29 14:34:53 +0200

    Add ansible app

--- radiofeed/ansible/cookiecutters/ansible-dokku/hooks/post_gen_project.py
+++ radiofeed/ansible/cookiecutters/ansible-dokku/hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 import sys
 
 sys.stdout.write(
+    "\n\n\n"
     "###########################################################\n"
     "#   USE ANSIBLE-VAULT TO ENCRYPT ALL SENSITIVE FILES !!!  #\n"
     "###########################################################\n"
