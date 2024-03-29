70f825da2098716dec8aa89dc15fb4411161a860
Dan Jacob <danjac2018@gmail.com>
2024-03-29 22:08:08 +0200

    Config changes

--- radiofeed/ansible/management/commands/generate_ansible_config.py
+++ radiofeed/ansible/management/commands/generate_ansible_config.py
@@ -1,6 +1,8 @@
+import pathlib
 from argparse import ArgumentParser
 
 from cookiecutter.main import cookiecutter
+from django.conf import settings
 from django.contrib.auth.management import get_system_username
 from django.core.management.base import BaseCommand
 from django.core.management.utils import get_random_secret_key
@@ -28,7 +30,8 @@ class Command(BaseCommand):
         """Command handler implementation."""
 
         cookiecutter(
-            options["cookiecutter"],
+            str(pathlib.Path(settings.BASE_DIR / "cookiecutters")),
+            directory=options["cookiecutter"],
             overwrite_if_exists=options["overwrite"],
             extra_context={
                 "secret_key": get_random_secret_key(),
