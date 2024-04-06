de397a4fc34f085d7a6e7173bc67f9b371c3c6e5
Dan Jacob <danjac2018@gmail.com>
2024-04-06 13:13:46 +0300

    Pagination tests

---
+++ radiofeed/tests/test_pagination.py
@@ -0,0 +1,37 @@
+import pytest
+from django_htmx.middleware import HtmxDetails
+
+from radiofeed.middleware import Pagination
+from radiofeed.pagination import PaginationResponse
+from radiofeed.podcasts.tests.factories import create_podcast
+from radiofeed.tests.factories import create_batch
+
+
+class TestPaginationResponse:
+    @pytest.fixture()
+    def podcasts(self):
+        return create_batch(create_podcast, 12)
+
+    @pytest.mark.django_db()
+    def test_not_htmx(self, rf, podcasts):
+        req = rf.get("/")
+        req.htmx = False
+
+        req.pagination = Pagination(req)
+
+        response = PaginationResponse(req, podcasts, "index.html")
+        assert response.template_name == "index.html"
+        assert response.context_data["page_obj"].object_list == podcasts
+        assert response.context_data["pagination_target"] == "pagination"
+
+    @pytest.mark.django_db()
+    def test_htmx_matches_pagination_target(self, rf, podcasts):
+        req = rf.get("/", HTTP_HX_REQUEST="true", HTTP_HX_TARGET="pagination")
+
+        req.htmx = HtmxDetails(req)
+        req.pagination = Pagination(req)
+
+        response = PaginationResponse(req, podcasts, "index.html")
+        assert response.template_name == "index.html#pagination"
+        assert response.context_data["page_obj"].object_list == podcasts
+        assert response.context_data["pagination_target"] == "pagination"
