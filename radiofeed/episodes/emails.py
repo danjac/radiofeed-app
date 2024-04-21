24f31f3c888250aca12c8cbafc481eeaf2819e48
Dan Jacob <danjac2018@gmail.com>
2024-04-21 14:03:25 +0300

    Simplify send episodes email

--- radiofeed/episodes/emails.py
+++ radiofeed/episodes/emails.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
 
-from django.db.models import Count, OuterRef, Subquery
+from django.db.models import OuterRef, Subquery
 from django.utils import timezone
 
 from radiofeed.emails import send_email
-from radiofeed.episodes.models import AudioLog, Episode
+from radiofeed.episodes.models import Episode
 from radiofeed.podcasts.models import Podcast
 from radiofeed.users.models import User
 
@@ -17,47 +17,48 @@ def send_new_episodes_email(
 ) -> None:
     """Sends notifications for podcasts you listen to the most (based on history)."""
 
-    subscribed_podcast_ids = set(user.subscriptions.values_list("podcast", flat=True))
-    if not subscribed_podcast_ids:
+    if not (
+        subscribed_podcast_ids := set(
+            user.subscriptions.values_list("podcast", flat=True)
+        )
+    ):
         return
 
     # exclude any I have listened to or bookmarked
+
     exclude_episode_ids = (
         set(user.bookmarks.values_list("episode", flat=True))
         | set(user.audio_logs.values_list("episode", flat=True))
         | set(user.recommended_episodes.values_list("pk", flat=True))
     )
 
-    latest_episode_ids = set(
-        Podcast.objects.annotate(
-            num_listens=Count(
-                AudioLog.objects.filter(user=user.pk, episode__podcast=OuterRef("pk"))
-                .values("episode__podcast")
-                .distinct(),
-            ),
-            latest_episode=Subquery(
-                Episode.objects.filter(
-                    podcast=OuterRef("pk"), pub_date__gte=timezone.now() - since
-                )
-                .exclude(pk__in=exclude_episode_ids)
-                .order_by("-pub_date")
-                .values("pk")[:1]
-            ),
+    # we want to have just one episode/podcast
+    # for example, if a podcast has 3 episodes in past 24 hours, just show the latest
+
+    if not (
+        latest_episode_ids := set(
+            Podcast.objects.annotate(
+                latest_episode=Subquery(
+                    Episode.objects.filter(
+                        podcast=OuterRef("pk"),
+                        pub_date__gte=timezone.now() - since,
+                    )
+                    .exclude(pk__in=exclude_episode_ids)
+                    .order_by("-pub_date")
+                    .values("pk")[:1]
+                ),
+            )
+            .filter(
+                pk__in=subscribed_podcast_ids,
+                latest_episode__isnull=False,
+            )
+            .values_list("latest_episode", flat=True)
         )
-        .filter(
-            pk__in=subscribed_podcast_ids,
-            num_listens__gt=0,
-            latest_episode__isnull=False,
-        )
-        .order_by("-num_listens", "-pub_date")
-        .values_list("latest_episode", flat=True)
-    )
-    if not latest_episode_ids:
+    ):
         return
 
     if episodes := (
         Episode.objects.filter(pk__in=latest_episode_ids)
-        .exclude(pk__in=exclude_episode_ids)
         .order_by("-pub_date")
         .select_related("podcast")
     )[:num_episodes]:
