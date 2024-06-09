367e65982f6b957fbbb24ddc8fc090385fd124d3
Dan Jacob <danjac2018@gmail.com>
2024-06-09 20:16:56 +0300

    Image adjustments

--- listenwave/cover_image.py
+++ listenwave/cover_image.py
@@ -2,7 +2,7 @@ import functools
 import pathlib
 import urllib.parse
 from enum import StrEnum
-from typing import Final, TypedDict
+from typing import Final
 
 from django.conf import settings
 from django.core.signing import Signer
@@ -11,7 +11,7 @@ from django.templatetags.static import static
 from django.urls import reverse
 
 _COVER_IMAGE_SIZES: Final = (
-    64,
+    96,
     128,
     160,
     192,
@@ -27,31 +27,10 @@ class CoverImageSize(StrEnum):
     LARGE = "lg"
 
 
-class CoverImageInfo(TypedDict):
-    """Specific rendering information for each size."""
-
-    default_size: int
-
-    min_size: tuple[int, int]
-    max_size: tuple[int, int]
-
-
 _COVER_IMAGE_INFO = {
-    CoverImageSize.SMALL: CoverImageInfo(
-        default_size=64,
-        min_size=(0, 0),
-        max_size=(0, 0),
-    ),
-    CoverImageSize.MEDIUM: CoverImageInfo(
-        default_size=160,
-        min_size=(128, 767),
-        max_size=(160, 768),
-    ),
-    CoverImageSize.LARGE: CoverImageInfo(
-        default_size=224,
-        min_size=(192, 1525),
-        max_size=(224, 1526),
-    ),
+    CoverImageSize.SMALL: (96, 96),
+    CoverImageSize.MEDIUM: (128, 160),
+    CoverImageSize.LARGE: (192, 224),
 }
 
 
@@ -63,43 +42,30 @@ def is_cover_image_size(size: int) -> bool:
 @functools.cache
 def get_cover_image_attrs(cover_url: str, variant: CoverImageSize) -> dict[str, str]:
     """Returns the HTML attributes for an image."""
-    info = _COVER_IMAGE_INFO[variant]
+    min_size, full_size = _COVER_IMAGE_INFO[variant]
+    full_size_src = get_cover_image_url(cover_url, full_size)
 
     attrs = {
-        "height": info["default_size"],
-        "width": info["default_size"],
-    }
-
-    if not cover_url:
-        return attrs | {
-            "src": get_placeholder_url(info["default_size"]),
-        }
-
-    attrs |= {
-        "src": get_cover_image_url(cover_url, info["default_size"]),
+        "height": full_size,
+        "width": full_size,
+        "src": full_size_src,
     }
 
     # one size only
-    if (0, 0) in (info["min_size"], info["max_size"]):
-        return attrs | {"size": info["default_size"]}
+    if min_size == full_size:
+        return attrs
 
     srcset = ", ".join(
         [
-            f"{get_cover_image_url(cover_url, size)} {width}w"
-            for size, width in (
-                info["max_size"],
-                info["min_size"],
-            )
+            f"{full_size_src} {full_size}w",
+            f"{get_cover_image_url(cover_url, min_size)} {min_size}w",
         ]
     )
 
     sizes = ", ".join(
         [
-            f"({attr} {width}px) {size}px"
-            for (attr, size, width) in (
-                ("min-width", *info["max_size"]),
-                ("max-width", *info["min_size"]),
-            )
+            f"(max-width: 767.99px){min_size}px",
+            f"(min-width: 768px) {full_size}px",
         ]
     )
 
@@ -121,13 +87,14 @@ def get_cover_image_url(cover_url: str | None, size: int) -> str:
             + urllib.parse.urlencode({"url": Signer().sign(cover_url)})
         )
         if cover_url
-        else ""
+        else get_placeholder_url(size)
     )
 
 
-def get_placeholder_url(variant: CoverImageSize) -> str:
+@functools.cache
+def get_placeholder_url(size: int) -> str:
     """Return URL to cover image placeholder"""
-    return static(f"img/placeholder-{_COVER_IMAGE_INFO[variant]["default_size"]}.webp")
+    return static(f"img/placeholder-{size}.webp")
 
 
 @functools.cache
