7e7463d7cf6c77414bcdb98d3f4621b1b3f745e8
Dan Jacob <danjac2018@gmail.com>
2024-06-13 12:18:51 +0300

    Gunicorn tweaks

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -15,10 +15,10 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # total available memory in GB
 memory = math.floor(psutil.virtual_memory().total / (pow(1000, 3)))
 
-# set max_requests to arbitrary value of memory * 100
-# e.g. 4GB = 400 max requests
-max_requests = memory * 100
+# set max_requests to arbitrary value of memory * 50
+# e.g. 4GB = 200 max requests
+max_requests = memory * 50
 
-# set max_requests to 1/20 of max_requests
-# e.g. max requests 800 = (800 / 20) 40 max_requests_jitter
+# set max_requests_jitter to 5% of max_requests
+# e.g. max_requests 200 = (200 / 20) 10 max_requests_jitter
 max_requests_jitter = math.floor(max_requests / 20)
