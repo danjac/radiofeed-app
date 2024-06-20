f26f38df02615d5d1c2eeda557172e529a1c3578
Dan Jacob <danjac2018@gmail.com>
2024-06-20 10:47:11 +0300

    Update to htmx 2.0

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,15 @@
 import Alpine from "alpinejs";
-import htmx from "htmx.org";
+import "htmx.org";
 import "./audio-player";
 
 // global HTMX configuration
 // https://htmx.org/docs/#config
 
-htmx.config.historyCacheSize = 0;
-htmx.config.refreshOnHistoryMiss = false;
-htmx.config.scrollBehavior = "smooth";
-htmx.config.scrollIntoViewOnBoost = false;
-htmx.config.useTemplateFragments = true;
+// htmx.config.historyCacheSize = 0;
+// htmx.config.refreshOnHistoryMiss = false;
+// htmx.config.scrollBehavior = "smooth";
+// htmx.config.scrollIntoViewOnBoost = false;
+// htmx.config.useTemplateFragments = true;
 
 // initialize global Alpine instance
 
