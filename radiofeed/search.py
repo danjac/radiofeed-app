34de16230ccdde85b71bc5b8e1b50601dcf55e67
Dan Jacob <danjac2018@gmail.com>
2024-07-07 23:43:16 +0300

    Move SearchDetails to middleware

--- radiofeed/search.py
+++ radiofeed/search.py
@@ -4,42 +4,8 @@ from typing import ClassVar
 
 from django.contrib.postgres.search import SearchQuery, SearchRank
 from django.db.models import F, Q
-from django.http import HttpRequest, QueryDict
-from django.utils.encoding import force_str
-from django.utils.functional import cached_property
 
 from radiofeed.types import T_QuerySet
-
-
-class SearchDetails:
-    """Handles search parameters in request."""
-
-    param: str = "search"
-
-    def __init__(self, request: HttpRequest) -> None:
-        self.request = request
-
-    def __str__(self) -> str:
-        """Returns search query value."""
-        return self.value
-
-    def __bool__(self) -> bool:
-        """Returns `True` if search in query and has a non-empty value."""
-        return bool(self.value)
-
-    @cached_property
-    def value(self) -> str:
-        """Returns the search query value, if any."""
-        return force_str(self.request.GET.get(self.param, "")).strip()
-
-    @cached_property
-    def qs(self) -> str:
-        """Returns querystring with search."""
-        return (
-            "?" + QueryDict.fromkeys([self.param], value=self.value).urlencode()
-            if self
-            else ""
-        )
 
 
 class SearchQuerySetMixin(T_QuerySet):
