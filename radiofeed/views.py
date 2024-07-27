4d1532656d4249b7139e0abdc0792288508703a4
Dan Jacob <danjac2018@gmail.com>
2024-07-27 20:35:22 +0300

    feat: add all icons

--- radiofeed/views.py
+++ radiofeed/views.py
@@ -1,5 +1,7 @@
 import datetime
+import functools
 import io
+from collections.abc import Iterator
 from typing import Final
 
 import httpx
@@ -20,6 +22,7 @@ from radiofeed.http_client import get_client
 from radiofeed.templatetags import ACCEPT_COOKIES_NAME
 
 _CACHE_TIMEOUT: Final = 60 * 60 * 24 * 350
+
 
 _cache_control = cache_control(max_age=_CACHE_TIMEOUT, immutable=True, public=True)
 _cache_page = cache_page(_CACHE_TIMEOUT)
@@ -106,12 +109,6 @@ def manifest(request: HttpRequest) -> JsonResponse:
     """PWA manifest.json file."""
     start_url = reverse("podcasts:index")
 
-    icon = {
-        "src": static("img/wave-512.png"),
-        "type": "image/png",
-        "sizes": "512x512",
-    }
-
     categories = settings.PWA_CONFIG["manifest"]["categories"]
     description = settings.PWA_CONFIG["manifest"]["description"]
     theme_color = settings.PWA_CONFIG["manifest"]["theme_color"]
@@ -145,11 +142,7 @@ def manifest(request: HttpRequest) -> JsonResponse:
                 static("img/desktop.png"),
                 static("img/mobile.png"),
             ],
-            "icons": [
-                icon,
-                icon | {"purpose": "maskable"},
-                icon | {"purpose": "any"},
-            ],
+            "icons": list(_app_icons()),
             "shortcuts": [],
             "lang": "en",
         }
@@ -241,3 +234,52 @@ def cover_image(request: HttpRequest, size: int) -> FileResponse:
         output = get_placeholder_path(size).open("rb")
 
     return FileResponse(output, content_type="image/webp")
+
+
+def _app_icons() -> Iterator[dict]:
+    for icon in _icons():
+        app_icon = icon | {
+            "src": static(f"img/icons/{icon['src']}"),
+            "type": "image/png",
+        }
+        yield app_icon
+        yield app_icon | {"purpose": "maskable"}
+        yield app_icon | {"purpose": "any"}
+
+
+@functools.cache
+def _icons() -> list[dict]:
+    return [
+        {"src": "android/android-launchericon-512-512.png", "sizes": "512x512"},
+        {"src": "android/android-launchericon-192-192.png", "sizes": "192x192"},
+        {"src": "android/android-launchericon-144-144.png", "sizes": "144x144"},
+        {"src": "android/android-launchericon-96-96.png", "sizes": "96x96"},
+        {"src": "android/android-launchericon-72-72.png", "sizes": "72x72"},
+        {"src": "android/android-launchericon-48-48.png", "sizes": "48x48"},
+        {"src": "ios/16.png", "sizes": "16x16"},
+        {"src": "ios/20.png", "sizes": "20x20"},
+        {"src": "ios/29.png", "sizes": "29x29"},
+        {"src": "ios/32.png", "sizes": "32x32"},
+        {"src": "ios/40.png", "sizes": "40x40"},
+        {"src": "ios/50.png", "sizes": "50x50"},
+        {"src": "ios/57.png", "sizes": "57x57"},
+        {"src": "ios/58.png", "sizes": "58x58"},
+        {"src": "ios/60.png", "sizes": "60x60"},
+        {"src": "ios/64.png", "sizes": "64x64"},
+        {"src": "ios/72.png", "sizes": "72x72"},
+        {"src": "ios/76.png", "sizes": "76x76"},
+        {"src": "ios/80.png", "sizes": "80x80"},
+        {"src": "ios/87.png", "sizes": "87x87"},
+        {"src": "ios/100.png", "sizes": "100x100"},
+        {"src": "ios/114.png", "sizes": "114x114"},
+        {"src": "ios/120.png", "sizes": "120x120"},
+        {"src": "ios/128.png", "sizes": "128x128"},
+        {"src": "ios/144.png", "sizes": "144x144"},
+        {"src": "ios/152.png", "sizes": "152x152"},
+        {"src": "ios/167.png", "sizes": "167x167"},
+        {"src": "ios/180.png", "sizes": "180x180"},
+        {"src": "ios/192.png", "sizes": "192x192"},
+        {"src": "ios/256.png", "sizes": "256x256"},
+        {"src": "ios/512.png", "sizes": "512x512"},
+        {"src": "ios/1024.png", "sizes": "1024x1024"},
+    ]
