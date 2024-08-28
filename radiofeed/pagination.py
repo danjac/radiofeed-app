576108ba68d58bfa232c9d0e0f5d10dff915e67e
Dan Jacob <danjac2018@gmail.com>
2024-08-28 14:07:23 +0300

    chore: tidy up pagination code

--- radiofeed/pagination.py
+++ radiofeed/pagination.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.paginator import Paginator
+from django.core.paginator import Page, Paginator
 from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.template.response import TemplateResponse
@@ -14,15 +14,20 @@ def paginate(
     page_size: int = settings.PAGE_SIZE,
     param: str = "page",
     **pagination_kwargs,
+) -> Page:
+    """Returns paginated object list."""
+    return Paginator(object_list, page_size).get_page(
+        request.GET.get(param, ""),
+        **pagination_kwargs,
+    )
+
+
+def paginate_lazy(
+    request: HttpRequest, object_list: QuerySet, *args, **kwargs
 ) -> SimpleLazyObject:
     """Returns paginated object list. This is resolved lazily, so
     the database query is only evaluated when referenced in template."""
-    return SimpleLazyObject(
-        lambda: Paginator(object_list, page_size).get_page(
-            request.GET.get(param, ""),
-            **pagination_kwargs,
-        )
-    )
+    return SimpleLazyObject(lambda: paginate(request, object_list, *args, **kwargs))
 
 
 def render_pagination_response(
@@ -38,7 +43,9 @@ def render_pagination_response(
         request,
         template_name,
         {
-            "page_obj": paginate(request, object_list, **(pagination_kwargs or {})),
+            "page_obj": paginate_lazy(
+                request, object_list, **(pagination_kwargs or {})
+            ),
         }
         | (extra_context or {}),
         partial="pagination",
