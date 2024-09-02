c105f8203886395d6df27b2afe2a2b8b39a0b4fa
Dan Jacob <danjac2018@gmail.com>
2024-09-02 10:46:57 +0300

    refactor: add cached page

--- radiofeed/pagination.py
+++ radiofeed/pagination.py
@@ -1,11 +1,60 @@
+import dataclasses
+from collections.abc import Iterator
+
 from django.conf import settings
 from django.core.paginator import Page, Paginator
 from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.template.response import TemplateResponse
-from django.utils.functional import SimpleLazyObject
+from django.utils.functional import SimpleLazyObject, cached_property
 
 from radiofeed.template import render_template_partial
+
+
+@dataclasses.dataclass(frozen=True)
+class CachedPage:
+    """Page object wrapper using cached property values."""
+
+    page: Page
+
+    @cached_property
+    def paginator(self) -> Paginator:
+        """Returns Paginator instance."""
+        return self.page.paginator
+
+    @cached_property
+    def object_list(self) -> Paginator:
+        """Returns Paginator instance."""
+        return self.page.object_list
+
+    @cached_property
+    def has_other_pages(self) -> bool:
+        """If has other pages."""
+        return self.page.has_other_pages()
+
+    @cached_property
+    def has_next(self) -> bool:
+        """If has next page."""
+        return self.page.has_next()
+
+    @cached_property
+    def has_previous(self) -> bool:
+        """If has previous page."""
+        return self.page.has_previous()
+
+    @cached_property
+    def next_page_number(self) -> bool:
+        """If has next page."""
+        return self.page.next_page_number()
+
+    @cached_property
+    def previous_page_number(self) -> bool:
+        """If has previous page."""
+        return self.page.previous_page_number()
+
+    def __iter__(self) -> Iterator:
+        """Iterate through current page items."""
+        return iter(self.page)
 
 
 def paginate(
@@ -14,10 +63,12 @@ def paginate(
     page_size: int = settings.PAGE_SIZE,
     param: str = "page",
     **pagination_kwargs,
-) -> Page:
+) -> CachedPage:
     """Returns paginated object list."""
-    return Paginator(object_list, page_size).get_page(
-        request.GET.get(param, ""), **pagination_kwargs
+    return CachedPage(
+        page=Paginator(object_list, page_size).get_page(
+            request.GET.get(param, ""), **pagination_kwargs
+        )
     )
 
 
