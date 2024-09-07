e2be7e006c3d6b44b624cb1bc838686b7f3950dc
Dan Jacob <danjac2018@gmail.com>
2024-09-07 12:55:07 +0300

    refactor: parse_feeds to djclick

--- radiofeed/feedparser/management/commands/parse_feeds.py
+++ radiofeed/feedparser/management/commands/parse_feeds.py
@@ -1,8 +1,7 @@
-import argparse
 import contextlib
 from concurrent.futures import wait
 
-from django.core.management.base import BaseCommand
+import djclick as click
 from django.db.models import Count, F, QuerySet
 
 from radiofeed.feedparser import feed_parser, scheduler
@@ -12,47 +11,35 @@ from radiofeed.podcasts.models import Podcast
 from radiofeed.thread_pool import DatabaseSafeThreadPoolExecutor
 
 
-class Command(BaseCommand):
-    """Command implementation."""
+@click.command()
+@click.option("--limit", type=int, default=360, help="Number of feeds to process")
+def command(limit: int) -> None:
+    "Parses RSS feeds of all scheduled podcasts."
 
-    help = "Parses RSS feeds of all scheduled podcasts."
+    client = get_client()
 
-    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
-        """Add options."""
-        parser.add_argument(
-            "--limit",
-            "-l",
-            type=int,
-            default=360,
-            help="Number of feeds to process",
-        )
-
-    def handle(self, *args, **options) -> None:
-        """Implementation of handle."""
-        limit: int = options["limit"]
-
-        client = get_client()
-
-        with DatabaseSafeThreadPoolExecutor() as executor:
-            wait(
-                executor.db_safe_map(
-                    lambda podcast: self._parse_feed(podcast, client),
-                    self._get_scheduled_podcasts(limit),
-                )
+    with DatabaseSafeThreadPoolExecutor() as executor:
+        wait(
+            executor.db_safe_map(
+                lambda podcast: _parse_feed(podcast, client),
+                _get_scheduled_podcasts(limit),
             )
-
-    def _get_scheduled_podcasts(self, limit: int) -> QuerySet[Podcast]:
-        return (
-            scheduler.get_scheduled_podcasts()
-            .alias(subscribers=Count("subscriptions"))
-            .filter(active=True)
-            .order_by(
-                F("subscribers").desc(),
-                F("promoted").desc(),
-                F("parsed").asc(nulls_first=True),
-            )[:limit]
         )
 
-    def _parse_feed(self, podcast: Podcast, client: Client) -> None:
-        with contextlib.suppress(FeedParserError):
-            feed_parser.parse_feed(podcast, client)
+
+def _get_scheduled_podcasts(limit: int) -> QuerySet[Podcast]:
+    return (
+        scheduler.get_scheduled_podcasts()
+        .alias(subscribers=Count("subscriptions"))
+        .filter(active=True)
+        .order_by(
+            F("subscribers").desc(),
+            F("promoted").desc(),
+            F("parsed").asc(nulls_first=True),
+        )[:limit]
+    )
+
+
+def _parse_feed(podcast: Podcast, client: Client) -> None:
+    with contextlib.suppress(FeedParserError):
+        feed_parser.parse_feed(podcast, client)
