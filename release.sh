25e359cb296e74c749ab31dc5e83c1688bfaf8b0
Dan Jacob <danjac2018@gmail.com>
2024-09-14 14:49:32 +0300

    chore: add --traceback to commands in release.sh

--- release.sh
+++ release.sh
@@ -5,10 +5,12 @@ set -o errexit
 # increase statement timeout for larger migrations
 export STATEMENT_TIMEOUT=300
 
-python ./manage.py check --deploy
+python ./manage.py check --deploy --traceback
 
-python ./manage.py migrate --no-input
+python ./manage.py migrate --check --traceback
 
-python ./manage.py health_check
+python ./manage.py migrate --no-input --traceback
 
-python ./manage.py clear_cache
+python ./manage.py health_check --traceback
+
+python ./manage.py clear_cache --traceback
