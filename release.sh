a906a491cd5e152feb25bdbf12f5f7779bdfd7d3
Dan Jacob <danjac2018@gmail.com>
2024-09-15 11:41:40 +0300

    chore: remove migrate check step from release

--- release.sh
+++ release.sh
@@ -7,8 +7,6 @@ export STATEMENT_TIMEOUT=300
 
 python ./manage.py check --deploy --traceback
 
-python ./manage.py migrate --check --traceback
-
 python ./manage.py migrate --no-input --traceback
 
 python ./manage.py health_check --traceback
