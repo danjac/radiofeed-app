ee6f001518a581633063c49278235e6cef6ad7d1
Dan Jacob <danjac2018@gmail.com>
2024-09-21 10:47:37 +0300

    chore: use tuple instead of ClassVar

--- radiofeed/users/admin.py
+++ radiofeed/users/admin.py
@@ -1,5 +1,3 @@
-from typing import ClassVar
-
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 
@@ -10,12 +8,12 @@ from radiofeed.users.models import User
 class UserAdmin(auth_admin.UserAdmin):
     """User model admin."""
 
-    list_display: ClassVar[list[str]] = [
+    list_display = (
         "username",
         "email",
         "is_superuser",
         "date_joined",
         "last_login",
-    ]
-    search_fields: ClassVar[list[str]] = ["email", "username"]
-    ordering: ClassVar[list[str]] = ["-date_joined", "-last_login"]
+    )
+    search_fields = ("email", "username")
+    ordering = ("-date_joined", "-last_login")
