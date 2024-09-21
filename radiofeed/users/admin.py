111e128977b184fb9f4f206c0b9e08988949a39e
Dan Jacob <danjac2018@gmail.com>
2024-09-21 10:55:07 +0300

    refactor: remove custom user admin

--- radiofeed/users/admin.py
+++ radiofeed/users/admin.py
@@ -1,19 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.admin import UserAdmin
 
 from radiofeed.users.models import User
 
-
-@admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
-    """User model admin."""
-
-    list_display = (
-        "username",
-        "email",
-        "is_superuser",
-        "date_joined",
-        "last_login",
-    )
-    search_fields = ("email", "username")
-    ordering = ("-date_joined", "-last_login")
+admin.site.register(User, UserAdmin)
