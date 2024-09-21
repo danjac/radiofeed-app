8d4f021e1e4223881b25d4d633ba2bd5da87db5d
Dan Jacob <danjac2018@gmail.com>
2024-09-21 10:51:58 +0300

    refactor: remove custom user manager

--- radiofeed/users/models.py
+++ radiofeed/users/models.py
@@ -1,48 +1,8 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-class UserManager(BaseUserManager["User"]):
-    """Custom Manager for User model."""
-
-    def create_user(
-        self,
-        username: str,
-        email: str,
-        password: str | None = None,
-        **kwargs,
-    ) -> "User":
-        """Create new user."""
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            **kwargs,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(
-        self,
-        username: str,
-        email: str,
-        password: str | None = None,
-        **kwargs,
-    ) -> "User":
-        """Create new superuser."""
-        return self.create_user(
-            username,
-            email,
-            password,
-            is_staff=True,
-            is_superuser=True,
-            **kwargs,
-        )
 
 
 class User(AbstractUser):
     """Custom User model."""
 
     send_email_notifications = models.BooleanField(default=True)
-
-    objects: models.Manager["User"] = UserManager()
