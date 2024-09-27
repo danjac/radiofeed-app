29c189dab436e7de906f22bd2c8b652b5409153e
Dan Jacob <danjac2018@gmail.com>
2024-09-27 18:07:09 +0300

    refactor: tidy up settings_dropdown

--- radiofeed/users/templatetags/account_settings.py
+++ radiofeed/users/templatetags/account_settings.py
@@ -13,7 +13,6 @@ register = template.Library()
 class Item:
     """Settings navigation item."""
 
-    name: str
     icon: str
     url: str
     label: str
@@ -22,77 +21,60 @@ class Item:
 @register.inclusion_tag("account/_settings.html", takes_context=True)
 def settings_dropdown(context: RequestContext, current: str) -> dict:
     """Renders account settings dropdown menu."""
-
     items = _get_settings_items(context.request)
 
     try:
-        current_item = {item.name: item for item in items}[current]
+        current_item = items.pop(current)
     except KeyError as exc:
         raise template.TemplateSyntaxError(
             f"{current} is not one of the dropdown items"
         ) from exc
 
-    other_items = [item for item in items if item.name != current]
-
-    return {"current_item": current_item, "items": other_items}
+    return {"current_item": current_item, "items": items.values()}
 
 
-def _get_settings_items(request: HttpRequest) -> list[Item]:
-    items = [
-        Item(
-            name="preferences",
+def _get_settings_items(request: HttpRequest) -> dict[str, Item]:
+    items = {
+        "preferences": Item(
             label="Preferences",
             icon="adjustments-horizontal",
             url=reverse("users:preferences"),
         ),
-        Item(
-            name="stats",
+        "stats": Item(
             label="Statistics",
             icon="chart-bar",
             url=reverse("users:stats"),
         ),
-        Item(
-            name="feeds",
+        "feeds": Item(
             label="Import/Export Feeds",
             icon="rss",
             url=reverse("users:import_podcast_feeds"),
         ),
-        Item(
-            name="email",
+        "email": Item(
             label="Email Addresses",
             icon="envelope",
             url=reverse("account_email"),
         ),
-    ]
+    }
 
     if request.user.has_usable_password:
-        items = [
-            *items,
-            Item(
-                name="password",
-                label="Change Password",
-                icon="key",
-                url=reverse("account_change_password"),
-            ),
-        ]
+        items["password"] = Item(
+            label="Change Password",
+            icon="key",
+            url=reverse("account_change_password"),
+        )
 
     if get_adapter().list_providers(request):
-        items = [
-            *items,
-            Item(
-                name="social_logins",
-                label="Social Logins",
-                icon="user-group",
-                url=reverse("socialaccount_connections"),
-            ),
-        ]
+        items["social_logins"] = Item(
+            label="Social Logins",
+            icon="user-group",
+            url=reverse("socialaccount_connections"),
+        )
 
-    return [
-        *items,
-        Item(
-            name="delete_account",
-            label="Delete Account",
-            icon="trash",
-            url=reverse("users:delete_account"),
-        ),
-    ]
+    items["delete_account"] = Item(
+        label="Delete Account",
+        icon="trash",
+        url=reverse("users:delete_account"),
+    )
+
+    return items
