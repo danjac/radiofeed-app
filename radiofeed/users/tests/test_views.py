8c4d54f74afe76b4a06af756b929616380711bed
Dan Jacob <danjac2018@gmail.com>
2024-10-13 22:13:30 +0300

    test: view test refactoring

--- radiofeed/users/tests/test_views.py
+++ radiofeed/users/tests/test_views.py
@@ -1,14 +1,14 @@
+import http
 import pathlib
 
 import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse, reverse_lazy
-from pytest_django.asserts import assertRedirects
+from pytest_django.asserts import assertTemplateUsed
 
 from radiofeed.episodes.tests.factories import AudioLogFactory, BookmarkFactory
 from radiofeed.podcasts.models import Subscription
 from radiofeed.podcasts.tests.factories import PodcastFactory, SubscriptionFactory
-from radiofeed.tests.asserts import assert_200
 from radiofeed.users.models import User
 
 
@@ -17,7 +17,9 @@ class TestUserPreferences:
 
     @pytest.mark.django_db
     def test_get(self, client, auth_user):
-        assert_200(client.get(self.url, headers={"HX-Request": "true"}))
+        response = client.get(self.url, headers={"HX-Request": "true"})
+        assert response.status_code == http.HTTPStatus.OK
+        assertTemplateUsed(response, "account/preferences.html")
 
     @pytest.mark.django_db
     def test_post(self, client, auth_user):
@@ -35,15 +37,13 @@ class TestUserPreferences:
 
     @pytest.mark.django_db
     def test_post_not_htmx(self, client, auth_user):
-        assertRedirects(
-            client.post(
-                self.url,
-                {
-                    "send_email_notifications": False,
-                },
-            ),
+        response = client.post(
             self.url,
+            {
+                "send_email_notifications": False,
+            },
         )
+        assert response.url == self.url
 
         auth_user.refresh_from_db()
 
@@ -51,20 +51,32 @@ class TestUserPreferences:
 
 
 class TestUserStats:
+    url = reverse_lazy("users:stats")
+    template = "account/stats.html"
+
     @pytest.mark.django_db
     def test_stats(self, client, auth_user):
         SubscriptionFactory(subscriber=auth_user)
         AudioLogFactory(user=auth_user)
         BookmarkFactory(user=auth_user)
 
-        assert_200(client.get(reverse("users:stats")))
+        response = client.get(self.url)
+
+        assert response.status_code == http.HTTPStatus.OK
+
+        assertTemplateUsed(response, self.template)
 
     @pytest.mark.django_db
     def test_stats_plural(self, client, auth_user):
         AudioLogFactory.create_batch(3, user=auth_user)
         BookmarkFactory.create_batch(3, user=auth_user)
         SubscriptionFactory.create_batch(3, subscriber=auth_user)
-        assert_200(client.get(reverse("users:stats")))
+
+        response = client.get(self.url)
+
+        assert response.status_code == http.HTTPStatus.OK
+
+        assertTemplateUsed(response, self.template)
 
 
 class TestImportPodcastFeeds:
@@ -81,7 +93,9 @@ class TestImportPodcastFeeds:
 
     @pytest.mark.django_db
     def test_get(self, client, auth_user):
-        assert_200(client.get(self.url))
+        response = client.get(self.url)
+        assertTemplateUsed(response, "account/podcast_feeds.html")
+        assert response.status_code == http.HTTPStatus.OK
 
     @pytest.mark.django_db
     def test_post_has_new_feeds(self, client, auth_user, upload_file):
@@ -100,31 +114,29 @@ class TestImportPodcastFeeds:
 
     @pytest.mark.django_db
     def test_post_invalid_form(self, client, auth_user):
-        assert_200(
-            client.post(
-                self.url,
-                data={"opml": "test.xml"},
-                headers={
-                    "HX-Request": "true",
-                    "HX-Target": "import-feeds-form",
-                },
-            )
+        response = client.post(
+            self.url,
+            data={"opml": "test.xml"},
+            headers={
+                "HX-Request": "true",
+                "HX-Target": "import-feeds-form",
+            },
         )
 
+        assert response.status_code == http.HTTPStatus.OK
         assert not Subscription.objects.filter(subscriber=auth_user).exists()
 
     @pytest.mark.django_db
     def test_post_podcast_not_in_db(self, client, auth_user, upload_file):
-        assert_200(
-            client.post(
-                self.url,
-                data={"opml": upload_file},
-                headers={
-                    "HX-Request": "true",
-                    "HX-Target": "import-feeds-form",
-                },
-            )
+        response = client.post(
+            self.url,
+            data={"opml": upload_file},
+            headers={
+                "HX-Request": "true",
+                "HX-Target": "import-feeds-form",
+            },
         )
+        assert response.status_code == http.HTTPStatus.OK
 
         assert Subscription.objects.filter(subscriber=auth_user).count() == 0
 
@@ -137,31 +149,29 @@ class TestImportPodcastFeeds:
             subscriber=auth_user,
         )
 
-        assert_200(
-            client.post(
-                self.url,
-                data={"opml": upload_file},
-                headers={
-                    "HX-Request": "true",
-                    "HX-Target": "import-feeds-form",
-                },
-            )
+        response = client.post(
+            self.url,
+            data={"opml": upload_file},
+            headers={
+                "HX-Request": "true",
+                "HX-Target": "import-feeds-form",
+            },
         )
 
+        assert response.status_code == http.HTTPStatus.OK
         assert Subscription.objects.filter(subscriber=auth_user).exists()
 
     @pytest.mark.django_db
     def test_post_is_empty(self, client, auth_user, upload_file):
-        assert_200(
-            client.post(
-                self.url,
-                data={"opml": upload_file},
-                headers={
-                    "HX-Request": "true",
-                    "HX-Target": "import-feeds-form",
-                },
-            )
+        response = client.post(
+            self.url,
+            data={"opml": upload_file},
+            headers={
+                "HX-Request": "true",
+                "HX-Target": "import-feeds-form",
+            },
         )
+        assert response.status_code == http.HTTPStatus.OK
         assert not Subscription.objects.filter(subscriber=auth_user).exists()
 
 
@@ -179,21 +189,27 @@ class TestDeleteAccount:
 
     @pytest.mark.django_db
     def test_get_not_logged_in(self, client):
-        assert_200(client.get(self.url))
+        response = client.get(self.url)
+        assert response.status_code == http.HTTPStatus.OK
+        assertTemplateUsed(response, "account/delete_account.html")
 
     @pytest.mark.django_db
     def test_post_not_logged_in(self, client):
-        assert_200(client.post(self.url, {"confirm-delete": True}))
+        response = client.post(self.url, {"confirm-delete": True})
+        assert response.status_code == http.HTTPStatus.OK
 
     @pytest.mark.django_db
     def test_get(self, client, auth_user):
         # make sure we don't accidentally delete account on get request
-        assert_200(client.get(self.url))
+        response = client.get(self.url)
+        assert response.status_code == http.HTTPStatus.OK
+        assertTemplateUsed(response, "account/delete_account.html")
         assert User.objects.exists()
 
     @pytest.mark.django_db
     def test_post_unconfirmed(self, client, auth_user):
-        assert_200(client.get(self.url))
+        response = client.get(self.url)
+        assert response.status_code == http.HTTPStatus.OK
         assert User.objects.exists()
 
     @pytest.mark.django_db
