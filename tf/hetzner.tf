90b70a1b8676f6734256ed44a661e3ee0d0189be
Dan Jacob <danjac2018@gmail.com>
2024-10-15 12:07:32 +0300

    chore: terraform - add lb to subnet

--- tf/hetzner.tf
+++ tf/hetzner.tf
@@ -177,6 +177,19 @@ resource "hcloud_load_balancer" "lb" {
   network_zone       = var.network_zone
 }
 
+resource "hcloud_load_balancer_network" "lb_srvnetwork" {
+  load_balancer_id = hcloud_load_balancer.lb.id
+  network_id       = hcloud_network.network.id
+
+  # **Note**: the depends_on is important when directly attaching the
+  # server to a network. Otherwise Terraform will attempt to create
+  # server and sub-network in parallel. This may result in the server
+  # creation failing randomly.
+  depends_on = [
+    hcloud_network_subnet.subnet
+  ]
+}
+
 # Attach app servers to load balancer
 resource "hcloud_load_balancer_target" "load_balancer_target" {
   count            = length(var.app_servers)
