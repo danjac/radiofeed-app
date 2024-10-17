541a657640060dba552b9dcb5300e7aa78a39aae
Dan Jacob <danjac2018@gmail.com>
2024-10-17 10:41:58 +0300

    refactor: move partials into components

--- radiofeed/middleware.py
+++ radiofeed/middleware.py
@@ -59,7 +59,7 @@ class HtmxMessagesMiddleware(BaseMiddleware):
         if get_messages(request):
             response.write(
                 render_to_string(
-                    "partials.html#messages",
+                    "components/messages.html",
                     request=request,
                 )
             )
