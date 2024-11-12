5a4a03012ca792a0bdac02a7d59d58311ffe3a83
Dan Jacob <danjac2018@gmail.com>
2024-11-12 22:27:30 +0200

    feat: discover page includes recommendations

--- radiofeed/podcasts/tests/test_models.py
+++ radiofeed/podcasts/tests/test_models.py
@@ -215,6 +215,19 @@ class TestPodcastManager:
 
         assert Podcast.objects.scheduled().exists() is exists
 
+    @pytest.mark.django_db
+    def test_recommended(self, user):
+        podcast = SubscriptionFactory(subscriber=user).podcast
+        RecommendationFactory.create_batch(3, podcast=podcast)
+        PodcastFactory(promoted=True)
+        assert Podcast.objects.recommended(user).count() == 4
+
+    @pytest.mark.django_db
+    def test_recommended_is_subscribed(self, user):
+        podcast = SubscriptionFactory(subscriber=user).podcast
+        RecommendationFactory(recommended=podcast)
+        assert Podcast.objects.recommended(user).count() == 0
+
 
 class TestPodcastModel:
     def test_str(self):
