ef3dbc46c5c224f13cd85829336b39df0b8e6ea1
Dan Jacob <danjac2018@gmail.com>
2024-11-13 12:01:37 +0200

    fea: order recommended podcasts by relevance

--- radiofeed/podcasts/views.py
+++ radiofeed/podcasts/views.py
@@ -50,8 +50,8 @@ def discover(request: HttpRequest) -> HttpResponse:
         _get_podcasts()
         .recommended(request.user)
         .order_by(
-            "-pub_date",
             "-relevance",
+            "-pub_date",
         )
     )[: settings.DEFAULT_PAGE_SIZE]
 
