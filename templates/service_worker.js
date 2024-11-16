bc4347541d17257713347d1258a9bca46e8c7526
Dan Jacob <danjac2018@gmail.com>
2024-11-16 13:21:31 +0200

    refactor: service worker tidyup

--- templates/service_worker.js
+++ templates/service_worker.js
@@ -5,19 +5,19 @@ importScripts(
     "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
 );
 
-const CACHE = "pwabuilder-page";
+const CACHE = "serviceworker-{{ request.site.domain }}";
 
 const offlineFallbackPage = "{% static 'offline.html' %}";
 
-self.addEventListener("message", event => {
+self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
         self.skipWaiting();
     }
 });
 
-self.addEventListener("install", async event => {
+self.addEventListener("install", async (event) => {
     event.waitUntil(
-        caches.open(CACHE).then(cache => cache.add(offlineFallbackPage)),
+        caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)),
     );
 });
 
@@ -25,23 +25,19 @@ if (workbox.navigationPreload.isSupported()) {
     workbox.navigationPreload.enable();
 }
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
     if (event.request.mode === "navigate") {
         event.respondWith(
             (async () => {
                 try {
                     const preloadResp = await event.preloadResponse;
-
                     if (preloadResp) {
                         return preloadResp;
                     }
-
-                    const networkResp = await fetch(event.request);
-                    return networkResp;
+                    return await fetch(event.request);
                 } catch {
                     const cache = await caches.open(CACHE);
-                    const cachedResp = await cache.match(offlineFallbackPage);
-                    return cachedResp;
+                    return await cache.match(offlineFallbackPage);
                 }
             })(),
         );
