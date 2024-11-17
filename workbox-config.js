416dd09ae2aab0c8f55a60d7dc08c04141217b33
Dan Jacob <danjac2018@gmail.com>
2024-11-17 21:42:39 +0200

    refactor: include all assets in sw cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    globDirectory: "static/",
-    globPatterns: ["**/*.{css,js,png,webp,html}"],
-    swDest: "static/service-worker.js",
-    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-    runtimeCaching: [
-        {
-            urlPattern: /\/static\/.*/,
-            handler: "CacheFirst",
-        },
+    globDirectory: 'static/',
+    globPatterns: [
+        '**/*.{css,js,png,webp,xcf,html}'
     ],
+    swDest: 'static/sw.js',
+    ignoreURLParametersMatching: [
+        /^utm_/,
+        /^fbclid$/
+    ]
 };
