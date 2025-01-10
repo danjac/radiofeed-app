4316709e39ff6527f84937f690a72bf7fa2054e1
Dan Jacob <danjac2018@gmail.com>
2025-01-10 11:58:53 +0200

    chore: merge fixes

--- radiofeed/tests/asserts.py
+++ radiofeed/tests/asserts.py
@@ -6,9 +6,9 @@ from django.http import HttpResponse
 
 def assert_status(response: HttpResponse, status: http.HTTPStatus) -> None:
     """Assert that the response has the expected status code."""
-    assert (
-        response.status_code == status
-    ), f"Expected status {status}, but got {response.status_code}"
+    assert response.status_code == status, (
+        f"Expected status {status}, but got {response.status_code}"
+    )
 
 
 assert200 = functools.partial(assert_status, status=http.HTTPStatus.OK)
