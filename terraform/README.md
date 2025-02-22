d2610fb73008e128289d6a5ad484e02e5231547c
Dan Jacob <danjac2018@gmail.com>
2025-02-22 19:40:16 +0200

    feat: add Hetzner terraform setup

---
+++ terraform/README.md
@@ -0,0 +1,42 @@
+# Provisioning a cluster
+
+The file `hetzner.sh` is a script that provisions a cluster of Hetzner Cloud servers. It uses the Hetzner Cloud API to create the servers and then installs the necessary software on them.
+
+## Requirements
+
+- A Hetzner Cloud account
+- An API token for the Hetzner Cloud API
+- Cloudflare origin key certificates (private and public keys)
+- Terraform installed on your machine
+
+## Usage
+
+1. Set the following environment variables
+
+```bash
+export TF_VAR_hcloud_token="your-hcloud-token"
+export TF_VAR_cloudflare_origin_cert_path="/path/to/cert.pem"
+export TF_VAR_cloudflare_origin_key_path="/path/to/key.pem"
+```
+
+2. Initialize Terraform
+
+```bash
+terraform init
+```
+
+3. Preview the changes
+
+```bash
+terraform plan
+```
+
+4. Apply the changes
+
+```bash
+terraform apply -auto-approve
+```
+
+Once you have applied the changes, you will see the IP addresses of the servers that have been created including private IP addresses.
+
+You can now go to the `ansible` directory and run the following command to install the necessary software on the servers. See `ansible/README.md` for more information.
---
+++ terraform/hetzner.tf
@@ -0,0 +1,127 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.43"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+variable "hcloud_token" {}
+variable "ssh_public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "cloudflare_origin_cert_path" {}
+variable "cloudflare_origin_key_path" {}
+
+resource "hcloud_ssh_key" "default" {
+  name       = "my-ssh-key"
+  public_key = file(var.ssh_public_key_path)
+}
+
+resource "hcloud_network" "private_net" {
+  name     = "private-network"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "private_subnet" {
+  network_id   = hcloud_network.private_net.id
+  type         = "cloud"
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+
+resource "hcloud_firewall" "firewall" {
+  name = "vm-firewall"
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "22"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "hcloud_load_balancer" "lb" {
+  name               = "load-balancer"
+  load_balancer_type = "lb11"
+  location           = "hel1"
+
+  ssl_certificate {
+    certificate = file(var.cloudflare_origin_cert_path)
+    private_key = file(var.cloudflare_origin_key_path)
+  }
+}
+
+resource "hcloud_load_balancer_target" "lb_target_agents" {
+  load_balancer_id = hcloud_load_balancer.lb.id
+  type             = "label_selector"
+  label_selector   = "role=agent"
+}
+
+resource "hcloud_load_balancer_service" "lb_http" {
+  load_balancer_id = hcloud_load_balancer.lb.id
+  protocol         = "http"
+  listen_port      = 8000
+  destination_port = 8000
+
+  health_check {
+    protocol = "http"
+    port     = 8000
+    url_path = "/ht/"
+    interval = 15
+    timeout  = 10
+    retries  = 3
+  }
+}
+
+resource "hcloud_server" "database" {
+  name        = "database"
+  hostname    = "database"
+  image       = "ubuntu-24.04"
+  server_type = "cpx11"
+  location    = "hel1"
+  ssh_keys    = [hcloud_ssh_key.default.id]
+  labels      = { role = "database" }
+  firewall_ids = [hcloud_firewall.firewall.id]
+  backups     = true
+
+  network {
+    network_id = hcloud_network.private_net.id
+  }
+}
+
+resource "hcloud_server" "server" {
+  name        = "server"
+  hostname    = "server"
+  image       = "ubuntu-24.04"
+  server_type = "cpx11"
+  location    = "hel1"
+  ssh_keys    = [hcloud_ssh_key.default.id]
+  labels      = { role = "server" }
+  firewall_ids = [hcloud_firewall.firewall.id]
+
+  network {
+    network_id = hcloud_network.private_net.id
+  }
+}
+
+resource "hcloud_server" "agents" {
+  count       = 2
+  name        = "agent-${count.index + 1}"
+  hostname    = "agent-${count.index + 1}"
+  image       = "ubuntu-24.04"
+  server_type = "cpx11"
+  location    = "hel1"
+  ssh_keys    = [hcloud_ssh_key.default.id]
+  labels      = { role = "agent" }
+  firewall_ids = [hcloud_firewall.firewall.id]
+
+  network {
+    network_id = hcloud_network.private_net.id
+  }
+}
