f9b1284860fee6e60e0cfe3f8d0971d8a3757b8c
Dan Jacob <danjac2018@gmail.com>
2025-03-01 22:37:38 +0200

    refactor: tidy up commands

--- radiofeed/podcasts/management/commands/send_recommendations.py
+++ radiofeed/podcasts/management/commands/send_recommendations.py
@@ -1,6 +1,5 @@
 from allauth.account.models import EmailAddress
 from django.core.management.base import BaseCommand
-from django.db.models import QuerySet
 
 from radiofeed.podcasts import emails
 from radiofeed.thread_pool import execute_thread_pool
@@ -11,19 +10,14 @@ class Command(BaseCommand):
 
     help = "Send recommendation emails to users."
 
-    def handle(self, *args, **options):
+    def handle(self, **options):
         """Handle implementation."""
         execute_thread_pool(
             emails.send_recommendations_email,
-            self._get_recipients(),
-        )
-
-    def _get_recipients(self) -> QuerySet[EmailAddress]:
-        # Return only active users with email notifications enabled
-
-        return EmailAddress.objects.filter(
-            user__is_active=True,
-            user__send_email_notifications=True,
-            primary=True,
-            verified=True,
+            EmailAddress.objects.filter(
+                user__is_active=True,
+                user__send_email_notifications=True,
+                primary=True,
+                verified=True,
+            ),
         )
