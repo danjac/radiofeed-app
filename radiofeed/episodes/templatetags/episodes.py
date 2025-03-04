135c7258cc8a507d69d9ff9a1ac7d58e4d232637
Dan Jacob <danjac2018@gmail.com>
2025-03-04 23:47:35 +0200

    refactor: remove audio player js tag

--- radiofeed/episodes/templatetags/episodes.py
+++ radiofeed/episodes/templatetags/episodes.py
@@ -1,20 +1,10 @@
 from django import template
 from django.template.context import RequestContext
-from django.templatetags.static import static
-from django.utils.html import format_html
 
 from radiofeed.cover_image import get_metadata_info
 from radiofeed.episodes.models import Episode
 
 register = template.Library()
-
-
-@register.simple_tag(takes_context=True)
-def audio_player_script(context: RequestContext) -> str:
-    """Renders the JS required for audio player."""
-    if context.request.user.is_authenticated:
-        return format_html('<script src="{}"></script>', static("audio-player.js"))
-    return ""
 
 
 @register.simple_tag(takes_context=True)
