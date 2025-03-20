69d77e8e186487b67901f50028d2db6be33b0e34
Dan Jacob <danjac2018@gmail.com>
2025-03-20 13:00:49 +0200

    refactor: tidy up send_recommendations

--- radiofeed/podcasts/management/commands/send_recommendations.py
+++ radiofeed/podcasts/management/commands/send_recommendations.py
@@ -1,5 +1,6 @@
 import djclick as click
 from allauth.account.models import EmailAddress
+from django.db.models import QuerySet
 
 from radiofeed.podcasts import emails
 from radiofeed.thread_pool import execute_thread_pool
@@ -16,13 +17,18 @@ from radiofeed.thread_pool import execute_thread_pool
 def command(addresses: list[str]) -> None:
     """Send recommendation emails to users."""
 
+    execute_thread_pool(
+        emails.send_recommendations_email,
+        _get_recipients(addresses),
+    )
+
+
+def _get_recipients(addresses: list[str]) -> QuerySet[EmailAddress]:
     qs = EmailAddress.objects.filter(
         user__is_active=True,
         user__send_email_notifications=True,
-    )
-    if addresses:
-        recipients = qs.filter(email__in=addresses)
-    else:
-        recipients = qs.filter(primary=True, verified=True)
+    ).select_related("user")
 
-    execute_thread_pool(emails.send_recommendations_email, recipients)
+    if addresses:
+        return qs.filter(email__in=addresses)
+    return qs.filter(primary=True, verified=True)
