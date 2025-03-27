e77903f8d44e51ed2467a93c9e9df838c923faab
Dan Jacob <danjac2018@gmail.com>
2025-03-27 13:07:03 +0200

    refactor: use healthcheck subsets

--- release.sh
+++ release.sh
@@ -4,9 +4,11 @@ set -o errexit
 
 # Runs Django management commands to prepare the application for release:
 # - checks the deployment settings
+# - runs health checks
 # - applies database migrations
 
 MANAGE="python ./manage.py"
 
 $MANAGE check --deploy --traceback
+$MANAGE health_check --traceback
 $MANAGE migrate --no-input --traceback
