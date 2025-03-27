4c073da86077f13483e2231b3000dbc2c6b6ea26
Dan Jacob <danjac2018@gmail.com>
2025-03-27 18:30:07 +0200

    feat: set custom Gunicorn access log format

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -8,6 +8,8 @@ wsgi_app = "config.wsgi"
 
 accesslog = "-"
 
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)s'
+
 worker_class = "gthread"
 
 # Calculate the number of workers (CPU + 1)
