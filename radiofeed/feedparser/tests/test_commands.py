b895bf3f8339fc0e5f33c8ae5fe01e59903369da
Dan Jacob <danjac2018@gmail.com>
2025-04-10 20:00:10 +0300

    chore: remove management commands

--- radiofeed/feedparser/tests/test_commands.py
+++ radiofeed/feedparser/tests/test_commands.py
@@ -3,9 +3,7 @@ import pathlib
 import pytest
 from django.core.management import call_command
 
-from radiofeed.feedparser.exceptions import DuplicateError
 from radiofeed.podcasts.models import Podcast
-from radiofeed.podcasts.tests.factories import PodcastFactory
 
 
 class TestImportOpml:
@@ -36,36 +34,3 @@ class TestExportFeeds:
     @pytest.mark.django_db
     def test_ok(self, podcast):
         call_command("export_opml", "-")
-
-
-class TestParseFeeds:
-    @pytest.fixture
-    def mock_parse_ok(self, mocker):
-        return mocker.patch(
-            "radiofeed.feedparser.feed_parser.parse_feed",
-        )
-
-    @pytest.fixture
-    def mock_parse_fail(self, mocker):
-        return mocker.patch(
-            "radiofeed.feedparser.feed_parser.parse_feed",
-            side_effect=DuplicateError(),
-        )
-
-    @pytest.mark.django_db()(transaction=True)
-    def test_ok(self, mock_parse_ok):
-        PodcastFactory(pub_date=None)
-        call_command("parse_feeds")
-        mock_parse_ok.assert_called()
-
-    @pytest.mark.django_db()(transaction=True)
-    def test_not_scheduled(self, mock_parse_ok):
-        PodcastFactory(active=False)
-        call_command("parse_feeds")
-        mock_parse_ok.assert_not_called()
-
-    @pytest.mark.django_db()(transaction=True)
-    def test_feed_parser_error(self, mock_parse_fail):
-        PodcastFactory(pub_date=None)
-        call_command("parse_feeds")
-        mock_parse_fail.assert_called()
