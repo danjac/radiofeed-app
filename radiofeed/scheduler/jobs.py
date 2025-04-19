e923f1a07d7e3773c7da7681424cb9b3853beb54
Dan Jacob <danjac2018@gmail.com>
2025-04-19 16:39:18 +0300

    chore: move clear_sessions job to cron

--- radiofeed/scheduler/jobs.py
+++ radiofeed/scheduler/jobs.py
@@ -5,7 +5,12 @@ from radiofeed.scheduler import scheduler
 
 # non-app specific jobs
 #
-@scheduler.scheduled_job("interval", id="clear_sessions", days=1)
+@scheduler.scheduled_job(
+    "cron",
+    id="clear_sessions",
+    hour=2,
+    minute=15,
+)
 def clear_sessions():
     """Clear stale Django sessions."""
     call_command("clearsessions")
