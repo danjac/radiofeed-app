788b516c23037c4d00f9868f7ad8093fadcafa8b
Dan Jacob <danjac2018@gmail.com>
2025-04-20 12:38:02 +0300

    refactor: configure scheduler in command

--- radiofeed/scheduler/__init__.py
+++ radiofeed/scheduler/__init__.py
@@ -1,10 +1,3 @@
-from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.schedulers.blocking import BlockingScheduler
-from django.conf import settings
 
-scheduler = BlockingScheduler(
-    executors={
-        "default": ThreadPoolExecutor(settings.SCHEDULER_NUM_THREADS),
-    },
-    timezone=settings.TIME_ZONE,
-)
+scheduler = BlockingScheduler()
--- radiofeed/scheduler/management/commands/scheduler.py
+++ radiofeed/scheduler/management/commands/scheduler.py
@@ -1,5 +1,7 @@
-import contextlib
+import argparse
 
+from apscheduler.executors.pool import ThreadPoolExecutor
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.module_loading import autodiscover_modules
 from django_apscheduler.jobstores import DjangoJobStore
@@ -12,19 +14,36 @@ class Command(BaseCommand):
 
     help = "Runs scheduler for all jobs."
 
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        """Add arguments to the command parser."""
+        parser.add_argument(
+            "-t",
+            "--threads",
+            dest="num_threads",
+            type=int,
+            help="Number of threads to use for the scheduler.",
+            default=10,
+        )
+
     def handle(self, **options) -> None:
         """Runs scheduler.
 
         Should search all `INSTALLED_APPS` for a `jobs` module in each job.
         """
 
+        # configure scheduler
+        scheduler.configure(
+            executors={
+                "default": ThreadPoolExecutor(options["num_threads"]),
+            },
+            job_stores={
+                "default": DjangoJobStore(),
+            },
+            timezone=settings.TIME_ZONE,
+        )
+
+        # load all jobs across installed apps
         autodiscover_modules("jobs")
-
-        # remove default jobstore if it exists
-        with contextlib.suppress(KeyError):
-            scheduler.remove_jobstore("default")
-
-        scheduler.add_jobstore(DjangoJobStore(), "default")
 
         try:
             scheduler.start()
