59e1ab6cc34eac36ade36f9b6852ef1135d1bb06
Dan Jacob <danjac2018@gmail.com>
2025-05-09 15:28:37 +0300

    refactor: episodes

--- radiofeed/episodes/templatetags/audio_player.py
+++ radiofeed/episodes/templatetags/audio_player.py
@@ -1,14 +1,53 @@
+import contextlib
+from typing import Literal
+
 from django import template
+from django.http import HttpRequest
 from django.template.context import RequestContext
 
 from radiofeed import cover_image
-from radiofeed.episodes.models import Episode
+from radiofeed.episodes.models import AudioLog, Episode
 
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def get_media_metadata(context: RequestContext, episode: Episode) -> dict:
+@register.inclusion_tag("episodes/audio_player.html", takes_context=True)
+def audio_player(
+    context: RequestContext,
+    audio_log: AudioLog | None = None,
+    action: Literal["load", "play", "close"] = "load",
+) -> dict:
+    """Renders the audio player."""
+
+    context_dct = {
+        "request": context.request,
+        "action": action,
+    }
+
+    if (
+        context.request.user.is_authenticated
+        and action in ("load", "play")
+        and (audio_log := audio_log or _get_audio_log(context.request))
+    ):
+        return context_dct | {
+            "audio_log": audio_log,
+            "metadata": _get_media_metadata(context.request, audio_log.episode),
+        }
+
+    return context_dct
+
+
+def _get_audio_log(request: HttpRequest) -> AudioLog | None:
+    if request.user.is_authenticated and (episode_id := request.player.get()):
+        with contextlib.suppress(AudioLog.DoesNotExist):
+            return request.user.audio_logs.select_related(
+                "episode",
+                "episode__podcast",
+            ).get(episode_id=episode_id)
+    return None
+
+
+def _get_media_metadata(request: HttpRequest, episode: Episode) -> dict:
     """Returns media session metadata for integration with client device.
 
     For more details:
@@ -23,6 +62,7 @@ def get_media_metadata(context: RequestContext, episode: Episode) -> dict:
         "album": episode.podcast.cleaned_title,
         "artist": episode.podcast.cleaned_title,
         "artwork": cover_image.get_metadata_info(
-            context.request, episode.get_cover_url()
+            request,
+            episode.get_cover_url(),
         ),
     }
