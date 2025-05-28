a2398d5159b44fa4c1ae1eedaaf63ef1a4914a5a
Dan Jacob <danjac2018@gmail.com>
2025-05-28 17:25:29 +0300

    refactor: type fixes

--- radiofeed/podcasts/management/commands/create_recommendations.py
+++ radiofeed/podcasts/management/commands/create_recommendations.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.db.models import QuerySet
+from django.db.models import ValuesQuerySet
 from django.db.models.functions import Lower
 
 from radiofeed import tokenizer
@@ -25,14 +25,11 @@ class Command(BaseCommand):
             ),
         )
 
-    def _get_languages(self) -> QuerySet:
+    def _get_languages(self) -> ValuesQuerySet:
         return (
             Podcast.objects.annotate(language_code=Lower("language"))
             .filter(language_code__in=tokenizer.get_language_codes())
-            .values_list(
-                "language_code",
-                flat=True,
-            )
+            .values_list("language_code", flat=True)
             .order_by("language_code")
             .distinct()
         )
