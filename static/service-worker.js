8633238562081eb8b675dc1338612d325181e555
Dan Jacob <danjac2018@gmail.com>
2025-05-31 12:44:31 +0300

    fix: service worker fallback

--- static/service-worker.js
+++ static/service-worker.js
@@ -5,3 +5,24 @@ importScripts(
 if (workbox.navigationPreload.isSupported()) {
     workbox.navigationPreload.enable();
 }
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResponse = await event.preloadResponse;
+                    if (preloadResponse) {
+                        return preloadResponse;
+                    }
+                    return await fetch(event.request);
+                } catch (error) {
+                    return new Response("Offline", {
+                        status: 503,
+                        statusText: "Service Unavailable",
+                    });
+                }
+            })(),
+        );
+    }
+});
