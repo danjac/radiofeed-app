da935f24d8d91584e870de5d722f13a0b91332c5
Dan Jacob <danjac2018@gmail.com>
2025-10-02 12:50:29 +0300

    refactor: cache csrf header name

--- radiofeed/context_processors.py
+++ radiofeed/context_processors.py
@@ -1,18 +1,19 @@
+import functools
+
 from django.conf import settings
 from django.http import HttpHeaders
 
 
 def cache_timeout(_) -> dict[str, str]:
     """Returns default cache timeout"""
-    return {
-        "cache_timeout": settings.DEFAULT_CACHE_TIMEOUT,
-    }
+    return {"cache_timeout": settings.DEFAULT_CACHE_TIMEOUT}
 
 
 def csrf_header(_) -> dict[str, str | None]:
     """Returns CSRF header name"""
-    return {
-        "csrf_header": HttpHeaders.parse_header_name(
-            settings.CSRF_HEADER_NAME,
-        ),
-    }
+    return {"csrf_header": _csrf_header_name()}
+
+
+@functools.cache
+def _csrf_header_name() -> str | None:
+    return HttpHeaders.parse_header_name(settings.CSRF_HEADER_NAME)
