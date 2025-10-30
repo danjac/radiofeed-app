465c20ccb8edae4a1eb14a5c0eede541008da36f
Dan Jacob <danjac2018@gmail.com>
2025-10-30 12:27:41 +0200

    refactor: move config under top level package

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ import sys
 
 def main() -> None:
     """Django management entry point."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiofeed.config.settings")
     try:
         from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
