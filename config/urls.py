7dc79eaf3c725ce142ae86c41368e45f2d5a37d5
Dan Jacob <danjac2018@gmail.com>
2025-12-02 14:01:35 +0200

    refactor: move root urls under top-level package

--- config/urls.py
+++ config/urls.py
@@ -2,26 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from listenwave import views
-
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", include("listenwave.urls")),
     path("", include("listenwave.episodes.urls")),
     path("", include("listenwave.podcasts.urls")),
     path("", include("listenwave.users.urls")),
     path("account/", include("allauth.urls")),
-    path("about/", views.about, name="about"),
-    path("privacy/", views.privacy, name="privacy"),
-    path("accept-cookies/", views.accept_cookies, name="accept_cookies"),
-    path(
-        "covers/<int:size>/<str:encoded_url>.webp",
-        views.cover_image,
-        name="cover_image",
-    ),
-    path("robots.txt", views.robots, name="robots"),
-    path("manifest.json", views.manifest, name="manifest"),
-    path(".well-known/assetlinks.json", views.assetlinks, name="assetlinks"),
-    path(".well-known/security.txt", views.security, name="security"),
     path("ht/", include("health_check.urls")),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
---
+++ listenwave/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from listenwave import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("accept-cookies/", views.accept_cookies, name="accept_cookies"),
+    path(
+        "covers/<int:size>/<str:encoded_url>.webp",
+        views.cover_image,
+        name="cover_image",
+    ),
+    path("robots.txt", views.robots, name="robots"),
+    path("manifest.json", views.manifest, name="manifest"),
+    path(".well-known/assetlinks.json", views.assetlinks, name="assetlinks"),
+    path(".well-known/security.txt", views.security, name="security"),
+]
