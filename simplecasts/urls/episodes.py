8a76f2de0f40e0882596ce99750df5b6c945794f
Dan Jacob <danjac2018@gmail.com>
2026-01-07 12:38:30 +0200

    fix: test fixes

--- simplecasts/urls/episodes.py
+++ simplecasts/urls/episodes.py
@@ -14,9 +14,21 @@ urlpatterns = [
     ),
     path("search/episodes/", episodes.search_episodes, name="search_episodes"),
     path("history/", episodes.history, name="history"),
-    path("history/<int:episode_id>/complete/", episodes.mark_complete, name="mark_complete"),
-    path("history/<int:episode_id>/remove/", episodes.remove_audio_log, name="remove_audio_log"),
+    path(
+        "history/<int:episode_id>/complete/",
+        episodes.mark_complete,
+        name="mark_complete",
+    ),
+    path(
+        "history/<int:episode_id>/remove/",
+        episodes.remove_audio_log,
+        name="remove_audio_log",
+    ),
     path("bookmarks/", episodes.bookmarks, name="bookmarks"),
     path("bookmarks/<int:episode_id>/add/", episodes.add_bookmark, name="add_bookmark"),
-    path("bookmarks/<int:episode_id>/remove/", episodes.remove_bookmark, name="remove_bookmark"),
+    path(
+        "bookmarks/<int:episode_id>/remove/",
+        episodes.remove_bookmark,
+        name="remove_bookmark",
+    ),
 ]
