316fdcf0882d650f53df9ca5a12dcf55cdd8edb8
Dan Jacob <danjac2018@gmail.com>
2026-02-01 16:06:38 +0200

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation only changes
+        'style',    // Code style (formatting, semicolons, etc)
+        'refactor', // Code refactoring
+        'perf',     // Performance improvements
+        'test',     // Adding or updating tests
+        'build',    // Build system or external dependencies
+        'ci',       // CI configuration
+        'chore',    // Other changes (maintenance, tooling)
+        'revert',   // Revert a previous commit
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
+  },
+};
