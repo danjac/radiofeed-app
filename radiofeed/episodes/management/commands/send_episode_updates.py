101d71df585c592683e32296edf3e8aa13abdacc
Dan Jacob <danjac2018@gmail.com>
2026-02-11 16:45:06 +0200

    feat: add django tasks

--- radiofeed/episodes/management/commands/send_episode_updates.py
+++ radiofeed/episodes/management/commands/send_episode_updates.py
@@ -1,21 +1,7 @@
-import datetime
-import random
-from typing import TYPE_CHECKING
-
-from django.contrib.sites.models import Site
-from django.core.mail import get_connection
 from django.core.management.base import BaseCommand, CommandParser
-from django.db.models import Exists, OuterRef, QuerySet
-from django.utils import timezone
 
-from radiofeed.episodes.models import Episode
-from radiofeed.thread_pool import db_threadsafe, thread_pool_map
-from radiofeed.users.notifications import get_recipients, send_notification_email
-
-if TYPE_CHECKING:
-    from allauth.account.models import EmailAddress
-
-    from radiofeed.users.models import User
+from radiofeed.episodes.tasks import send_episode_updates
+from radiofeed.users.notifications import get_recipients
 
 
 class Command(BaseCommand):
@@ -43,77 +29,11 @@ class Command(BaseCommand):
     def handle(self, *, days_since: int, limit: int, **options) -> None:
         """Handler implementation."""
 
-        since = timezone.now() - datetime.timedelta(days=days_since)
+        recipient_ids = get_recipients().values_list("id", flat=True)
 
-        site = Site.objects.get_current()
-        connection = get_connection()
-
-        @db_threadsafe
-        def _worker(recipient: EmailAddress) -> tuple[EmailAddress, bool]:
-            if episodes := self._get_new_episodes(
-                recipient.user, since=since, limit=limit
-            ):
-                send_notification_email(
-                    site,
-                    recipient,
-                    f"Hi, {recipient.user.name}, check out these new podcast episodes!",
-                    "episodes/emails/notifications.html",
-                    {
-                        "episodes": episodes,
-                    },
-                    connection=connection,
-                )
-                return recipient, True
-            return recipient, False
-
-        recipients = get_recipients().select_related("user")
-
-        for recipient, sent in thread_pool_map(_worker, recipients):
-            if sent:
-                self.stdout.write(f"Sent episode notifications to {recipient.email}")
-
-    def _get_new_episodes(
-        self,
-        user: User,
-        *,
-        since: datetime.datetime,
-        limit: int,
-    ) -> QuerySet[Episode]:
-        # Fetch latest episode IDs for each podcast the user is subscribed to since given time
-        # Exclude any that the user has bookmarked or listened to
-        episodes = dict(
-            Episode.objects.annotate(
-                is_bookmarked=Exists(
-                    user.bookmarks.filter(
-                        episode=OuterRef("pk"),
-                    )
-                ),
-                is_listened=Exists(
-                    user.audio_logs.filter(
-                        episode=OuterRef("pk"),
-                    )
-                ),
-                is_subscribed=Exists(
-                    user.subscriptions.filter(
-                        podcast=OuterRef("podcast"),
-                    )
-                ),
+        for recipient_id in recipient_ids:
+            send_episode_updates.enqueue(
+                recipient_id=recipient_id,
+                days_since=days_since,
+                limit=limit,
             )
-            .filter(
-                is_bookmarked=False,
-                is_listened=False,
-                is_subscribed=True,
-                pub_date__gte=since,
-            )
-            .order_by("pub_date", "pk")
-            .values_list("podcast", "pk")
-        )
-        # Randomly sample up to `limit` episode IDs
-        if episode_ids := list(episodes.values()):
-            sample_ids = random.sample(episode_ids, min(len(episode_ids), limit))
-            return (
-                Episode.objects.filter(pk__in=sample_ids)
-                .select_related("podcast")
-                .order_by("-pub_date", "-pk")
-            )
-        return Episode.objects.none()
---
+++ radiofeed/episodes/tasks.py
@@ -0,0 +1,90 @@
+import datetime
+import logging
+import random
+from typing import TYPE_CHECKING
+
+from django.contrib.sites.models import Site
+from django.db.models import Exists, OuterRef, QuerySet
+from django.tasks import task  # type: ignore[reportMissingTypeStubs]
+from django.utils import timezone
+
+from radiofeed.episodes.models import Episode
+from radiofeed.users.notifications import get_recipients, send_notification_email
+
+if TYPE_CHECKING:
+    from radiofeed.users.models import User
+
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def send_episode_updates(
+    *,
+    recipient_id: int,
+    days_since: int = 7,
+    limit: int = 6,
+) -> None:
+    """Send podcast recommendations to users."""
+
+    recipient = get_recipients().select_related("user").get(pk=recipient_id)
+    site = Site.objects.get_current()
+    since = timezone.now() - datetime.timedelta(days=days_since)
+
+    if episodes := _get_new_episodes(recipient.user, since=since, limit=limit):
+        send_notification_email(
+            site,
+            recipient,
+            f"Hi, {recipient.user.name}, check out these new podcast episodes!",
+            "episodes/emails/notifications.html",
+            {
+                "episodes": episodes,
+            },
+        )
+        logger.info("Sent episode updates to %s", recipient.user)
+
+
+def _get_new_episodes(
+    user: User,
+    *,
+    since: datetime.datetime,
+    limit: int,
+) -> QuerySet[Episode]:
+    # Fetch latest episode IDs for each podcast the user is subscribed to since given time
+    # Exclude any that the user has bookmarked or listened to
+    episodes = dict(
+        Episode.objects.annotate(
+            is_bookmarked=Exists(
+                user.bookmarks.filter(
+                    episode=OuterRef("pk"),
+                )
+            ),
+            is_listened=Exists(
+                user.audio_logs.filter(
+                    episode=OuterRef("pk"),
+                )
+            ),
+            is_subscribed=Exists(
+                user.subscriptions.filter(
+                    podcast=OuterRef("podcast"),
+                )
+            ),
+        )
+        .filter(
+            is_bookmarked=False,
+            is_listened=False,
+            is_subscribed=True,
+            pub_date__gte=since,
+        )
+        .order_by("pub_date", "pk")
+        .values_list("podcast", "pk")
+    )
+    # Randomly sample up to `limit` episode IDs
+    if episode_ids := list(episodes.values()):
+        sample_ids = random.sample(episode_ids, min(len(episode_ids), limit))
+        return (
+            Episode.objects.filter(pk__in=sample_ids)
+            .select_related("podcast")
+            .order_by("-pub_date", "-pk")
+        )
+    return Episode.objects.none()
