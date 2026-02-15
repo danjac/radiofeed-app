ac4c3d98096c6a0c44a0587671f0cbc01594a5bb
Dan Jacob <danjac2018@gmail.com>
2026-02-16 00:28:50 +0200

    chore: tidy up release.sh

--- release.sh
+++ release.sh
@@ -5,7 +5,6 @@ set -o errexit
 # Runs Django management commands to prepare the application for release:
 # - checks the deployment settings
 # - applies database migrations
-# - runs health checks
 
 MANAGE="python ./manage.py"
 
